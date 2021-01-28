e85c9ec653cd8cd52f44f61712d1c30c7449b499
josipmrden <josip.mrden@memgraph.io>
2021-01-28 14:16:46 +0100

    Added tsp, setcover and distance

---
+++ python/distance_calculator.py
@@ -0,0 +1,112 @@
+import mgp
+import math
+
+KM_MULTIPLIER = 0.001
+LATITUDE = 'lat'
+LONGITUDE = 'lng'
+
+
+@mgp.read_proc
+def single(context: mgp.ProcCtx,
+              start: mgp.Nullable[mgp.Any],
+              end: mgp.Nullable[mgp.Any],
+              metrics: str = 'm'
+              ) -> mgp.Record(distance=mgp.Number):
+    '''
+    This distance calculator procedure  for one entry returns 1 field.
+
+      * `distance` is the final result obtained by calculating distance (in metres) between the 2 points who
+        each have its latitude and longitude.
+
+    The procedure can be invoked in openCypher using the following calls:
+      CALL distance_calculator.single((:Point {lat:1, lng:2}), (:Point {lat:2, lng:3.5})) YIELD distance;
+      MATCH (n1:Point), (n2:Point) CALL distance_calculator.procedure(n, 1) YIELD * RETURN *;
+    '''
+
+    if not isinstance(start, (mgp.Vertex)) or not isinstance(end, (mgp.Vertex)):
+        return mgp.Record(distance=None)
+
+    distance = calculate_distance_between_points(
+        dict(start.properties.items()),
+        dict(end.properties.items()),
+        metrics)
+
+    return mgp.Record(distance=distance)
+
+
+@mgp.read_proc
+def multiple(context: mgp.ProcCtx,
+              start_points: mgp.Nullable[mgp.Any],
+              end_points: mgp.Nullable[mgp.Any],
+              metrics: str = 'm'
+              ) -> mgp.Record(distances=list):
+    '''
+    This distance calculator procedure  for multiple entries returns 1 field.
+
+      * `distances` is the final result obtained by calculating distances (in metres) between pairs of
+      points who each have its latitude and longitude
+
+    The procedure can be invoked in openCypher using the following calls:
+      CALL distance_calculator.multiple([(:Point {lat:1, lng:2})]), [(:Point {lat:3,lng:4.5})])) YIELD distances;
+    '''
+
+    if len(start_points) != len(end_points) or len(start_points) == 0:
+        return mgp.Record(distances=None)
+
+    for i in range(len(start_points)):
+        if not isinstance(start_points[i], (mgp.Vertex)) or not isinstance(end_points[i], (mgp.Vertex)):
+            return mgp.Record(distances=None)
+
+    distances = []
+    for i in range(len(start_points)):
+        d = calculate_distance_between_points(
+            dict(start_points[i].properties.items()),
+            dict(end_points[i].properties.items()),
+            metrics)
+
+        distances.append(d)
+
+    return mgp.Record(distances=distances)
+
+
+def calculate_distance_between_points(start, end, metrics='m'):
+    '''
+    Returns distance based on the metrics between 2 points.
+    :param start: Start node - dictionary with lat and lng
+    :param end: End node - dictionary with lat and lng
+    :param metrics: m - in metres, km - in kilometres
+    :return: float distance
+    '''
+
+    if (LATITUDE not in start.keys()
+        or LONGITUDE not in start.keys()
+        or LATITUDE not in end.keys()
+        or LONGITUDE not in end.keys()):
+        return 0
+
+    lat_1 = start[LATITUDE]
+    lng_1 = start[LONGITUDE]
+    lat_2 = end[LATITUDE]
+    lng_2 = end[LONGITUDE]
+
+    R = 6371E3
+    pi_radians = math.pi / 180.00
+
+    phi_1 = lat_1 * pi_radians
+    phi_2 = lat_2 * pi_radians
+    delta_phi = (lat_2 - lat_1) * pi_radians
+    delta_lambda = (lng_2 - lng_1) * pi_radians
+
+    sin_delta_phi = math.sin(delta_phi / 2.0)
+    sin_delta_lambda = math.sin(delta_lambda / 2.0)
+
+    a = sin_delta_phi ** 2 + math.cos(phi_1) * math.cos(phi_2) * (sin_delta_lambda ** 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    #Distance in metres
+    distance = R * c
+
+    if metrics.lower() == 'km':
+        distance *= KM_MULTIPLIER
+
+    return distance
---
+++ python/set_cover.py
@@ -0,0 +1,275 @@
+import mgp
+import random
+import abc
+
+from gekko import GEKKO
+
+@mgp.read_proc
+def cp_solve(context: mgp.ProcCtx,
+              element_vertexes: list,
+              set_vertexes: list
+              ) -> mgp.Record(resulting_sets=list):
+    '''
+    This set cover solver method returns 1 filed
+
+      * `resulting_sets` is a minimal set of sets in which all the element have been contained
+
+    The input arguments consist of
+
+      * `element_vertexes` that is a list of element nodes
+      * `set_vertexes` that is a list of set nodes those elements are contained in
+
+    Element and set equivalents at a certain index come in pairs so mappings between sets and elements are consistent.
+
+    The procedure can be invoked in openCypher using the following calls, e.g.:
+      CALL set_cover.cp_solve([(:Point), (:Point)], [(:Set), (:Set)]) YIELD resulting_sets;
+
+    The method uses constraint programming as a solving tool for obtaining a minimal set of sets that contain
+        all the elements.
+    '''
+
+    creator = GekkoMatchingProblemCreator()
+    mp = creator.create_matching_problem(element_vertexes, set_vertexes)
+
+    solver = GekkoMPSolver()
+    result = solver.solve(matching_problem=mp)
+
+    resulting_nodes = [context.graph.get_vertex_by_id(x) for x in result]
+
+    return [mgp.Record(resulting_sets=resulting_nodes)]
+
+
+@mgp.read_proc
+def greedy(context: mgp.ProcCtx,
+              element_vertexes: list,
+              set_vertexes: list
+              ) -> mgp.Record(resulting_sets=list):
+    '''
+    This set cover solver method returns 1 filed
+
+      * `resulting_sets` is a minimal set of sets in which all the element have been contained
+
+    The input arguments consist of
+
+      * `element_vertexes` that is a list of element nodes
+      * `set_vertexes` that is a list of set nodes those elements are contained in
+
+    Element and set equivalents at a certain index come in pairs so mappings between sets and elements are consistent.
+
+    The procedure can be invoked in openCypher using the following calls, e.g.:
+      CALL set_cover.cp_solve([(:Point), (:Point)], [(:Set), (:Set)]) YIELD resulting_sets;
+
+    The method uses a greedy method as a solving tool for obtaining a minimal set of sets that contain
+        all the elements.
+    '''
+
+    creator = GreedyMatchingProblemCreator()
+    mp = creator.create_matching_problem(element_vertexes, set_vertexes)
+
+    solver = GreedyMPSolver()
+    result = solver.solve(matching_problem=mp)
+
+    resulting_nodes = [context.graph.get_vertex_by_id(x) for x in result]
+
+    return [mgp.Record(resulting_sets=resulting_nodes)]
+
+
+class GreedyMatchingProblem:
+    '''
+    Matching problem to be used with greedy solving of set cover.
+    '''
+
+    def __init__(self, elements, containing_sets, elements_by_sets):
+        self.elements = elements
+        self.containing_sets = containing_sets
+        self.elements_by_sets = elements_by_sets
+
+
+class GekkoMatchingProblem:
+    '''
+    Matching problem to be used with gekko constraint programming solving of set cover.
+    '''
+
+    def __init__(self, containing_sets, sets_by_elements):
+        self.containing_sets = containing_sets
+        self.sets_by_elements = sets_by_elements
+
+
+class MatchingProblemCreator(abc.ABC):
+    '''
+    Creator abstract class of matching problems
+    '''
+
+    @abc.abstractmethod
+    def create_matching_problem(self, element_vertexes, set_vertexes):
+        '''
+        Creates a matching problem
+        :param element_vertexes: Element vertexes pair component list
+        :param set_vertexes: Set vertexes pair component list
+        :return: matching problem
+        '''
+
+        pass
+
+
+class GreedyMatchingProblemCreator(MatchingProblemCreator):
+    '''
+    Creator class for set cover to be solved with greedy method
+    '''
+
+    def create_matching_problem(self, element_vertexes, set_vertexes):
+        '''
+        Creates a matching problem to be solved with greedy method
+        :param element_vertexes: Element vertexes pair component list
+        :param set_vertexes: Set vertexes pair component list
+        :return: matching problem
+        '''
+
+        element_values = [x.id for x in element_vertexes]
+        set_values = [x.id for x in set_vertexes]
+        all_elements = set(element_values)
+        all_sets = set(set_values)
+
+        elements_by_sets = dict()
+
+        for idx in range(len(element_values)):
+            element = element_values[idx]
+            contained_set = set_values[idx]
+
+            if contained_set not in elements_by_sets.keys():
+                elements_by_sets[contained_set] = set()
+
+            elements_by_sets[contained_set].add(element)
+
+        return GreedyMatchingProblem(all_elements, all_sets, elements_by_sets)
+
+
+class GekkoMatchingProblemCreator(MatchingProblemCreator):
+    '''
+    Creator class for set cover to be solved with gekko constraint programming
+    '''
+
+    def create_matching_problem(self, element_vertexes, set_vertexes):
+        '''
+        Creates a matching problem to be solved with gekko constraing programming method
+        :param element_vertexes: Element vertexes pair component list
+        :param set_vertexes: Set vertexes pair component list
+        :return: matching problem
+        '''
+
+        element_values = [x.id for x in element_vertexes]
+        set_values = [x.id for x in set_vertexes]
+        set_values_distinct = set(set_values)
+        sets_by_elements = dict()
+
+        for idx in range(len(element_values)):
+            element = element_values[idx]
+            contained_set = set_values[idx]
+
+            if element not in sets_by_elements.keys():
+                sets_by_elements[element] = set()
+
+            sets_by_elements[element].add(contained_set)
+
+        return GekkoMatchingProblem(set_values_distinct, sets_by_elements)
+
+
+class MatchingProblemSolver(abc.ABC):
+    '''
+    Solver of set cover matching problem
+    '''
+
+    @abc.abstractmethod
+    def solve(self, matching_problem):
+        '''
+        Solves the matching problem and returns the set indices
+        :param matching_problem: matching problem
+        :return: set indices
+        '''
+        pass
+
+
+class GekkoMPSolver(MatchingProblemSolver):
+    '''
+    Solver of set cover with gekko constraint programming
+    '''
+
+    def solve(self, matching_problem):
+        '''
+        Solves the matching problem and returns the set indices
+        :param matching_problem: matching problem
+        :return: set indices
+        '''
+
+        m = GEKKO(remote=True)
+        m.options.SOLVER = 1
+        containing_const = m.Const(1, name='const')
+
+        set_list = list(matching_problem.containing_sets)
+        vars = [m.Var(lb=0, ub=1, integer=True, name=GekkoMPSolver.get_variable_name(i))
+                         for i in range(len(set_list))]
+        set_ordinal_map = dict()
+
+        for i in range(len(set_list)):
+            set_ordinal_map[set_list[i]] = i
+
+        for element in matching_problem.sets_by_elements.keys():
+            containing_sets = matching_problem.sets_by_elements[element]
+            contained_sets_eq = 0
+
+            for contained_set in list(containing_sets):
+                ordinal_number = set_ordinal_map[contained_set]
+                contained_sets_eq = contained_sets_eq + vars[ordinal_number]
+
+            m.Equation(equation=contained_sets_eq >= containing_const)
+
+        m.Obj(sum(vars))
+        m.solve()
+
+        resulting_sets = []
+        for idx, var in enumerate(vars):
+            if var.value[0] == 1.0:
+                resulting_sets.append(set_list[idx])
+
+        return resulting_sets
+
+
+    @staticmethod
+    def get_variable_name(set_no):
+        '''
+        Returns unique variable name based on the set id
+        :param set_no: set id
+        :return: set variable name
+        '''
+
+        return f"containing_set_{set_no}"
+
+
+class GreedyMPSolver(MatchingProblemSolver):
+    '''
+    Solver of set cover with greedy method
+    '''
+
+    def solve(self, matching_problem):
+        '''
+        Solves the matching problem and returns the set indices
+        :param matching_problem: matching problem
+        :return: set indices
+        '''
+
+        universe = matching_problem.elements
+
+        possible_sets = list(matching_problem.containing_sets)
+        random.shuffle(possible_sets)
+
+        picked_sets = list()
+        covered_universe = set()
+
+        while len(universe) != len(covered_universe):
+            picked_set = possible_sets[0]
+            picked_sets.append(picked_set)
+            possible_sets = possible_sets[1:]
+
+            covered_universe |= matching_problem.elements_by_sets[picked_set]
+
+        return picked_sets
