4b4a517c6e8f389fc85aaf78ed54414ab99cafd5
Marko Budiselic <mbudiselicbuda@gmail.com>
2021-03-13 10:36:23 +0100

    Add initial parts of query modules Rust library

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -7,10 +7,42 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::ffi::CString;
 use std::os::raw::c_int;
 
-struct MgValue {
+//// START "library" part.
+// TODO(gitbuda): The expect is panicking (after CString::new), being in panic is not good in
+// general, but in this particular case it will affect Memgraph (take it down). Figure out how to
+// not panic and still operate correctly.
+
+#[derive(Debug, Clone)]
+struct MgpAllocationError;
+impl std::fmt::Display for MgpAllocationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Unable to allocate memory inside Rust procedure.")
+    }
+}
+
+#[derive(Debug, Clone)]
+struct MgpPreparingResultError;
+impl std::fmt::Display for MgpPreparingResultError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Unable to prepare result within Rust procedure.")
+    }
+}
+
+#[derive(Debug, Clone)]
+struct MgpAddProcedureParameterTypeError;
+impl std::fmt::Display for MgpAddProcedureParameterTypeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "Unable to add a type of procedure paramater in Rust Module."
+        )
+    }
+}
+
+struct MgpValue {
     value: *mut mgp_value,
 }
-impl Drop for MgValue {
+impl Drop for MgpValue {
     fn drop(&mut self) {
         unsafe {
             mgp_value_destroy(self.value);
@@ -18,62 +50,137 @@ impl Drop for MgValue {
     }
 }
 
+fn make_int_value(
+    value: i64,
+    result: *mut mgp_result,
+    memory: *mut mgp_memory,
+) -> Result<MgpValue, MgpAllocationError> {
+    let unable_alloc_value_msg = CString::new("Unable to allocate an integer.")
+        .expect("CString::new failed prior to allocating an integer value.");
+    unsafe {
+        let mg_value: MgpValue = MgpValue {
+            value: mgp_value_make_int(value, memory),
+        };
+        if mg_value.value.is_null() {
+            mgp_result_set_error_msg(result, unable_alloc_value_msg.into_raw());
+            return Err(MgpAllocationError);
+        }
+        return Ok(mg_value);
+    }
+}
+
+struct MgpResultRecord {
+    record: *mut mgp_result_record,
+}
+
+fn make_result_record(result: *mut mgp_result) -> Result<MgpResultRecord, MgpAllocationError> {
+    let record_fail_msg = CString::new("Unable to allocate record")
+        .expect("CString::new failed on record allocation");
+    unsafe {
+        let record = mgp_result_new_record(result);
+        if record.is_null() {
+            mgp_result_set_error_msg(result, record_fail_msg.into_raw());
+            return Err(MgpAllocationError);
+        }
+        Ok(MgpResultRecord { record: record })
+    }
+}
+
+fn insert_result_record(
+    mgp_record: &MgpResultRecord,
+    mgp_name: String,
+    mgp_value: &MgpValue,
+    result: *mut mgp_result,
+) -> Result<(), MgpPreparingResultError> {
+    let name =
+        CString::new(mgp_name.into_bytes()).expect("CString::new failed on allocating a name");
+    let name_not_inserted_msg = CString::new("Unable to insert record to the result.")
+        .expect("CString::new failed prior to inserting record.");
+
+    unsafe {
+        let inserted =
+            mgp_result_record_insert(mgp_record.record, name.into_raw(), mgp_value.value);
+        if inserted == 0 {
+            mgp_result_set_error_msg(result, name_not_inserted_msg.into_raw());
+            return Err(MgpPreparingResultError);
+        }
+        return Ok(());
+    }
+}
+
+fn add_read_procedure(
+    proc_ptr: extern "C" fn(*const mgp_list, *const mgp_graph, *mut mgp_result, *mut mgp_memory),
+    name: String,
+    module: *mut mgp_module,
+) -> *mut mgp_proc {
+    unsafe {
+        return mgp_module_add_read_procedure(
+            module,
+            CString::new(name.into_bytes())
+                .expect("A valid procedure name.")
+                .into_raw(),
+            Some(proc_ptr),
+        );
+    }
+}
+
+fn add_result_type(
+    procedure: *mut mgp_proc,
+    name: String,
+) -> Result<(), MgpAddProcedureParameterTypeError> {
+    unsafe {
+        if !mgp_proc_add_result(
+            procedure,
+            CString::new(name.into_bytes())
+                .expect("A valid argument name.")
+                .into_raw(),
+            mgp_type_int(),
+        ) == 0
+        {
+            return Err(MgpAddProcedureParameterTypeError);
+        }
+        return Ok(());
+    }
+}
+//// END "library" part.
+
 extern "C" fn test_procedure(
     _args: *const mgp_list,
     _graph: *const mgp_graph,
-    _result: *mut mgp_result,
-    _memory: *mut mgp_memory,
+    result: *mut mgp_result,
+    memory: *mut mgp_memory,
 ) {
-    let unable_alloc_a_msg =
-        CString::new("Unable to allocate a.").expect("CString::new failed on allocating a value");
-    let a_name = CString::new("a").expect("CString::new failed on allocating a name");
-    let a_not_insterted_msg = CString::new("Unable to insert a to the record")
-        .expect("CString::new failed on inserting a");
-    let record_fail_msg = CString::new("Unable to allocate record")
-        .expect("CString::new failed on record allocation");
-
-    unsafe {
-        let record = mgp_result_new_record(_result);
-        if record.is_null() {
-            mgp_result_set_error_msg(_result, record_fail_msg.into_raw());
+    match make_result_record(result) {
+        Ok(mgp_record) => match make_int_value(0, result, memory) {
+            Ok(mgp_value) => {
+                match insert_result_record(&mgp_record, "a".to_string(), &mgp_value, result) {
+                    Ok(_) => {}
+                    Err(_) => {
+                        return;
+                    }
+                }
+            }
+            Err(_) => {
+                return;
+            }
+        },
+        Err(_) => {
             return;
-        }
-        let a: MgValue = MgValue {
-            value: mgp_value_make_int(0, _memory),
-        };
-        if a.value.is_null() {
-            mgp_result_set_error_msg(_result, unable_alloc_a_msg.into_raw());
-            return;
-        }
-        let a_inserted = mgp_result_record_insert(record, a_name.into_raw(), a.value);
-        if a_inserted == 0 {
-            mgp_result_set_error_msg(_result, a_not_insterted_msg.into_raw());
         }
     }
 }
 
 #[no_mangle]
 pub extern "C" fn mgp_init_module(module: *mut mgp_module, _memory: *mut mgp_memory) -> c_int {
-    unsafe {
-        let procedure = mgp_module_add_read_procedure(
-            module,
-            CString::new("test_procedure")
-                .expect("A valid function name.")
-                .into_raw(),
-            Some(test_procedure),
-        );
-        if !mgp_proc_add_result(
-            procedure,
-            CString::new("a")
-                .expect("A valid argument name.")
-                .into_raw(),
-            mgp_type_int(),
-        ) == 0
-        {
+    let procedure = add_read_procedure(test_procedure, "test_procedure".to_string(), module);
+    match add_result_type(procedure, "a".to_string()) {
+        Ok(_) => {
+            return 0;
+        }
+        Err(_) => {
             return 1;
         }
     }
-    0
 }
 
 #[no_mangle]
