5ad3122308310d77e731e4eae42e061fa7edbec2
Josip Matak <31473001+jmatak@users.noreply.github.com>
2021-04-01 16:47:27 +0200

    C++ Memgraph Utility (#16)
    * Initial Memgraph utility update.
    * Expose mg_graph as a link library.
    * Template mg_graph utility.
    * Default TSize parameter to uint64_t.
    * Safer management of raw pointer.

--- cpp/connectivity_module/connectivity_module.cpp
+++ cpp/connectivity_module/connectivity_module.cpp
@@ -1,136 +1,93 @@
 #include <queue>
 #include <unordered_map>
 
-#include "mg_procedure.h"
-#include "utils/on_scope_exit.hpp"
+#include <mg_exceptions.hpp>
+#include <mg_procedure.h>
+#include <mg_utils.hpp>
+
+namespace {
+
+const char *field_component_id = "component_id";
+const char *field_vertex = "node";
+
+void InsertWeaklyComponentResult(const mgp_graph *graph, mgp_result *result,
+                                 mgp_memory *memory, const int component_id,
+                                 const int vertex_id) {
+  mgp_result_record *record = mgp_result_new_record(result);
+  if (record == nullptr) {
+    throw mg_exception::NotEnoughMemoryException();
+  }
+
+  mg_utility::InsertNodeValueResult(graph, record, field_vertex, vertex_id,
+                                    memory);
+  mg_utility::InsertIntValueResult(record, field_component_id, component_id,
+                                   memory);
+}
 
 /// Finds weakly connected components of a graph.
 ///
 /// Time complexity: O(|V|+|E|)
-static void weak(const mgp_list *args, const mgp_graph *graph,
+static void Weak(const mgp_list *args, const mgp_graph *memgraph_graph,
                  mgp_result *result, mgp_memory *memory) {
-  std::unordered_map<int64_t, int64_t> vertex_component;
-  mgp_vertices_iterator *vertices_iterator =
-      mgp_graph_iter_vertices(graph, memory);
-  if (vertices_iterator == nullptr) {
-    mgp_result_set_error_msg(result, "Unable to create vertices iterator");
+  try {
+    auto graph = mg_utility::GetGraphView(memgraph_graph, result, memory);
+
+    std::unordered_map<std::uint64_t, std::uint64_t> vertex_component;
+    std::uint64_t curr_component = 0;
+    for (auto vertex : graph->Nodes()) {
+      if (vertex_component.find(vertex.id) != vertex_component.end())
+        continue;
+
+      // Run BFS from current vertex.
+      std::queue<std::uint64_t> q;
+
+      q.push(vertex.id);
+      vertex_component[vertex.id] = curr_component;
+      while (!q.empty()) {
+        auto v_id = q.front();
+        q.pop();
+
+        // Iterate over inbound edges.
+        for (auto neihgbor : graph->Neighbours(v_id)) {
+          auto next_id = neihgbor.node_id;
+
+          if (vertex_component.find(next_id) != vertex_component.end()) {
+            continue;
+          }
+          vertex_component[next_id] = curr_component;
+          q.push(next_id);
+        }
+      }
+
+      ++curr_component;
+    }
+
+    for (const auto &p : vertex_component) {
+
+      auto vertex_id = graph->GetMemgraphNodeId(p.first);
+      auto component_id = p.second;
+
+      InsertWeaklyComponentResult(memgraph_graph, result, memory, component_id,
+                                  vertex_id);
+    }
+  } catch (const std::exception &e) {
+    // We must not let any exceptions out of our module.
+    mgp_result_set_error_msg(result, e.what());
     return;
   }
-  utils::OnScopeExit vertex_iterator_deleter([&vertices_iterator] {
-    if (vertices_iterator != nullptr) {
-      mgp_vertices_iterator_destroy(vertices_iterator);
-    }
-  });
-
-  int64_t curr_component = 0;
-  for (const mgp_vertex *vertex = mgp_vertices_iterator_get(vertices_iterator);
-       vertex != nullptr;
-       vertex = mgp_vertices_iterator_next(vertices_iterator)) {
-    mgp_vertex_id vertex_id = mgp_vertex_get_id(vertex);
-    if (vertex_component.find(vertex_id.as_int) != vertex_component.end())
-      continue;
-
-    // Run BFS from current vertex.
-    std::queue<int64_t> q;
-    q.push(vertex_id.as_int);
-    vertex_component[vertex_id.as_int] = curr_component;
-    while (!q.empty()) {
-      mgp_vertex *v = mgp_graph_get_vertex_by_id(graph, {q.front()}, memory);
-      if (v == nullptr) {
-        mgp_result_set_error_msg(result, "Unable to get vertex");
-        return;
-      }
-      utils::OnScopeExit v_deleter([&v] {
-        if (v != nullptr) {
-          mgp_vertex_destroy(v);
-        }
-      });
-
-      q.pop();
-
-      // Iterate over inbound edges.
-      mgp_edges_iterator *edges_iterator = mgp_vertex_iter_in_edges(v, memory);
-      if (edges_iterator == nullptr) {
-        mgp_result_set_error_msg(result, "Unable to create edges iterator");
-        return;
-      }
-      for (const mgp_edge *edge = mgp_edges_iterator_get(edges_iterator);
-           edge != nullptr; edge = mgp_edges_iterator_next(edges_iterator)) {
-        mgp_vertex_id next_id = mgp_vertex_get_id(mgp_edge_get_from(edge));
-        if (vertex_component.find(next_id.as_int) != vertex_component.end())
-          continue;
-        vertex_component[next_id.as_int] = curr_component;
-        q.push(next_id.as_int);
-      }
-
-      // Iterate over outbound edges.
-      mgp_edges_iterator_destroy(edges_iterator);
-      edges_iterator = mgp_vertex_iter_out_edges(v, memory);
-      if (edges_iterator == nullptr) {
-        mgp_result_set_error_msg(result, "Unable to create edges iterator");
-        return;
-      }
-      for (const mgp_edge *edge = mgp_edges_iterator_get(edges_iterator);
-           edge != nullptr; edge = mgp_edges_iterator_next(edges_iterator)) {
-        mgp_vertex_id next_id = mgp_vertex_get_id(mgp_edge_get_to(edge));
-        if (vertex_component.find(next_id.as_int) != vertex_component.end())
-          continue;
-        vertex_component[next_id.as_int] = curr_component;
-        q.push(next_id.as_int);
-      }
-
-      mgp_edges_iterator_destroy(edges_iterator);
-    }
-
-    ++curr_component;
-  }
-
-  for (const auto &p : vertex_component) {
-    mgp_result_record *record = mgp_result_new_record(result);
-    if (record == nullptr) {
-      mgp_result_set_error_msg(result, "Unable to allocate record");
-      return;
-    }
-
-    mgp_value *id_value = mgp_value_make_int(p.first, memory);
-    if (id_value == nullptr) {
-      mgp_result_set_error_msg(result, "Unable to allocated id");
-      return;
-    }
-    utils::OnScopeExit id_value_deleter([&id_value] {
-      if (id_value != nullptr) {
-        mgp_value_destroy(id_value);
-      }
-    });
-
-    mgp_value *comp_value = mgp_value_make_int(p.second, memory);
-    if (comp_value == nullptr) {
-      mgp_result_set_error_msg(result, "Unable to allocate value");
-      return;
-    }
-    utils::OnScopeExit comp_value_deleter([&comp_value] {
-      if (comp_value != nullptr) {
-        mgp_value_destroy(comp_value);
-      }
-    });
-
-    int mem_id_inserted = mgp_result_record_insert(record, "id", id_value);
-    int comp_inserted =
-        mgp_result_record_insert(record, "component", comp_value);
-    if (!mem_id_inserted || !comp_inserted) {
-      mgp_result_set_error_msg(result,
-                               "Unable to insert id and value to record");
-      return;
-    }
-  }
 }
+} // namespace
 
 extern "C" int mgp_init_module(struct mgp_module *module,
                                struct mgp_memory *memory) {
   struct mgp_proc *wcc_proc =
-      mgp_module_add_read_procedure(module, "weak", weak);
-  if (!mgp_proc_add_result(wcc_proc, "id", mgp_type_int())) return 1;
-  if (!mgp_proc_add_result(wcc_proc, "component", mgp_type_int())) return 1;
+      mgp_module_add_read_procedure(module, "get", Weak);
+  if (!wcc_proc)
+    return 1;
+  if (!mgp_proc_add_result(wcc_proc, field_component_id, mgp_type_int()))
+    return 1;
+  if (!mgp_proc_add_result(wcc_proc, field_vertex, mgp_type_node()))
+    return 1;
   return 0;
 }
 
---
+++ cpp/mg_utility/mg_exceptions.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <exception>
+#include <iostream>
+
+namespace mg_exception {
+struct NotEnoughMemoryException : public std::exception {
+  const char *what() const throw() { return "Not enough memory!"; }
+};
+
+struct InvalidIDException : public std::exception {
+  const char *what() const throw() { return "Invalid ID!"; }
+};
+} // namespace mg_exception
