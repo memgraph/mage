5a8575908428237b2ba51321338fce2689c1320c
MasterMedo <mislav.vuletic@gmail.com>
2021-04-12 14:48:15 +0200

    Fix errors and add documentation

--- python/random_walk.py
+++ python/random_walk.py
@@ -11,16 +11,17 @@ def get_path(
     start: mgp.Vertex,
     number_of_nodes: int = DEFAULT_NUMBER_OF_NODES,
 ) -> mgp.Record(path=mgp.Path):
-    """
+    """Generates a random path of length `number_of_nodes` or less starting
+    from the `start` vertex.
     """
     path = mgp.Path(start)
     vertex = start
-    for _ in range(10):
+    for _ in range(number_of_nodes):
         try:
             edge = random.choice(list(vertex.out_edges))
             path.expand(edge)
-            vertex = edge.to_vertex()
-        except Exception:
+            vertex = edge.to_vertex
+        except IndexError:
             break
 
     return mgp.Record(path=path)
