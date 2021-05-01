ce6b6a3cdc6df0c1a380fead7432b2d6d02963f1
Marko Budiselic <marko.budiselic@memgraph.com>
2021-05-01 12:36:49 +0200

    Remove deref_nullptr from include bindings

--- rust/rsmgp-sys/src/mgp.rs
+++ rust/rsmgp-sys/src/mgp.rs
@@ -2,7 +2,6 @@
     dead_code,
     non_upper_case_globals,
     non_camel_case_types,
-    deref_nullptr,
-    non_snake_case
+    non_snake_case,
 )]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
