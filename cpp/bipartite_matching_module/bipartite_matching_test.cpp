6e8da6e9a3fe0a195603f75d0090a7a1911ebe21
jmatak <josip.matak@hotmail.com>
2021-05-04 10:37:23 +0200

    Naming fix

--- cpp/bipartite_matching_module/bipartite_matching_test.cpp
+++ cpp/bipartite_matching_module/bipartite_matching_test.cpp
@@ -17,104 +17,107 @@ TEST(BipartiteMatching, RandomCompleteBipartiteGraphs) {
     std::uniform_int_distribution<> dist(1, max_size);
     auto size_a = dist(rng), size_b = dist(rng);
 
-    std::vector<std::pair<uint64_t, uint64_t>> edges;
+    std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges;
     for (int i = 1; i <= size_a; ++i)
-      for (int j = 1; j <= size_b; ++j) edges.emplace_back(i, j);
+      for (int j = 1; j <= size_b; ++j) bipartite_edges.emplace_back(i, j);
 
-    auto max_match = bipartite_matching_util::MaximumMatching(edges);
+    auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
     ASSERT_EQ(max_match, std::min(size_a, size_b));
   }
 }
 
 TEST(BipartiteMatching, NotBipartiteGraph) {
-  auto g = mg_generate::BuildGraph(5, {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}});
-  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*g);
+  auto graph =
+      mg_generate::BuildGraph(5, {{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}});
+  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*graph);
   ASSERT_FALSE(is_graph_bipartite);
 }
 
 TEST(BipartiteMatching, BipartiteGraph) {
-  auto g = mg_generate::BuildGraph(4, {{0, 1}, {1, 2}, {2, 3}, {3, 0}});
-  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*g);
+  auto graph = mg_generate::BuildGraph(4, {{0, 1}, {1, 2}, {2, 3}, {3, 0}});
+  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*graph);
   ASSERT_TRUE(is_graph_bipartite);
 }
 
 TEST(BipartiteMatching, BipartiteGraphWith2Components) {
-  auto g = mg_generate::BuildGraph(5, {{0, 1}, {1, 2}, {3, 4}});
-  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*g);
+  auto graph = mg_generate::BuildGraph(5, {{0, 1}, {1, 2}, {3, 4}});
+  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*graph);
   ASSERT_TRUE(is_graph_bipartite);
 }
 
 TEST(BipartiteMatching, NotBipartiteGraphWithSelfLoop) {
-  auto g = mg_generate::BuildGraph(5, {{0, 1}, {1, 2}, {2, 3}, {0, 0}});
-  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*g);
+  auto graph = mg_generate::BuildGraph(5, {{0, 1}, {1, 2}, {2, 3}, {0, 0}});
+  auto is_graph_bipartite = bipartite_matching_util::IsGraphBipartite(*graph);
   ASSERT_FALSE(is_graph_bipartite);
 }
 
 TEST(BipartiteMatching, Handmade1) {
-  std::vector<std::pair<uint64_t, uint64_t>> edges = {{1, 2}, {1, 3}, {2, 1}, {2, 4}, {3, 3}, {4, 3}, {4, 4}, {5, 5}};
-  auto max_match = bipartite_matching_util::MaximumMatching(edges);
+  std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges = {{1, 2}, {1, 3}, {2, 1}, {2, 4},
+                                                                {3, 3}, {4, 3}, {4, 4}, {5, 5}};
+  auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
   ASSERT_EQ(max_match, 5);
 }
 
 TEST(BipartiteMatching, HandmadeGraph1) {
-  auto g = mg_generate::BuildGraph(10, {{0, 6}, {0, 7}, {1, 5}, {1, 8}, {2, 7}, {3, 7}, {3, 8}, {4, 9}});
-  auto max_match = bipartite_matching_alg::BipartiteMatching(*g);
+  auto graph = mg_generate::BuildGraph(10, {{0, 6}, {0, 7}, {1, 5}, {1, 8}, {2, 7}, {3, 7}, {3, 8}, {4, 9}});
+  auto max_match = bipartite_matching_alg::BipartiteMatching(*graph);
   ASSERT_EQ(max_match, 5);
 }
 
 TEST(BipartiteMatching, Handmade2) {
-  std::vector<std::pair<uint64_t, uint64_t>> edges = {{5, 2}, {1, 2}, {4, 3}, {3, 1}, {2, 2}, {4, 4}};
-  auto max_match = bipartite_matching_util::MaximumMatching(edges);
+  std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges = {{5, 2}, {1, 2}, {4, 3}, {3, 1}, {2, 2}, {4, 4}};
+  auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
   ASSERT_EQ(max_match, 3);
 }
 
 TEST(BipartiteMatching, HandmadeGraph2) {
-  auto g = mg_generate::BuildGraph(9, {{4, 6}, {0, 6}, {3, 7}, {2, 5}, {1, 6}, {3, 8}});
-  auto max_match = bipartite_matching_alg::BipartiteMatching(*g);
+  auto graph = mg_generate::BuildGraph(9, {{4, 6}, {0, 6}, {3, 7}, {2, 5}, {1, 6}, {3, 8}});
+  auto max_match = bipartite_matching_alg::BipartiteMatching(*graph);
   ASSERT_EQ(max_match, 3);
 }
 
 TEST(BipartiteMatching, Handmade3) {
-  std::vector<std::pair<uint64_t, uint64_t>> edges = {{1, 2}, {1, 5}, {2, 3}, {3, 2}, {4, 3}, {4, 4}, {5, 1}};
-  auto max_match = bipartite_matching_util::MaximumMatching(edges);
+  std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges = {{1, 2}, {1, 5}, {2, 3}, {3, 2}, {4, 3}, {4, 4}, {5, 1}};
+  auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
   ASSERT_EQ(max_match, 5);
 }
 
 TEST(BipartiteMatching, HandmadeGraph3) {
-  auto g = mg_generate::BuildGraph(10, {{0, 6}, {0, 9}, {1, 7}, {2, 6}, {3, 7}, {3, 8}, {4, 5}});
-  auto max_match = bipartite_matching_alg::BipartiteMatching(*g);
+  auto graph = mg_generate::BuildGraph(10, {{0, 6}, {0, 9}, {1, 7}, {2, 6}, {3, 7}, {3, 8}, {4, 5}});
+  auto max_match = bipartite_matching_alg::BipartiteMatching(*graph);
   ASSERT_EQ(max_match, 5);
 }
 
 TEST(BipartiteMatching, Handmade4) {
-  std::vector<std::pair<uint64_t, uint64_t>> edges = {{1, 4}, {1, 10}, {2, 6}, {2, 8}, {2, 9},  {3, 9},  {4, 6}, {4, 8},
-                                                      {5, 1}, {5, 3},  {5, 9}, {6, 1}, {6, 5},  {6, 7},  {7, 4}, {7, 7},
-                                                      {8, 2}, {8, 8},  {9, 3}, {9, 5}, {10, 1}, {10, 2}, {10, 7}};
-  auto max_match = bipartite_matching_util::MaximumMatching(edges);
+  std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges = {
+      {1, 4}, {1, 10}, {2, 6}, {2, 8}, {2, 9}, {3, 9}, {4, 6}, {4, 8}, {5, 1},  {5, 3},  {5, 9}, {6, 1},
+      {6, 5}, {6, 7},  {7, 4}, {7, 7}, {8, 2}, {8, 8}, {9, 3}, {9, 5}, {10, 1}, {10, 2}, {10, 7}};
+  auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
   ASSERT_EQ(max_match, 10);
 }
 
 TEST(BipartiteMatching, HandmadeGraph4) {
-  auto g = mg_generate::BuildGraph(
+  auto graph = mg_generate::BuildGraph(
       20, {{0, 13}, {0, 19}, {1, 15}, {1, 17}, {1, 18}, {2, 18}, {3, 15}, {3, 17}, {4, 10}, {4, 12}, {4, 18}, {5, 10},
            {5, 14}, {5, 16}, {6, 13}, {6, 16}, {7, 11}, {7, 17}, {8, 12}, {8, 14}, {9, 10}, {9, 11}, {9, 17}});
-  auto max_match = bipartite_matching_alg::BipartiteMatching(*g);
+  auto max_match = bipartite_matching_alg::BipartiteMatching(*graph);
   ASSERT_EQ(max_match, 10);
 }
 
 TEST(BipartiteMatching, Handmade5) {
-  std::vector<std::pair<uint64_t, uint64_t>> edges = {
+  std::vector<std::pair<uint64_t, uint64_t>> bipartite_edges = {
       {1, 7}, {2, 1}, {2, 6}, {2, 8}, {3, 4}, {3, 5}, {3, 6}, {3, 7}, {4, 2}, {4, 4},  {4, 5},  {5, 5},  {5, 6},
       {6, 3}, {6, 4}, {6, 7}, {7, 5}, {7, 7}, {8, 4}, {8, 5}, {9, 3}, {9, 6}, {9, 10}, {10, 1}, {10, 8}, {10, 9}};
-  auto max_match = bipartite_matching_util::MaximumMatching(edges);
+  auto max_match = bipartite_matching_util::MaximumMatching(bipartite_edges);
   ASSERT_EQ(max_match, 9);
 }
 
 TEST(BipartiteMatching, HandmadeGraph5) {
-  auto g = mg_generate::BuildGraph(20, {{0, 16}, {1, 10}, {1, 15}, {1, 17}, {2, 13}, {2, 14}, {2, 15}, {2, 16}, {3, 11},
-                                        {3, 13}, {3, 14}, {4, 14}, {4, 15}, {5, 12}, {5, 13}, {5, 16}, {6, 14}, {6, 16},
-                                        {7, 13}, {7, 14}, {8, 12}, {8, 15}, {8, 19}, {9, 10}, {9, 17}, {9, 18}});
-  auto max_match = bipartite_matching_alg::BipartiteMatching(*g);
+  auto graph =
+      mg_generate::BuildGraph(20, {{0, 16}, {1, 10}, {1, 15}, {1, 17}, {2, 13}, {2, 14}, {2, 15}, {2, 16}, {3, 11},
+                                   {3, 13}, {3, 14}, {4, 14}, {4, 15}, {5, 12}, {5, 13}, {5, 16}, {6, 14}, {6, 16},
+                                   {7, 13}, {7, 14}, {8, 12}, {8, 15}, {8, 19}, {9, 10}, {9, 17}, {9, 18}});
+  auto max_match = bipartite_matching_alg::BipartiteMatching(*graph);
   ASSERT_EQ(max_match, 9);
 }
 
@@ -126,14 +129,15 @@ TEST(BipartiteMatching, Performance) {
   auto num_of_edges = num_of_nodes * num_of_nodes / 5;
 
   std::uniform_int_distribution<> dist(1, num_of_nodes);
-  std::set<std::pair<uint64_t, uint64_t>> edges;
-  for (int i = 0; i < num_of_edges; ++i) {
+  std::set<std::pair<uint64_t, uint64_t>> bipartite_edges;
+  for (std::size_t i = 0; i < num_of_edges; ++i) {
     auto from = dist(rng), to = dist(rng);
-    edges.insert({from, to});
+    bipartite_edges.insert({from, to});
   }
 
   mg_test_utility::Timer timer;
-  bipartite_matching_util::MaximumMatching(std::vector<std::pair<uint64_t, uint64_t>>(edges.begin(), edges.end()));
+  bipartite_matching_util::MaximumMatching(
+      std::vector<std::pair<uint64_t, uint64_t>>(bipartite_edges.begin(), bipartite_edges.end()));
   auto time_elapsed = timer.Elapsed();
   ASSERT_TRUE(time_elapsed < std::chrono::seconds(1));
 }
