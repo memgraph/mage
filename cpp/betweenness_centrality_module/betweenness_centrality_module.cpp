c6c496657df44710be5a4693ebac125617744865
SuzanaPratljacic <suzana.pratljacic31097@gmail.com>
2021-05-11 11:37:12 +0200

    Add e2e test

--- cpp/betweenness_centrality_module/betweenness_centrality_module.cpp
+++ cpp/betweenness_centrality_module/betweenness_centrality_module.cpp
@@ -8,6 +8,9 @@ namespace {
 constexpr char const *kFieldBCScore = "betweeenness_centrality";
 constexpr char const *kFieldNode = "node";
 
+constexpr char const *kArgumentDirected = "directed";
+constexpr char const *kArgumentNormalized = "normalized";
+
 void InsertBCRecord(const mgp_graph *graph, mgp_result *result, mgp_memory *memory, const double betweeenness_centrality,
                        const int node_id) {
   auto *record = mgp_result_new_record(result);
@@ -20,15 +23,12 @@ void InsertBCRecord(const mgp_graph *graph, mgp_result *result, mgp_memory *memo
 
 void GetBetweennessCentrality(const mgp_list *args, const mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   try {
-    //const mgp_value *directed_mgp_value = mgp_list_at(args, 0);
-    //const mgp_value * normalized_mgp_value = mgp_list_at(args, 1);
+    auto directed_value = mgp_list_at(args, 0);
+    auto normalized_value = mgp_list_at(args, 1);
 
-    //auto directed = mgp_value_get_bool(directed_mgp_value);
-    //auto normalized = mgp_value_get_bool(normalized_mgp_value);
-
-    bool directed = false;
-    bool normalized = false;
-
+    auto directed = mgp_value_get_bool(directed_value);
+    auto normalized = mgp_value_get_int(normalized_value);
+  
     auto graph_type = mg_graph::GraphType::kUndirectedGraph;
     if (directed) graph_type = mg_graph::GraphType::kDirectedGraph;
 
@@ -36,10 +36,9 @@ void GetBetweennessCentrality(const mgp_list *args, const mgp_graph *memgraph_gr
     auto BC = betweenness_centrality_alg::BetweennessCentrality(*graph, directed, normalized);
 
     auto number_of_nodes = graph->Nodes().size();
-    for (std::uint64_t node_id = 0; node_id < number_of_nodes; ++node_id) {
-      std::cout << node_id << " ";
+    for (std::uint64_t node_id = 0; node_id < number_of_nodes; ++node_id)
       InsertBCRecord(memgraph_graph, result, memory, BC[node_id], graph->GetMemgraphNodeId(node_id));
-    }
+  
   } catch (const std::exception &e) {
     // We must not let any exceptions out of our module.
     mgp_result_set_error_msg(result, e.what());
@@ -52,11 +51,22 @@ void GetBetweennessCentrality(const mgp_list *args, const mgp_graph *memgraph_gr
 // Here you can register multiple procedures your module supports.
 extern "C" int mgp_init_module(mgp_module *module, mgp_memory *memory) {
   mgp_proc *proc = mgp_module_add_read_procedure(module, "get", GetBetweennessCentrality);
-
   if (!proc) return 1;
 
-  if (!mgp_proc_add_result(proc, "node", mgp_type_node())) return 1;
-  if (!mgp_proc_add_result(proc, "betweenness", mgp_type_number())) return 1;
+  auto bool_value_directed = mgp_value_make_bool(true, memory);
+  auto bool_value_normalized =  mgp_value_make_bool(true, memory);
+  // Query module arguments
+  if (!mgp_proc_add_opt_arg(proc, kArgumentDirected, mgp_type_bool(), bool_value_directed))
+    return 1;
+  if (!mgp_proc_add_opt_arg(proc, kArgumentNormalized, mgp_type_bool(), bool_value_normalized))
+    return 1;
+  
+  mgp_value_destroy(bool_value_directed);
+  mgp_value_destroy(bool_value_normalized);
+
+  // Query module output record
+  if (!mgp_proc_add_result(proc, kFieldNode, mgp_type_node())) return 1;
+  if (!mgp_proc_add_result(proc, kFieldBCScore, mgp_type_float())) return 1;
 
   return 0;
 }
