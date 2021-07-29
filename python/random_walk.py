217131f932b16f2a15addebf5147240265adf386
MasterMedo <mislav.vuletic@gmail.com>
2021-07-29 13:43:09 +0200

    add code documentation

--- python/random_walk.py
+++ python/random_walk.py
@@ -2,21 +2,22 @@ import mgp
 import random
 
 
-DEFAULT_NUMBER_OF_NODES = 10
-
-
 @mgp.read_proc
-def get_path(
-    context: mgp.ProcCtx,
+def get(
     start: mgp.Vertex,
-    number_of_nodes: int = DEFAULT_NUMBER_OF_NODES,
+    length: int = 10,
 ) -> mgp.Record(path=mgp.Path):
-    """Generates a random path of length `number_of_nodes` or less starting
+    """Generates a random path of length `length` or less starting
     from the `start` vertex.
+
+    :param mgp.Vertex start: The starting node of the walk.
+    :param int length: The number of edges to traverse.
+    :return: Random path.
+    :rtype: mgp.Record(mgp.Path)
     """
     path = mgp.Path(start)
     vertex = start
-    for _ in range(number_of_nodes):
+    for _ in range(length):
         try:
             edge = random.choice(list(vertex.out_edges))
             path.expand(edge)
