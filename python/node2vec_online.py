030a250ab563e5112aef1d36f5ed0257f4935944
antoniofilipovic <filipovicantonio1998@gmail.com>
2021-09-09 16:49:53 +0200

    reformat node2vec online

--- python/node2vec_online.py
+++ python/node2vec_online.py
@@ -5,8 +5,8 @@ import mgp
 from itertools import chain, repeat
 from inspect import cleandoc
 
-from mage.node2vec_online_module.w2v_learners import (GensimWord2Vec)
-from mage.node2vec_online_module.walk_sampling import (StreamWalkUpdater)
+from mage.node2vec_online_module.w2v_learners import GensimWord2Vec
+from mage.node2vec_online_module.walk_sampling import StreamWalkUpdater
 
 
 class Node2VecContext:
@@ -40,10 +40,7 @@ class Node2VecContext:
         self._start_time = start_time
 
     def is_initialized(self):
-        return (
-                self._learner is not None
-                and self._updater is not None
-        )
+        return self._learner is not None and self._updater is not None
 
     def update_model(self, source: int, target: int, current_time: int):
         sampled_pairs = self.updater.process_new_edge(source, target, current_time)
@@ -55,41 +52,43 @@ node2vec_context = Node2VecContext()
 
 @mgp.read_proc
 def set_streamwalk_updater(
-        ctx: mgp.ProcCtx,
-        half_life: int = 7200,
-        max_length: int = 3,
-        beta: mgp.Number = 0.9,
-        cutoff: int = 604800,
-        sampled_walks: int = 4,
-        full_walks: bool = False) -> mgp.Record(message=str):
+    ctx: mgp.ProcCtx,
+    half_life: int = 7200,
+    max_length: int = 3,
+    beta: mgp.Number = 0.9,
+    cutoff: int = 604800,
+    sampled_walks: int = 4,
+    full_walks: bool = False,
+) -> mgp.Record(message=str):
     """
-        This function sets updater to StreamWalk.
+    This function sets updater to StreamWalk.
 
-        If parameters are already set, function doesn't reset parameters.
+    If parameters are already set, function doesn't reset parameters.
 
-        :param half_life: Half-life in seconds for time decay
-        :param max_length: Maximum length of the sampled temporal random walks
-        :param beta: Damping factor for long paths
-        :param cutoff: Temporal cutoff in seconds to exclude very distant past
-        :param sampled_walks:  Number of sampled walks for each edge update
-        :param full_walks: Return every node of the sampled walk for representation learning (full_walks=True) or
-        only the endpoints of the walk (full_walks=False)
+    :param half_life: Half-life in seconds for time decay
+    :param max_length: Maximum length of the sampled temporal random walks
+    :param beta: Damping factor for long paths
+    :param cutoff: Temporal cutoff in seconds to exclude very distant past
+    :param sampled_walks:  Number of sampled walks for each edge update
+    :param full_walks: Return every node of the sampled walk for representation learning (full_walks=True) or
+    only the endpoints of the walk (full_walks=False)
 
-        :return: empty record
+    :return: empty record
 
-        Example call (give all information):
-           CALL node2vec_online.set_stream_walk_updater(7200, 3, 0.9, 604800, 4, False) YIELD *;
+    Example call (give all information):
+       CALL node2vec_online.set_stream_walk_updater(7200, 3, 0.9, 604800, 4, False) YIELD *;
 
-        Example call (with predefined parameters):
-           CALL node2vec_online.set_stream_walk_updater() YIELD *;
+    Example call (with predefined parameters):
+       CALL node2vec_online.set_stream_walk_updater() YIELD *;
     """
     global node2vec_context
 
     if node2vec_context.is_initialized():
         return mgp.Record(
             message="StreamWalk updater is already initialized."
-                    "Call: `CALL node2vec_online.reset() YIELD *:` in order to set parameters again. "
-                    "Warning: all embeddings will be lost.")
+            "Call: `CALL node2vec_online.reset() YIELD *:` in order to set parameters again. "
+            "Warning: all embeddings will be lost."
+        )
 
     node2vec_context.updater = StreamWalkUpdater(
         half_life=half_life,
@@ -104,31 +103,31 @@ def set_streamwalk_updater(
 
 @mgp.read_proc
 def set_word2vec_learner(
-        ctx: mgp.ProcCtx,
-        embedding_dimension: int = 128,
-        learning_rate: mgp.Number = 0.01,
-        skip_gram: bool = True,
-        negative_rate: mgp.Number = 10,
-        threads: mgp.Nullable[int] = None,
+    ctx: mgp.ProcCtx,
+    embedding_dimension: int = 128,
+    learning_rate: mgp.Number = 0.01,
+    skip_gram: bool = True,
+    negative_rate: mgp.Number = 10,
+    threads: mgp.Nullable[int] = None,
 ) -> mgp.Record(message=str):
     """
-        This function needs to be called to set parameters.
+     This function needs to be called to set parameters.
 
-        If parameters are already set, function doesn't reset parameters.
+     If parameters are already set, function doesn't reset parameters.
 
-        :param embedding_dimension number of dimensions of the representation of embedding vector
-        :param learning_rate learning rate
-        :param skip_gram use skip-gram model
-        :param negative_rate negative rate for skip-gram model
-        :param threads maximum number of threads for parallelization
-        :return: empty record
+     :param embedding_dimension number of dimensions of the representation of embedding vector
+     :param learning_rate learning rate
+     :param skip_gram use skip-gram model
+     :param negative_rate negative rate for skip-gram model
+     :param threads maximum number of threads for parallelization
+     :return: empty record
 
 
-       Example call (give all information):
-           CALL node2vec_online.set_gensim_word2vec_learner(128, 0.01, True, 10, 1) YIELD *;
+    Example call (give all information):
+        CALL node2vec_online.set_gensim_word2vec_learner(128, 0.01, True, 10, 1) YIELD *;
 
-       Example call (with predefined parameters):
-           CALL node2vec_online.set_gensim_word2vec_learner() YIELD *;
+    Example call (with predefined parameters):
+        CALL node2vec_online.set_gensim_word2vec_learner() YIELD *;
     """
 
     global node2vec_context
@@ -136,8 +135,9 @@ def set_word2vec_learner(
     if node2vec_context.is_initialized():
         return mgp.Record(
             message="Word2Vec learner is already initialized. "
-                    "Call: `CALL node2vec_online.reset() YIELD *;`in order to set parameters again. "
-                    "Warning: all embeddings will be lost.")
+            "Call: `CALL node2vec_online.reset() YIELD *;`in order to set parameters again. "
+            "Warning: all embeddings will be lost."
+        )
 
     if threads is None:
         threads = multiprocessing.cpu_count()
@@ -155,7 +155,7 @@ def set_word2vec_learner(
 @mgp.read_proc
 def reset() -> mgp.Record(message=str):
     """
-        This function resets parameters and embeddings already learned.
+    This function resets parameters and embeddings already learned.
     """
 
     global node2vec_context
@@ -168,22 +168,24 @@ def reset() -> mgp.Record(message=str):
 
 @mgp.read_proc
 def get(
-        ctx: mgp.ProcCtx,
+    ctx: mgp.ProcCtx,
 ) -> mgp.Record(node=mgp.Vertex, embedding=mgp.List[mgp.Number]):
     """
-       Returns current node embeddings as list of mgp.Vertex and embedding which is vector
+    Returns current node embeddings as list of mgp.Vertex and embedding which is vector
 
-       Example call (give all information):
-           CALL graph_analyzer.analyze() YIELD *;
+    Example call (give all information):
+        CALL graph_analyzer.analyze() YIELD *;
 
-       Example call (with parameter):
-           CALL graph_analyzer.analyze(['nodes', 'edges']) YIELD *;
+    Example call (with parameter):
+        CALL graph_analyzer.analyze(['nodes', 'edges']) YIELD *;
     """
 
     if not node2vec_context.is_initialized():
-        raise mgp.AbortError("Learner or updater are not initialized. Initialize them by calling:"
-                             "`CALL node2vec_online.set_word2vec_learner() YIELD *;`"
-                             "`CALL node2vec_online.set_streamwalk_updater() YIELD *;`")
+        raise mgp.AbortError(
+            "Learner or updater are not initialized. Initialize them by calling:"
+            "`CALL node2vec_online.set_word2vec_learner() YIELD *;`"
+            "`CALL node2vec_online.set_streamwalk_updater() YIELD *;`"
+        )
 
     embedding_vectors = node2vec_context.learner.get_embedding_vectors()
 
@@ -201,19 +203,19 @@ def get(
 @mgp.read_proc
 def update(ctx: mgp.ProcCtx, edges: mgp.List[mgp.Edge]) -> mgp.Record():
     """
-       For this function to work, Learner and Updater must be set.
+    For this function to work, Learner and Updater must be set.
 
-       This function updates embeddings of nodes.
+    This function updates embeddings of nodes.
 
-       Example call (give all information):
-           MATCH (n)-[e]->(m)
-           WITH COLLECT(e) as edges_list
-           CALL node2vec_online.update(edges_list) RETURN edges_list
-           LIMIT 5;
+    Example call (give all information):
+        MATCH (n)-[e]->(m)
+        WITH COLLECT(e) as edges_list
+        CALL node2vec_online.update(edges_list) RETURN edges_list
+        LIMIT 5;
 
-       Other possibility is to set TRIGGER call:
-           CALL node2vec_online.update(['nodes', 'edges']) YIELD *;
-       """
+    Other possibility is to set TRIGGER call:
+        CALL node2vec_online.update(['nodes', 'edges']) YIELD *;
+    """
 
     global node2vec_context
 
@@ -223,13 +225,17 @@ def update(ctx: mgp.ProcCtx, edges: mgp.List[mgp.Edge]) -> mgp.Record():
         node2vec_context.start_time = current_time
 
     if not node2vec_context.is_initialized():
-        raise mgp.AbortError("Learner or updater are not initialized. Initialize them by calling:"
-                             "`CALL node2vec_online.set_word2vec_learner() YIELD *;`"
-                             "`CALL node2vec_online.set_streamwalk_updater() YIELD *;` ")
+        raise mgp.AbortError(
+            "Learner or updater are not initialized. Initialize them by calling:"
+            "`CALL node2vec_online.set_word2vec_learner() YIELD *;`"
+            "`CALL node2vec_online.set_streamwalk_updater() YIELD *;` "
+        )
 
     for e in edges:
         ctx.check_must_abort()
-        node2vec_context.update_model(e.from_vertex.id, e.to_vertex.id, int(current_time))
+        node2vec_context.update_model(
+            e.from_vertex.id, e.to_vertex.id, int(current_time)
+        )
     return mgp.Record()
 
 
