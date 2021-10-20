4cfbac86a900c136757f1d2669c6a1ac3d424fa4
antoniofilipovic <filipovicantonio1998@gmail.com>
2021-10-20 16:37:11 +0200

    add write proc

--- python/node2vec.py
+++ python/node2vec.py
@@ -38,7 +38,6 @@ def learn_embeddings(
 @mgp.read_proc
 def get_embeddings(
     ctx: mgp.ProcCtx,
-    edges: List[mgp.Edge],
     is_directed: bool = False,
     p=2.0,
     q=0.5,
@@ -55,7 +54,7 @@ def get_embeddings(
     hs=0,
     negative=5,
     epochs=5,
-) -> mgp.Record(node=mgp.Vertex, embedding=mgp.List[mgp.Number]):
+) -> mgp.Record(nodes=mgp.List[mgp.Vertex], embeddings=mgp.List[mgp.List[mgp.Number]]):
     """
     Function to get node embeddings. Uses gensim.models.Word2Vec params.
 
@@ -116,28 +115,134 @@ def get_embeddings(
     }
 
     edges_weights = {}
-
-    for edge in edges:
-        edge_weight = float(edge.properties.get("weight", default=1))
-        src_id = int(edge.from_vertex.id)
-        dest_id = int(edge.to_vertex.id)
-        edges_weights[(src_id, dest_id)] = edge_weight
+    for vertex in ctx.graph.vertices:
+        for edge in vertex.out_edges:
+            edge_weight = float(edge.properties.get("weight", default=1))
+            edges_weights[(edge.from_vertex.id, edge.to_vertex.id)] = edge_weight
 
     graph: Graph = GraphHolder(edges_weights, is_directed)
     second_order_random_walk = SecondOrderRandomWalk(
         p=p, q=q, num_walks=int(num_walks), walk_length=int(walk_length)
     )
+
+    walks = second_order_random_walk.sample_node_walks(graph)
+    embeddings = learn_embeddings(walks, **word2vec_params)
+
+    embeddings_result = []
+    nodes_result = []
+    for node_id, embedding in embeddings.items():
+        embeddings[node_id] = [float(e) for e in embedding]
+        nodes_result.append(ctx.graph.get_vertex_by_id(node_id))
+        embeddings_result.append(embeddings[node_id])
+
+    return mgp.Record(nodes=nodes_result, embeddings=embeddings_result)
+
+
+@mgp.write_proc
+def set_embeddings(
+    ctx: mgp.ProcCtx,
+    is_directed: bool = False,
+    p=2.0,
+    q=0.5,
+    num_walks=4,
+    walk_length=5,
+    vector_size=100,
+    alpha=0.025,
+    window=5,
+    min_count=5,
+    seed=1,
+    workers=1,
+    min_alpha=0.0001,
+    sg=1,
+    hs=0,
+    negative=5,
+    epochs=5,
+) -> mgp.Record(nodes=mgp.List[mgp.Vertex], embeddings=mgp.List[mgp.List[mgp.Number]]):
+    """
+    Function to get node embeddings. Uses gensim.models.Word2Vec params.
+
+    Parameters
+    ----------
+    edges : List[mgp.Edge]
+        All the edges in graph.
+    is_directed : bool, optional
+        If bool=True, graph is treated as directed, else not directed
+    p : float, optional
+        Return hyperparameter for calculating transition probabilities.
+    q : float, optional
+        Inout hyperparameter for calculating transition probabilities.
+    num_walks : int, optional
+        Number of walks per node in walk sampling.
+    walk_length : int, optional
+        Length of one walk in walk sampling.
+
+    vector_size : int, optional
+        Dimensionality of the word vectors.
+    window : int, optional
+        Maximum distance between the current and predicted word within a sentence.
+    min_count : int, optional
+        Ignores all words with total frequency lower than this.
+    workers : int, optional
+        Use these many worker threads to train the model (=faster training with multicore machines).
+    sg : {0, 1}, optional
+        Training algorithm: 1 for skip-gram; otherwise CBOW.
+    hs : {0, 1}, optional
+        If 1, hierarchical softmax will be used for model training.
+        If 0, and `negative` is non-zero, negative sampling will be used.
+    negative : int, optional
+        If > 0, negative sampling will be used, the int for negative specifies how many "noise words"
+        should be drawn (usually between 5-20).
+        If set to 0, no negative sampling is used.
+    cbow_mean : {0, 1}, optional
+        If 0, use the sum of the context word vectors. If 1, use the mean, only applies when cbow is used.
+    alpha : float, optional
+        The initial learning rate.
+    min_alpha : float, optional
+        Learning rate will linearly drop to `min_alpha` as training progresses.
+    seed : int, optional
+        Seed for the random number generator. Initial vectors for each word are seeded with a hash of
+        the concatenation of word + `str(seed)`.
+    """
+    word2vec_params = {
+        Parameters.VECTOR_SIZE: vector_size,
+        Parameters.WINDOW: window,
+        Parameters.MIN_COUNT: min_count,
+        Parameters.WORKERS: workers,
+        Parameters.MIN_ALPHA: min_alpha,
+        Parameters.SEED: seed,
+        Parameters.ALPHA: alpha,
+        Parameters.EPOCHS: epochs,
+        Parameters.SG: sg,
+        Parameters.HS: hs,
+        Parameters.NEGATIVE: negative,
+    }
+
+    edges_weights = {}
+    for vertex in ctx.graph.vertices:
+        for edge in vertex.out_edges:
+            edge_weight = float(edge.properties.get("weight", default=1))
+            edges_weights[(edge.from_vertex.id, edge.to_vertex.id)] = edge_weight
+
+    graph: Graph = GraphHolder(edges_weights, is_directed)
+    second_order_random_walk = SecondOrderRandomWalk(
+        p=p, q=q, num_walks=int(num_walks), walk_length=int(walk_length)
+    )
+
     walks = second_order_random_walk.sample_node_walks(graph)
 
     embeddings = learn_embeddings(walks, **word2vec_params)
+    embeddings_result = []
+    nodes_result = []
 
     for node_id, embedding in embeddings.items():
         embeddings[node_id] = [float(e) for e in embedding]
+        vertex = ctx.graph.get_vertex_by_id(node_id)
+        vertex.properties.set("embedding", embeddings[node_id])
 
-    return [
-        mgp.Record(node=ctx.graph.get_vertex_by_id(node_id), embedding=embedding)
-        for node_id, embedding in embeddings.items()
-    ]
+        nodes_result.append(ctx.graph.get_vertex_by_id(node_id))
+        embeddings_result.append(embeddings[node_id])
+
+    return mgp.Record(nodes=nodes_result, embeddings=embeddings_result)
 
 
 @mgp.read_proc
