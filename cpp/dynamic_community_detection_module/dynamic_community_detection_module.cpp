d3887c533d514ecf61fe49f2f318d7c458847eb7
antepusic <ante.pusic@fer.hr>
2021-11-15 00:35:29 +0100

    Fix last commit's logic

--- cpp/dynamic_community_detection_module/dynamic_community_detection_module.cpp
+++ cpp/dynamic_community_detection_module/dynamic_community_detection_module.cpp
@@ -197,7 +197,7 @@ void Update(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result,
       const auto labels = algorithm.SetLabels(std::move(graph));
 
       for (const auto [node_id, label] : labels) {
-        InsertCommunityDetectionRecord(memgraph_graph, result, memory, node_id,
+        InsertCommunityDetectionUpdateRecord(memgraph_graph, result, memory, node_id,
                                        label);
       }
     }
@@ -326,7 +326,7 @@ extern "C" int mgp_init_module(struct mgp_module *module,
                             mgp::type_list(mgp::type_relationship()),
                             default_deleted_edges);
 
-      mgp::proc_add_result(update_proc, kFieldNode.data(), mgp::type_node());
+      mgp::proc_add_result(update_proc, kFieldNode.data(), mgp::type_int());
       mgp::proc_add_result(update_proc, kFieldCommunityId.data(),
                            mgp::type_int());
 
