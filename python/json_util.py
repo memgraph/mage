9607abfb632a04c8d03efffd6a916cad90974659
katarinasupe <supe.katarina@gmail.com>
2021-11-23 17:59:36 +0100

    Change after review

--- python/json_util.py
+++ python/json_util.py
@@ -1,6 +1,15 @@
 import mgp
 import json
-import urllib.request
+from urllib.request import urlopen
+from urllib.error import URLError
+from pathlib import Path
+
+
+def extract_objects(file):
+    objects = json.load(file)
+    if type(objects) is dict:
+        objects = [objects]
+    return objects
 
 
 @mgp.read_proc
@@ -13,11 +22,14 @@ def load_from_path(ctx: mgp.ProcCtx, path: str) -> mgp.Record(objects=mgp.List[o
     path : str
         Path to the JSON that is being loaded.
     """
-    with open(path) as json_file:
-        objects = json.load(json_file)
+    file = Path(path)
+    if file.exists():
+        opened_file = open(file)
+        objects = extract_objects(opened_file)
+    else:
+        raise FileNotFoundError("There is no file " + path)
 
-        if type(objects) is dict:
-            objects = [objects]
+    opened_file.close()
 
     return mgp.Record(objects=objects)
 
@@ -29,13 +41,14 @@ def load_from_url(ctx: mgp.ProcCtx, url: str) -> mgp.Record(objects=mgp.List[obj
 
     Parameters
     ----------
-    path : str
+    url : str
         URL to the JSON that is being loaded.
     """
-    with urllib.request.urlopen(url) as json_url:
-        objects = json.loads(json_url.read().decode())
-
-        if type(objects) is dict:
-            objects = [objects]
+    try:
+        response = urlopen(url)
+    except URLError:
+        print("There is no file " + url)
+    else:
+        objects = extract_objects(response)
 
     return mgp.Record(objects=objects)
