700a6ae91a1014aa0c907f676b4ddd7da6cfb193
jmatak <josip.matak@hotmail.com>
2021-11-30 15:35:31 +0100

    Reset on init

--- cpp/pagerank_module/pagerank_online_module.cpp
+++ cpp/pagerank_module/pagerank_online_module.cpp
@@ -215,6 +215,9 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     }
   }
 
+  // When module is reloaded, we shold reset the data structures
+  pagerank_online_alg::Reset();
+
   return 0;
 }
 
