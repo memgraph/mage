d45687ea3f9d83aef00ee70ff7d23b1494495baa
katarinasupe <supe.katarina@gmail.com>
2021-12-02 16:51:53 +0100

    Add export util

---
+++ python/export_util.py
@@ -0,0 +1,52 @@
+from typing import List
+import mgp
+import json as js
+
+
+@mgp.read_proc
+def json(ctx: mgp.ProcCtx, file: str) -> mgp.Record():
+    """
+    Procedure to export whole database as JSON to a local file. 
+
+    Parameters
+    ----------
+    path : str
+        Path to the file where JSON will be saved.
+    """
+    nodes = list()
+    edges = list()
+    graph = list()
+
+    for vertex in ctx.graph.vertices:
+        labels = [label.name for label in vertex.labels]
+        properties = dict()
+        for key in vertex.properties.keys():
+            properties[key] = vertex.properties.get(key)
+        node = {
+            "id": vertex.id,
+            "labels": labels,
+            "properties": properties,
+            "type": "node"
+        }
+        nodes.append(node)
+
+        for edge in vertex.out_edges:
+            properties = dict()
+            for key in edge.properties.keys():
+                properties[key] = edge.properties.get(key)
+            relationship = {
+                "id": edge.id,
+                "start": edge.from_vertex.id,
+                "end": edge.to_vertex.id,
+                "label": edge.type.name,
+                "properties": properties,
+                "type": "relationship"
+            }
+            edges.append(relationship)
+
+        graph = nodes + edges
+
+    with open(file, 'w') as outfile:
+        js.dump(graph, outfile, indent=4, sort_keys=True, default=str)
+
+    return mgp.Record()
