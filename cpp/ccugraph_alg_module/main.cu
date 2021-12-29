e257ce16d1650a9dc46c41d38d041553ba0c6301
Marko Budiselic <marko.budiselic@memgraph.com>
2021-12-29 17:06:05 +0100

    Add working cugraph example

--- cpp/ccugraph_alg_module/main.cu
+++ cpp/ccugraph_alg_module/main.cu
@@ -16,13 +16,12 @@
 #include <iostream>
 
 #include <cugraph/algorithms.hpp>
+#include <cugraph/graph_functions.hpp>
 #include <mg_exceptions.hpp>
 #include <mg_utils.hpp>
 #include <raft/distance/distance.hpp>
 #include <raft/handle.hpp>
 #include <rmm/device_uvector.hpp>
-
-// TODO(gitbuda): Just a RAPIDS example, put something useful here, buda ❤ cuda + rapids + cugraph!
 
 constexpr char const *kProcedureRapidsExample = "rapids_example";
 constexpr char const *kProcedureCugraphExample = "cugraph_example";
@@ -57,7 +56,45 @@ void ExampleRapidsProc(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *re
 }
 
 void ExampleCugraphProc(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
-  // TODO(gitbuda): Implement example cuGraph Memgraph procedure.
+  using vertex_t = int64_t;
+  using edge_t = int64_t;
+  using weight_t = float;
+  using vector_weight_t = rmm::device_uvector<weight_t>;
+  using result_t = float;
+
+  raft::handle_t handle{};
+  auto stream = handle.get_stream_view();
+  std::vector<vertex_t> h_vertices = {0, 1};
+  std::vector<vertex_t> h_rows = {0};
+  rmm::device_uvector<vertex_t> d_rows(h_rows.size(), stream);
+  raft::update_device(d_rows.data(), h_rows.data(), h_rows.size(), stream);
+  std::vector<vertex_t> h_cols = {1};
+  rmm::device_uvector<vertex_t> d_cols(h_cols.size(), stream);
+  raft::update_device(d_cols.data(), h_cols.data(), h_cols.size(), stream);
+  std::vector<weight_t> h_weights = {1.0};
+  auto d_weights = std::make_optional<vector_weight_t>(h_weights.size(), stream);
+  raft::update_device((*d_weights).data(), h_weights.data(), h_weights.size(), stream);
+  rmm::device_uvector<result_t> d_pageranks(h_vertices.size(), stream);
+  result_t constexpr alpha{0.85};
+  result_t constexpr epsilon{1e-6};
+  // IMPORTANT: store_transposed has to be true because cugraph::pagerank only
+  // accepts true. It's hard to detect/debug problem because nvcc error
+  // messages contain only the top call details + graph_view has many template
+  // paremeters.
+  cugraph::graph_t<vertex_t, edge_t, weight_t, true, false> graph(handle);
+  std::tie(graph, std::ignore) = cugraph::create_graph_from_edgelist<vertex_t, edge_t, weight_t, true, false>(
+      handle, std::nullopt, std::move(d_rows), std::move(d_cols), std::move(d_weights),
+      cugraph::graph_properties_t{false, false}, false);
+  auto graph_view = graph.view();
+  stream.synchronize();
+
+  cugraph::pagerank<vertex_t, edge_t, weight_t, result_t, false>(
+      handle, graph_view, std::nullopt, std::nullopt, std::nullopt, std::nullopt, d_pageranks.data(), alpha, epsilon);
+
+  for (int i = 0; i < d_pageranks.size(); ++i) {
+    auto value = d_pageranks.element(i, stream);
+    InsertRecord(memgraph_graph, result, memory, value);
+  }
 }
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
@@ -66,7 +103,7 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
         mgp::module_add_read_procedure(module, kProcedureRapidsExample, ExampleRapidsProc);
     mgp::proc_add_result(example_rapids_proc, kResultValue, mgp::type_float());
     struct mgp_proc *example_cugraph_proc =
-        mgp::module_add_read_procedure(module, kProcedureCugraphExample, ExampleRapidsProc);
+        mgp::module_add_read_procedure(module, kProcedureCugraphExample, ExampleCugraphProc);
     mgp::proc_add_result(example_cugraph_proc, kResultValue, mgp::type_float());
   } catch (std::exception &e) {
     return 1;
