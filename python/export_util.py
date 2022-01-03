5d52fe6e9051efd0ffb056df1906381192f6e021
katarinasupe <supe.katarina@gmail.com>
2022-01-03 12:10:14 +0100

    Edges to relationships list

--- python/export_util.py
+++ python/export_util.py
@@ -6,7 +6,7 @@ import json as js
 @mgp.read_proc
 def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
     """
-    Procedure to export the whole database as JSON to a local file. 
+    Procedure to export the whole database as JSON to a local file.
 
     Parameters
     ----------
@@ -14,39 +14,43 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
         Path to the file where JSON will be saved.
     """
     nodes = list()
-    edges = list()
+    relationships = list()
     graph = list()
 
     for vertex in ctx.graph.vertices:
         labels = [label.name for label in vertex.labels]
         properties = dict()
+
         for key in vertex.properties.keys():
             properties[key] = vertex.properties.get(key)
+
         node = {
             "id": vertex.id,
             "labels": labels,
             "properties": properties,
-            "type": "node"
+            "type": "node",
         }
         nodes.append(node)
 
         for edge in vertex.out_edges:
             properties = dict()
+
             for key in edge.properties.keys():
                 properties[key] = edge.properties.get(key)
+
             relationship = {
                 "id": edge.id,
                 "start": edge.from_vertex.id,
                 "end": edge.to_vertex.id,
                 "label": edge.type.name,
                 "properties": properties,
-                "type": "relationship"
+                "type": "relationship",
             }
-            edges.append(relationship)
+            relationships.append(relationship)
 
-        graph = nodes + edges
+        graph = nodes + relationships
 
-    with open(path, 'w') as outfile:
+    with open(path, "w") as outfile:
         js.dump(graph, outfile, indent=4, sort_keys=True, default=str)
 
     return mgp.Record()
