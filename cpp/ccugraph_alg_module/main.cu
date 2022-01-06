ae2602d6540918e90a7eb0ed0aac11b5252d5420
Marko Budiselic <marko.budiselic@memgraph.com>
2022-01-06 17:21:46 +0100

    Add memgraph data to pagerank (still crashes)

--- cpp/ccugraph_alg_module/main.cu
+++ cpp/ccugraph_alg_module/main.cu
@@ -14,6 +14,7 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <utility>
 
 #include <cugraph/algorithms.hpp>
 #include <cugraph/graph_functions.hpp>
@@ -26,6 +27,8 @@
 constexpr char const *kProcedureRapidsExample = "rapids_example";
 constexpr char const *kProcedureCugraphExample = "cugraph_example";
 constexpr char const *kResultValue = "value";
+constexpr char const *kFieldNode = "node";
+constexpr char const *kFieldRank = "rank";
 
 void InsertRecord(mgp_graph *, mgp_result *result, mgp_memory *memory, const double value) {
   auto *record = mgp::result_new_record(result);
@@ -55,45 +58,70 @@ void ExampleRapidsProc(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *re
   }
 }
 
-void ExampleCugraphProc(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
-  using vertex_t = int64_t;
-  using edge_t = int64_t;
-  using weight_t = float;
-  using vector_weight_t = rmm::device_uvector<weight_t>;
-  using result_t = float;
+using vertex_t = int64_t;
+using edge_t = int64_t;
+using weight_t = float;
+using vector_weight_t = rmm::device_uvector<weight_t>;
+using result_t = float;
 
-  raft::handle_t handle{};
+void InsertPagerankRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const std::uint64_t node_id,
+                          double rank) {
+  auto *record = mgp::result_new_record(result);
+
+  mg_utility::InsertNodeValueResult(graph, record, kFieldNode, node_id, memory);
+  mg_utility::InsertDoubleValueResult(record, kFieldRank, rank, memory);
+}
+
+decltype(auto) CreateCugraphFromMemgraph(raft::handle_t const &handle, mgp_graph *mg_graph, mgp_result *result,
+                                         mgp_memory *memory) {
+  auto mg_graph_view = mg_utility::GetGraphView(mg_graph, result, memory, mg_graph::GraphType::kDirectedGraph);
+  const auto &h_vertices = mg_graph_view->Nodes();
+  const auto &mg_edges = mg_graph_view->Edges();
+  std::vector<vertex_t> h_rows;
+  h_rows.reserve(mg_edges.size());
+  std::vector<vertex_t> h_cols;
+  h_cols.reserve(mg_edges.size());
+  std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(h_rows),
+                 [](const mg_graph::Edge<std::uint64_t> &edge) -> vertex_t { return edge.from; });
+  std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(h_cols),
+                 [](const mg_graph::Edge<std::uint64_t> &edge) -> vertex_t { return edge.to; });
   auto stream = handle.get_stream_view();
-  std::vector<vertex_t> h_vertices = {0, 1};
-  std::vector<vertex_t> h_rows = {0};
   rmm::device_uvector<vertex_t> d_rows(h_rows.size(), stream);
   raft::update_device(d_rows.data(), h_rows.data(), h_rows.size(), stream);
-  std::vector<vertex_t> h_cols = {1};
   rmm::device_uvector<vertex_t> d_cols(h_cols.size(), stream);
   raft::update_device(d_cols.data(), h_cols.data(), h_cols.size(), stream);
-  std::vector<weight_t> h_weights = {1.0};
+  std::vector<weight_t> h_weights(h_vertices.size(), 1.0);
   auto d_weights = std::make_optional<vector_weight_t>(h_weights.size(), stream);
   raft::update_device((*d_weights).data(), h_weights.data(), h_weights.size(), stream);
-  rmm::device_uvector<result_t> d_pageranks(h_vertices.size(), stream);
-  result_t constexpr alpha{0.85};
-  result_t constexpr epsilon{1e-6};
   // IMPORTANT: store_transposed has to be true because cugraph::pagerank only
   // accepts true. It's hard to detect/debug problem because nvcc error
   // messages contain only the top call details + graph_view has many template
   // paremeters.
-  cugraph::graph_t<vertex_t, edge_t, weight_t, true, false> graph(handle);
-  std::tie(graph, std::ignore) = cugraph::create_graph_from_edgelist<vertex_t, edge_t, weight_t, true, false>(
+  cugraph::graph_t<vertex_t, edge_t, weight_t, true, false> mg_cugraph(handle);
+  std::tie(mg_cugraph, std::ignore) = cugraph::create_graph_from_edgelist<vertex_t, edge_t, weight_t, true, false>(
       handle, std::nullopt, std::move(d_rows), std::move(d_cols), std::move(d_weights),
       cugraph::graph_properties_t{false, false}, false);
-  auto graph_view = graph.view();
   stream.synchronize();
+  return std::make_pair(std::move(mg_graph_view), std::move(mg_cugraph));
+}
 
-  cugraph::pagerank<vertex_t, edge_t, weight_t, result_t, false>(
-      handle, graph_view, std::nullopt, std::nullopt, std::nullopt, std::nullopt, d_pageranks.data(), alpha, epsilon);
+void ExampleCugraphProc(mgp_list *args, mgp_graph *mg_graph, mgp_result *result, mgp_memory *memory) {
+  raft::handle_t handle{};
+  auto stream = handle.get_stream_view();
 
-  for (int i = 0; i < d_pageranks.size(); ++i) {
-    auto value = d_pageranks.element(i, stream);
-    InsertRecord(memgraph_graph, result, memory, value);
+  auto [mg_graph_view, mg_cugraph] = CreateCugraphFromMemgraph(handle, mg_graph, result, memory);
+  auto mg_cugraph_view = mg_cugraph.view();
+  rmm::device_uvector<result_t> d_pageranks(mg_cugraph_view.get_number_of_vertices(), stream);
+
+  result_t constexpr alpha{0.85};
+  result_t constexpr epsilon{1e-6};
+  cugraph::pagerank<vertex_t, edge_t, weight_t, result_t, false>(handle, mg_cugraph_view, std::nullopt, std::nullopt,
+                                                                 std::nullopt, std::nullopt, d_pageranks.data(), alpha,
+                                                                 epsilon);
+
+  for (int node_id = 0; node_id < d_pageranks.size(); ++node_id) {
+    auto rank = d_pageranks.element(node_id, stream);
+    InsertPagerankRecord(mg_graph, result, memory, mg_graph_view->GetMemgraphNodeId(node_id), rank);
   }
 }
 
--- cpp/mg_utility/data_structures/graph_view.hpp
+++ cpp/mg_utility/data_structures/graph_view.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstdint>
 #include <vector>
 
 #include "graph_data.hpp"
