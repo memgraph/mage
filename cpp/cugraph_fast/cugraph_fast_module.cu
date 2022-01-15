d2c29b2dbd1bfe7f962d3dfa7824c603eabe5c80
Marko Budiselic <marko.budiselic@memgraph.com>
2022-01-15 22:23:34 +0100

    Clean includes

--- cpp/cugraph_fast/cugraph_fast_module.cu
+++ cpp/cugraph_fast/cugraph_fast_module.cu
@@ -12,10 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <stdio.h>
-#include <iostream>
-#include <utility>
-
 #include <cugraph/algorithms.hpp>
 #include <cugraph/graph_functions.hpp>
 #include <mg_exceptions.hpp>
