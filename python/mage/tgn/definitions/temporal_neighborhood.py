7e6da605eb1d2133d2b5a6895b6ac1557b13f101
antoniofilipovic <filipovicantonio1998@gmail.com>
2022-02-08 15:08:50 +0100

    add working node features recursion

---
+++ python/mage/tgn/definitions/temporal_neighborhood.py
@@ -0,0 +1,39 @@
+from typing import Tuple, Dict, List
+
+import numpy as np
+
+
+class TemporalNeighborhood:
+    def __init__(self):
+        super(TemporalNeighborhood, self).__init__()
+
+        self.neighborhood: Dict[int, List[Tuple[int, int, float]]] = {}
+
+    def update_neighborhood(self, sources, destinations, edge_idx, timestamps):
+        # idea is that smallest new timestamp is always greater than last biggest one in dict so we don't need to
+        # sort arrays:)
+        # if it doesn't exist, create empty, else overwrite
+        self.neighborhood = {**{node: [] for node in set(sources).union(set(destinations))}, **self.neighborhood}
+        for neighbor in zip(sources, destinations, edge_idx, timestamps):
+            self.neighborhood[neighbor[0]].append((neighbor[1:]))
+            self.neighborhood[neighbor[1]].append((neighbor[0], neighbor[2], neighbor[3]))
+
+    def get_neighborhood(self, node: int, timestamp: int, num_neighbors: int):
+        # todo check if we need copy
+
+        if node not in self.neighborhood:
+            return np.zeros(num_neighbors, dtype=int), np.zeros(num_neighbors, dtype=int), np.zeros(num_neighbors)
+        neighbors_tuple = self.neighborhood[node].copy()
+
+        unzipped_tuple = list(zip(*neighbors_tuple))
+        neighbors = list(unzipped_tuple[0])[-num_neighbors:]
+        edge_idxs = list(unzipped_tuple[1])[-num_neighbors:]
+        timestamps = list(unzipped_tuple[2])[-num_neighbors:]
+
+        neighbors = np.append(arr=neighbors, values=np.zeros(num_neighbors-len(neighbors)))
+        edge_idxs = np.append(arr=edge_idxs, values=np.zeros(num_neighbors - len(edge_idxs)))
+        timestamps = np.append(arr=timestamps, values=np.zeros(num_neighbors - len(timestamps)))
+        return neighbors, edge_idxs, timestamps
+
+    def find_neighborhood(self, nodes: List[int], num_neighbors: int) -> Dict[int, List[Tuple[int, int, float]]]:
+        return {node: self.neighborhood[node].copy()[:num_neighbors] for node in nodes}
--- python/mage/tgn/definitions/tgn.py
+++ python/mage/tgn/definitions/tgn.py
@@ -1,4 +1,4 @@
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
 import numpy as np
 import torch
@@ -12,6 +12,7 @@ from tgn.definitions.message_aggregator import MeanMessageAggregator, LastMessag
 from tgn.definitions.message_function import MessageFunctionMLP, MessageFunctionIdentity, MessageFunction
 from tgn.definitions.messages import RawMessage, NodeRawMessage, InteractionRawMessage
 from tgn.definitions.raw_message_store import RawMessageStore
+from tgn.definitions.temporal_neighborhood import TemporalNeighborhood
 
 
 class TGN(nn.Module):
@@ -30,6 +31,13 @@ class TGN(nn.Module):
         self.num_of_layers = num_of_layers
         self.memory_dimension = memory_dimension
         self.time_dimension = time_dimension
+        self.num_node_features = num_node_features
+        self.num_edge_features = num_edge_features
+
+        self.edge_features: Dict[int, torch.Tensor] = {}
+        self.node_features: Dict[int, torch.Tensor] = {}
+
+        self.temporal_neighborhood = TemporalNeighborhood()
 
         # dimension of raw message for edge
         # m_ij = (s_i, s_j, delta t, e_ij)
@@ -82,13 +90,18 @@ class TGN(nn.Module):
         TGNLayer = get_layer_type(layer_type)
 
         for i in range(num_of_layers):
-            layer = TGNLayer()
+            layer = TGNLayer(self.temporal_neighborhood,
+                             self.memory_dimension + self.num_node_features,
+                             self.num_edge_features,
+                             self.time_dimension,
+                             layer=num_of_layers - i - 1)
 
             tgn_layers.append(layer)
 
         self.tgn_net = nn.Sequential(*tgn_layers)
 
-    def forward(self, data: List[Event]):
+    def forward(self, data: Tuple[
+        np.ndarray, np.ndarray, np.ndarray, np.ndarray, Dict[int, torch.Tensor], Dict[int, torch.Tensor]]):
 
         # edge_features = Dict(int, np.array)
         # node_features -> Dict(int, np.array)
@@ -109,7 +122,16 @@ class TGN(nn.Module):
         # By doing this, the computation of the memory-related modules directly influences the loss
         self.process_previous_batches()
 
-        result = self.tgn_net(data)
+        working_nodes = np.concatenate([sources.copy(), destinations.copy()], dtype=int)
+        embeddings = self.get_0_layer_embeddings(self.num_of_layers,
+                                                 working_nodes,
+                                                 num_neighbors=5,
+                                                 timestamps=np.concatenate([timestamps, timestamps]))  # for source
+        # and then destination nodes
+
+        tgn_data = (embeddings, self.memory.copy(), working_nodes, timestamps)
+
+        result = self.tgn_net(tgn_data)
 
         # here we update raw message store, and this batch will be used in next
         # call of tgn in function self.process_previous_batches
@@ -123,9 +145,86 @@ class TGN(nn.Module):
                                                     edge_idxs=edge_idxs,
                                                     timestamps=timestamps)
 
+        self.temporal_neighborhood.update_neighborhood(sources=sources,
+                                                       destinations=destinations,
+                                                       timestamps=timestamps,
+                                                       edge_idx=edge_idxs)
+
+        for edge_idx, edge_feature in edge_features.items():
+            self.edge_features[edge_idx] = edge_feature
+
+        for node_id, node_feature in node_features.items():
+            self.node_features[node_id] = node_feature
+
         return result
 
-    def process_previous_batches(self):
+    def get_0_layer_node_feature(self, nodes: np.array):
+        node_features = torch.empty((len(nodes), self.memory_dimension + self.num_node_features))
+        for i, node in enumerate(nodes):
+            memory_features = self.memory.get_node_memory(node)
+            node_feature = self.node_features[node] if node in self.node_features else torch.zeros(
+                self.num_node_features)
+            concat_features = torch.concat((memory_features, node_feature)).reshape(
+                (1, self.memory_dimension + self.num_node_features))
+            node_features[i][:] = concat_features
+        return node_features
+
+
+    def get_0_layer_edge_feature(self, nodes: np.array):
+        node_features = torch.empty((len(nodes), self.memory_dimension + self.num_node_features))
+        for i, node in enumerate(nodes):
+            memory_features = self.memory.get_node_memory(node)
+            node_feature = self.node_features[node] if node in self.node_features else torch.zeros(
+                self.num_node_features)
+            concat_features = torch.concat((memory_features, node_feature)).reshape(
+                (1, self.memory_dimension + self.num_node_features))
+            node_features[i][:] = concat_features
+        return node_features
+
+    def get_0_layer_timestamp_feature(self, nodes: np.array):
+        node_features = torch.empty((len(nodes), self.memory_dimension + self.num_node_features))
+        for i, node in enumerate(nodes):
+            memory_features = self.memory.get_node_memory(node)
+            node_feature = self.node_features[node] if node in self.node_features else torch.zeros(
+                self.num_node_features)
+            concat_features = torch.concat((memory_features, node_feature)).reshape(
+                (1, self.memory_dimension + self.num_node_features))
+            node_features[i][:] = concat_features
+        return node_features
+
+    def get_0_layer_embeddings(self, num_layers: int, nodes: np.array,
+                               num_neighbors: int, timestamps: np.array):
+
+        embedding = np.empty(shape=(
+            len(nodes) * num_neighbors ** (num_layers - 1), num_neighbors,
+            self.memory_dimension + self.num_node_features))
+        edge_features = np.empty(
+            shape=(len(nodes) * num_neighbors ** num_layers, num_neighbors, self.num_edge_features))
+        time_diff = np.empty(
+            shape=(len(nodes) * num_neighbors ** num_layers, num_neighbors, 1))
+        zeroth_dim = embedding.shape[0]
+        node_block_size = zeroth_dim // len(nodes)  # how many matrices does one node occupy
+        for i, node in enumerate(nodes):
+            node_neighbors, edge_idxs, neighbors_timestamps = self.temporal_neighborhood.get_neighborhood(node,
+                                                                                                          timestamps[i],
+                                                                                                          num_neighbors,
+                                                                                                          )
+            if num_layers == 1:
+                # concat neighbors
+                embedding[i] = self.get_0_layer_node_feature(node_neighbors)
+                continue
+            prev_layer_embedding, prev_layer_edge_features, prev_layer_time_diff = self.get_0_layer_embeddings(
+                num_layers - 1,
+                node_neighbors,
+                num_neighbors,
+                np.repeat(timestamps[i], num_neighbors)
+            )
+
+            embedding[i * node_block_size:(i + 1) * node_block_size][:][:] = prev_layer_embedding
+
+        return embedding, edge_features, time_diff
+
+    def process_previous_batches(self) -> None:
 
         # dict nodeid -> List[event]
         raw_messages = self.raw_message_store.get_messages()
@@ -140,8 +239,8 @@ class TGN(nn.Module):
         self.update_memory(aggregated_messages, self.memory, self.memory_updater)
 
     def update_raw_message_store_current_batch(self, sources: np.array, destinations: np.array, timestamps: np.array,
-                                               edge_idxs: np.array, edge_features: Dict[int, np.array],
-                                               node_features: Dict[int, np.array]) -> None:
+                                               edge_idxs: np.array, edge_features: Dict[int, torch.Tensor],
+                                               node_features: Dict[int, torch.Tensor]) -> None:
 
         # node_events: Dict[int, List[Event]] = create_node_events()
         interaction_events: Dict[int, List[Event]] = self.create_interaction_events(sources=sources,
@@ -207,8 +306,8 @@ class TGN(nn.Module):
         return processed_messages_dict
 
     def create_raw_messages(self,
-                            events: Dict[int, List[Event]], memory: Memory, node_features: Dict[int, List[float]],
-                            edge_features: Dict[int, List[float]]):
+                            events: Dict[int, List[Event]], memory: Memory, node_features: Dict[int, torch.Tensor],
+                            edge_features: Dict[int, torch.Tensor]):
         raw_messages = {node: [] for node in events}
         for node in events:
             node_events = events[node]
@@ -226,7 +325,7 @@ class TGN(nn.Module):
                                               timestamp=event.timestamp,
                                               dest_memory=memory.get_node_memory(event.dest),
                                               source=node,
-                                              edge_features=torch.from_numpy(edge_features[event.edge_indx]),
+                                              edge_features=edge_features[event.edge_indx],
                                               delta_time=torch.as_tensor(
                                                   np.array(event.timestamp).astype(
                                                       'float')) - memory.get_last_node_update(
@@ -236,7 +335,7 @@ class TGN(nn.Module):
                                               timestamp=event.timestamp,
                                               dest_memory=memory.get_node_memory(event.source),
                                               source=event.dest,
-                                              edge_features=torch.from_numpy(edge_features[event.edge_indx]),
+                                              edge_features=edge_features[event.edge_indx],
                                               delta_time=torch.as_tensor(
                                                   np.array(event.timestamp).astype(
                                                       'float')) - memory.get_last_node_update(
@@ -256,7 +355,7 @@ class TGN(nn.Module):
             aggregated_messages[node] = aggregator_function(processed_messages[node])
         return aggregated_messages
 
-    def update_memory(self, messages, memory, memory_updater):
+    def update_memory(self, messages, memory, memory_updater) -> None:
         # todo change to do all updates at once
         for node in messages:
             updated_memory = memory_updater((messages[node], memory.get_node_memory(node)))
@@ -271,8 +370,14 @@ class TGNLayer(nn.Module):
     Base class for all implementations
     """
 
-    def __init__(self):
+    def __init__(self, temporal_neighborhood: TemporalNeighborhood, embedding_dimension: int, edge_feature_dim: int,
+                 time_encoding_dim: int, layer: int):
         super().__init__()
+        self.temporal_neighborhood = temporal_neighborhood
+        self.embedding_dimension = embedding_dimension
+        self.edge_feature_dim = edge_feature_dim
+        self.time_encoding_dim = time_encoding_dim
+        self.layer = layer
 
 
 class TGNLayerGraphSumEmbedding(TGNLayer):
@@ -280,12 +385,16 @@ class TGNLayerGraphSumEmbedding(TGNLayer):
     TGN layer implementation inspired by official TGN implementation
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, temporal_neighborhood: TemporalNeighborhood, embedding_dimension: int, edge_feature_dim: int,
+                 time_encoding_dim: int, layer: int):
+        super().__init__(temporal_neighborhood, embedding_dimension, edge_feature_dim, time_encoding_dim, layer)
+        # Initialize W1 matrix and W2 matrix
 
-    def forward(self, data):
+    def forward(self, data: Tuple[torch.Tensor, Memory, np.ndarray, np.ndarray]):
         print("hi from TGN Graph Sum Embedding layer")
-        return data
+        embeddings, current_memory, working_nodes, timestamps = data
+
+        return (embeddings, current_memory, working_nodes, timestamps)
 
 
 class TGNLayerGraphAttentionEmbedding(TGNLayer):
