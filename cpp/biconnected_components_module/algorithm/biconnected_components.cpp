cb2b1a5fa67c10ebb18e2c79d1585693eeaa1993
antepusic <ante.pusic@fer.hr>
2022-03-15 08:58:12 +0100

    Add extended biconnected components algorithm

--- cpp/biconnected_components_module/algorithm/biconnected_components.cpp
+++ cpp/biconnected_components_module/algorithm/biconnected_components.cpp
@@ -1,8 +1,24 @@
 #include "biconnected_components.hpp"
 
+bcc_utility::NodeState::NodeState(std::uint64_t number_of_nodes) {
+  visited.reserve(number_of_nodes);
+  discovery.reserve(number_of_nodes);
+  low_link.reserve(number_of_nodes);
+  counter = 0;
+}
+
+void bcc_utility::NodeState::Update(std::uint64_t node_id) {
+  counter++;
+  visited[node_id] = true;
+  discovery[node_id] = counter;
+  low_link[node_id] = counter;
+}
+
 void bcc_utility::BccDFS(std::uint64_t node_id, std::uint64_t parent_id, bcc_utility::NodeState *state,
-                         std::stack<mg_graph::Edge<>> *edge_stack, std::vector<std::vector<mg_graph::Edge<>>> *bcc,
-                         const mg_graph::GraphView<> &graph) {
+                         std::stack<mg_graph::Edge<>> *edge_stack,
+                         std::vector<std::vector<mg_graph::Edge<>>> *bcc_edges,
+                         std::vector<std::unordered_set<std::uint64_t>> *bcc_nodes, const mg_graph::GraphView<> &graph,
+                         std::unordered_set<uint64_t> &articulationPoints) {
   auto root = node_id == parent_id;
 
   state->Update(node_id);
@@ -23,47 +39,58 @@ void bcc_utility::BccDFS(std::uint64_t node_id, std::uint64_t parent_id, bcc_uti
 
     ++root_count;
     edge_stack->push(edge);
-    BccDFS(next_id, node_id, state, edge_stack, bcc, graph);
+    BccDFS(next_id, node_id, state, edge_stack, bcc_edges, bcc_nodes, graph, articulationPoints);
     state->low_link[node_id] = std::min(state->low_link[node_id], state->low_link[next_id]);
 
     // Articulation point check
     if (((root && root_count > 1) ||  // special case for root
          (!root && state->low_link[next_id] >= state->discovery[node_id])) &&
         !edge_stack->empty()) {
-      bcc->emplace_back();
+      // get articulation point
+      articulationPoints.insert(node_id);
+
+      bcc_edges->emplace_back();
+      bcc_nodes->emplace_back();
       while (edge_stack->top().id != edge.id) {
-        bcc->back().push_back(edge_stack->top());
+        bcc_edges->back().push_back(edge_stack->top());
+        bcc_nodes->back().insert(edge_stack->top().from);
+        bcc_nodes->back().insert(edge_stack->top().to);
         edge_stack->pop();
       }
-      bcc->back().push_back(edge_stack->top());
+      bcc_edges->back().push_back(edge_stack->top());
+      bcc_nodes->back().insert(edge_stack->top().from);
+      bcc_nodes->back().insert(edge_stack->top().to);
       edge_stack->pop();
     }
   }
 }
 
-std::vector<std::vector<mg_graph::Edge<>>> bcc_algorithm::GetBiconnectedComponents(const mg_graph::GraphView<> &graph) {
+std::vector<std::vector<mg_graph::Edge<>>> bcc_algorithm::GetBiconnectedComponents(
+    const mg_graph::GraphView<> &graph, std::unordered_set<uint64_t> &articulationPoints,
+    std::vector<std::unordered_set<std::uint64_t>> &bcc_nodes) {
   auto number_of_nodes = graph.Nodes().size();
   bcc_utility::NodeState state(number_of_nodes);
 
-  std::vector<std::vector<mg_graph::Edge<>>> bcc;
+  std::vector<std::vector<mg_graph::Edge<>>> bcc_edges;
   std::stack<mg_graph::Edge<>> edge_stack;
 
   for (const auto &node : graph.Nodes()) {
-    if (state.visited[node.id]) {
-      continue;
-    }
+    if (state.visited[node.id]) continue;
 
-    BccDFS(node.id, node.id, &state, &edge_stack, &bcc, graph);
+    BccDFS(node.id, node.id, &state, &edge_stack, &bcc_edges, &bcc_nodes, graph, articulationPoints);
 
     // Any edges left on stack form a BCC
     if (!edge_stack.empty()) {
-      bcc.emplace_back();
+      bcc_edges.emplace_back();
+      bcc_nodes.emplace_back();
       while (!edge_stack.empty()) {
-        bcc.back().push_back(edge_stack.top());
+        bcc_edges.back().push_back(edge_stack.top());
+        bcc_nodes.back().insert(edge_stack.top().from);
+        bcc_nodes.back().insert(edge_stack.top().to);
         edge_stack.pop();
       }
     }
   }
 
-  return bcc;
+  return bcc_edges;
 }
