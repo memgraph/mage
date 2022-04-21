a217025a54dfdf986d47c14acc79a557e5670efe
jmatak <josip.matak@memgraph.io>
2022-04-21 11:42:25 +0200

    Update utility for weighted graphs

--- cpp/cugraph_module/mg_cugraph_utility.hpp
+++ cpp/cugraph_module/mg_cugraph_utility.hpp
@@ -21,10 +21,15 @@ auto CreateCugraphFromMemgraph(const mg_graph::GraphView<> &mg_graph, raft::hand
   mg_src.reserve(mg_edges.size());
   std::vector<TVertexT> mg_dst;
   mg_dst.reserve(mg_edges.size());
+  std::vector<TWeightT> mg_weight;
+  mg_weight.reserve(mg_edges.size());
+
   std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_src),
                  [](const auto &edge) -> TVertexT { return edge.from; });
   std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_dst),
                  [](const auto &edge) -> TVertexT { return edge.to; });
+  std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_weight),
+                 [mg_graph](const auto &edge) -> TWeightT { return mg_graph.GetWeight(edge.id); });
 
   // Synchronize the data structures to the GPU
   auto stream = handle.get_stream();
@@ -32,6 +37,8 @@ auto CreateCugraphFromMemgraph(const mg_graph::GraphView<> &mg_graph, raft::hand
   raft::update_device(cu_src.data(), mg_src.data(), mg_src.size(), stream);
   rmm::device_uvector<TVertexT> cu_dst(mg_dst.size(), stream);
   raft::update_device(cu_dst.data(), mg_dst.data(), mg_dst.size(), stream);
+  rmm::device_uvector<TWeightT> cu_weight(mg_weight.size(), stream);
+  raft::update_device(cu_weight.data(), mg_weight.data(), mg_weight.size(), stream);
 
   // TODO: Deal_with/pass edge weights to CuGraph graph.
   // TODO: Allow for multigraphs
@@ -39,7 +46,7 @@ auto CreateCugraphFromMemgraph(const mg_graph::GraphView<> &mg_graph, raft::hand
   // NOTE: Renumbering is not required because graph coming from Memgraph is already correctly numbered.
   std::tie(cu_graph, std::ignore) =
       cugraph::create_graph_from_edgelist<TVertexT, TEdgeT, TWeightT, TStoreTransposed, TMultiGPU>(
-          handle, std::nullopt, std::move(cu_src), std::move(cu_dst), std::nullopt,
+          handle, std::nullopt, std::move(cu_src), std::move(cu_dst), std::move(cu_weight),
           cugraph::graph_properties_t{false, false}, false, false);
   stream.synchronize_no_throw();
 
@@ -52,15 +59,22 @@ auto CreateCugraphLegacyFromMemgraph(const mg_graph::GraphView<> &mg_graph, raft
   const auto n_edges = mg_edges.size();
   const auto n_vertices = mg_graph.Nodes().size();
 
+  const auto &mg_edges = mg_graph.Edges();
+
   // Flatten the data vector
   std::vector<TVertexT> mg_src;
   mg_src.reserve(mg_edges.size());
   std::vector<TVertexT> mg_dst;
   mg_dst.reserve(mg_edges.size());
+  std::vector<TWeightT> mg_weight;
+  mg_weight.reserve(mg_edges.size());
+
   std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_src),
                  [](const auto &edge) -> TVertexT { return edge.from; });
   std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_dst),
                  [](const auto &edge) -> TVertexT { return edge.to; });
+  std::transform(mg_edges.begin(), mg_edges.end(), std::back_inserter(mg_weight),
+                 [mg_graph](const auto &edge) -> TWeightT { return mg_graph.GetWeight(edge.id); });
 
   // Synchronize the data structures to the GPU
   auto stream = handle.get_stream();
@@ -68,9 +82,11 @@ auto CreateCugraphLegacyFromMemgraph(const mg_graph::GraphView<> &mg_graph, raft
   raft::update_device(cu_src.data(), mg_src.data(), mg_src.size(), stream);
   rmm::device_uvector<TVertexT> cu_dst(mg_dst.size(), stream);
   raft::update_device(cu_dst.data(), mg_dst.data(), mg_dst.size(), stream);
+  rmm::device_uvector<TWeightT> cu_weight(mg_weight.size(), stream);
+  raft::update_device(cu_weight.data(), mg_weight.data(), mg_weight.size(), stream);
 
   cugraph::legacy::GraphCOOView<TVertexT, TEdgeT, TWeightT> cooview(
-      cu_src.data(), cu_dst.data(), nullptr, static_cast<TVertexT>(n_vertices), static_cast<TEdgeT>(n_edges));
+      cu_src.data(), cu_dst.data(), mg_weight.data(), static_cast<TVertexT>(n_vertices), static_cast<TEdgeT>(n_edges));
 
   return cugraph::coo_to_csr<TVertexT, TEdgeT, TWeightT>(cooview);
 }
