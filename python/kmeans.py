9bad0847b7f0e67a93f570cc2ff8d1ad1447e063
antoniofilipovic <filipovicantonio1998@gmail.com>
2022-05-24 18:07:13 +0200

    refactor kmeans

--- python/kmeans.py
+++ python/kmeans.py
@@ -1,3 +1,9 @@
+"""
+Purpose of this query module is to offer easy kmeans clustering algorithm on top of the embeddings that you
+might have stored in nodes. All you need to do is call kmeans.get_clusters(5, "embedding") where 5
+represents number of clusters you want to get, and "embedding" represents node property name in which
+embedding of node is stored
+"""
 from sklearn.cluster import KMeans
 from typing import List, Tuple
 
@@ -12,21 +18,31 @@ def get_created_clusters(number_of_clusters: int, embeddings: List[List[float]],
 
 @mgp.write_proc
 def get_clusters(
-        ctx: mgp.ProcCtx, nodes: mgp.List[mgp.Vertex], embeddings: mgp.List[mgp.List[mgp.Number]],
-        number_of_groups: mgp.Number) -> mgp.Record(vertex=mgp.Vertex, cluster=mgp.Number):
-    nodes_labels_list = get_created_clusters(number_of_groups, embeddings, nodes)
-    return [mgp.Record(vertex=vertex, label=label) for vertex, label in nodes_labels_list]
+        ctx: mgp.ProcCtx, number_of_groups: mgp.Number, embedding_property: str = "embedding") -> mgp.Record(
+    vertex=mgp.Vertex, cluster=mgp.Number):
+    nodes = []
+    embeddings = []
+    for node in ctx.graph.vertices:
+        nodes.append(node)
+        embeddings.append(node.properties.get(embedding_property))
 
+    nodes_labels_list = get_created_clusters(number_of_groups, embeddings, nodes)
+    return [mgp.Record(vertex=vertex, cluster=int(label)) for vertex, label in nodes_labels_list]
 
 
 @mgp.write_proc
 def set_clusters(
-        ctx: mgp.ProcCtx, nodes: mgp.List[mgp.Vertex], embeddings: mgp.List[mgp.List[mgp.Number]],
-        number_of_groups: mgp.Number, label_property="label") -> mgp.Record():
- 
+        ctx: mgp.ProcCtx, number_of_groups: mgp.Number, embedding_property: str = "embedding",
+        label_property="label") -> mgp.Record( vertex=mgp.Vertex, cluster=mgp.Number):
+    nodes = []
+    embeddings = []
+    for node in ctx.graph.vertices:
+        nodes.append(node)
+        embeddings.append(node.properties.get(embedding_property))
+
     nodes_labels_list = get_created_clusters(number_of_groups, embeddings, nodes)
 
     for vertex, label in nodes_labels_list:
         vertex.properties.set(label_property, int(label))
 
-    return mgp.Record()
+    return [mgp.Record(vertex=vertex, cluster=int(label)) for vertex, label in nodes_labels_list]
