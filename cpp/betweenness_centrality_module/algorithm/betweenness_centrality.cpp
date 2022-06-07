8c02e8853f5c0be4fe69e6f46eedb6da78b8cff3
antoniofilipovic <filipovicantonio1998@gmail.com>
2022-06-07 18:10:40 +0200

    Revert "temporarely remove betweenness centrality module"
    This reverts commit e4b774abe182b319370a057116633d96cb0269c1.

---
+++ cpp/betweenness_centrality_module/algorithm/betweenness_centrality.cpp
@@ -0,0 +1,114 @@
+#include <omp.h>
+#include <queue>
+#include <stack>
+#include <vector>
+
+#include "betweenness_centrality.hpp"
+
+namespace betweenness_centrality_util {
+
+void BFS(const std::uint64_t source_node, const mg_graph::GraphView<> &graph, std::stack<std::uint64_t> &visited,
+         std::vector<std::vector<std::uint64_t>> &predecessors, std::vector<std::uint64_t> &shortest_paths_counter) {
+  auto number_of_nodes = graph.Nodes().size();
+
+  // -1 to indicate that node is not visited
+  std::vector<int> distance(number_of_nodes, -1);
+
+  shortest_paths_counter[source_node] = 1;
+  distance[source_node] = 0;
+
+  std::queue<std::uint64_t> BFS_queue;
+  BFS_queue.push(source_node);
+
+  while (!BFS_queue.empty()) {
+    auto current_node_id = BFS_queue.front();
+    BFS_queue.pop();
+    visited.push(current_node_id);
+
+    for (auto neighbor : graph.Neighbours(current_node_id)) {
+      auto neighbor_id = neighbor.node_id;
+
+      // node found for the first time
+      if (distance[neighbor_id] < 0) {
+        BFS_queue.push(neighbor_id);
+        distance[neighbor_id] = distance[current_node_id] + 1;
+      }
+
+      // shortest path from node to neighbor_id goes through current_node
+      if (distance[neighbor_id] == distance[current_node_id] + 1) {
+        shortest_paths_counter[neighbor_id] += shortest_paths_counter[current_node_id];
+        predecessors[neighbor_id].emplace_back(current_node_id);
+      }
+    }
+  }
+}
+}  // namespace betweenness_centrality_util
+
+namespace {
+///
+///@brief An in-place method that normalizes a vector by multiplying each component by a given constant.
+///
+///@param vec The vector that should be normalized
+///@param constant The constant with which the components of a vector are multiplied
+///
+void Normalize(std::vector<double> &vec, double constant) {
+  for (auto &value : vec) value *= constant;
+}
+}  // namespace
+
+namespace betweenness_centrality_alg {
+
+std::vector<double> BetweennessCentrality(const mg_graph::GraphView<> &graph, bool directed, bool normalize,
+                                          int threads) {
+  auto number_of_nodes = graph.Nodes().size();
+  std::vector<double> betweenness_centrality(number_of_nodes, 0);
+
+  // perform bfs for every node in the graph
+  omp_set_dynamic(0);
+  omp_set_num_threads(threads);
+#pragma omp parallel for
+  for (std::uint64_t node_id = 0; node_id < number_of_nodes; node_id++) {
+    // data structures used in BFS
+    std::stack<std::uint64_t> visited;
+    std::vector<std::vector<std::uint64_t>> predecessors(number_of_nodes, std::vector<std::uint64_t>());
+    std::vector<std::uint64_t> shortest_paths_counter(number_of_nodes, 0);
+    betweenness_centrality_util::BFS(node_id, graph, visited, predecessors, shortest_paths_counter);
+
+    std::vector<double> dependency(number_of_nodes, 0);
+
+    while (!visited.empty()) {
+      auto current_node = visited.top();
+      visited.pop();
+
+      for (auto p : predecessors[current_node]) {
+        double fraction = static_cast<double>(shortest_paths_counter[p]) / shortest_paths_counter[current_node];
+        dependency[p] += fraction * (1 + dependency[current_node]);
+      }
+
+      if (current_node != node_id) {
+        if (directed) {
+#pragma omp atomic update
+          betweenness_centrality[current_node] += dependency[current_node];
+        }
+        // centrality scores need to be divided by two since all shortest paths are considered twice
+        else {
+#pragma omp atomic update
+          betweenness_centrality[current_node] += dependency[current_node] / 2.0;
+        }
+      }
+    }
+  }
+
+  if (normalize) {
+    // normalized by dividing the value by the number of pairs of nodes
+    // not including the node whose value we normalize
+    auto number_of_pairs = (number_of_nodes - 1) * (number_of_nodes - 2);
+    const auto numerator = directed ? 1.0 : 2.0;
+    double constant = number_of_nodes > 2 ? numerator / number_of_pairs : 1.0;
+    Normalize(betweenness_centrality, constant);
+  }
+
+  return betweenness_centrality;
+}
+
+}  // namespace betweenness_centrality_alg
---
+++ cpp/betweenness_centrality_module/betweenness_centrality_module.cpp
@@ -0,0 +1,95 @@
+#include <thread>
+
+#include <mg_utils.hpp>
+
+#include "algorithm/betweenness_centrality.hpp"
+
+namespace {
+
+constexpr char const *kProcedureGet = "get";
+
+constexpr char const *kFieldBCScore = "betweenness_centrality";
+constexpr char const *kFieldNode = "node";
+
+constexpr char const *kArgumentDirected = "directed";
+constexpr char const *kArgumentNormalized = "normalized";
+constexpr char const *kArgumentThreads = "threads";
+
+void InsertBCRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const double betweenness_centrality,
+                    const int node_id) {
+  auto *record = mgp::result_new_record(result);
+
+  mg_utility::InsertNodeValueResult(graph, record, kFieldNode, node_id, memory);
+  mg_utility::InsertDoubleValueResult(record, kFieldBCScore, betweenness_centrality, memory);
+}
+
+void GetBetweennessCentrality(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  try {
+    auto directed = mgp::value_get_bool(mgp::list_at(args, 0));
+    auto normalize = mgp::value_get_bool(mgp::list_at(args, 1));
+    auto threads = mgp::value_get_int(mgp::list_at(args, 2));
+
+    if (threads <= 0) threads = std::thread::hardware_concurrency();
+
+    auto graph_type = directed ? mg_graph::GraphType::kDirectedGraph : mg_graph::GraphType::kUndirectedGraph;
+
+    auto graph = mg_utility::GetGraphView(memgraph_graph, result, memory, graph_type);
+    auto BC = betweenness_centrality_alg::BetweennessCentrality(*graph, directed, normalize, threads);
+
+    auto number_of_nodes = graph->Nodes().size();
+    for (std::uint64_t node_id = 0; node_id < number_of_nodes; ++node_id)
+      InsertBCRecord(memgraph_graph, result, memory, BC[node_id], graph->GetMemgraphNodeId(node_id));
+
+  } catch (const std::exception &e) {
+    // We must not let any exceptions out of our module.
+    mgp::result_set_error_msg(result, e.what());
+    return;
+  }
+}
+}  // namespace
+
+// Each module needs to define mgp_init_module function.
+// Here you can register multiple procedures your module supports.
+extern "C" int mgp_init_module(mgp_module *module, mgp_memory *memory) {
+  mgp_value *bool_value_directed;
+  mgp_value *bool_value_normalized;
+  mgp_value *int_value_threads;
+
+  try {
+    auto *proc = mgp::module_add_read_procedure(module, kProcedureGet, GetBetweennessCentrality);
+
+    // Query module arguments
+    bool_value_directed = mgp::value_make_bool(true, memory);
+    bool_value_normalized = mgp::value_make_bool(true, memory);
+    int_value_threads = mgp::value_make_int(std::thread::hardware_concurrency(), memory);
+
+    mgp::proc_add_opt_arg(proc, kArgumentDirected, mgp::type_bool(), bool_value_directed);
+    mgp::proc_add_opt_arg(proc, kArgumentNormalized, mgp::type_bool(), bool_value_normalized);
+    mgp::proc_add_opt_arg(proc, kArgumentThreads, mgp::type_int(), int_value_threads);
+
+    // Query module output record
+    mgp::proc_add_result(proc, kFieldNode, mgp::type_node());
+    mgp::proc_add_result(proc, kFieldBCScore, mgp::type_float());
+
+  } catch (const std::exception &e) {
+    // Destroy the values if exception occurs
+    mgp_value_destroy(bool_value_directed);
+    mgp_value_destroy(bool_value_normalized);
+    mgp_value_destroy(int_value_threads);
+    return 1;
+  }
+
+  mgp_value_destroy(bool_value_directed);
+  mgp_value_destroy(bool_value_normalized);
+  mgp_value_destroy(int_value_threads);
+
+  return 0;
+}
+
+// This is an optional function if you need to release any resources before the
+// module is unloaded. You will probably need this if you acquired some
+// resources in mgp_init_module.
+extern "C" int mgp_shutdown_module() {
+  // Return 0 to indicate success.
+  return 0;
+}
