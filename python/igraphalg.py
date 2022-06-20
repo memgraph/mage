7e2902ada01e94b26a0ddc276f253008431d29d0
niko4299 <niko.krvavica@gmail.com>
2022-06-20 13:42:56 +0200

    need to add test for min_cut and simple_shortest_paths

--- python/igraphalg.py
+++ python/igraphalg.py
@@ -1,7 +1,8 @@
+from asyncio import unix_events
 import igraph
 import mgp
 from collections import defaultdict
-from typing import List
+from typing import List, Tuple, Dict
 
 
 @mgp.read_proc
@@ -12,8 +13,8 @@ def get_flow(
     edge_property: str = "weight",
 ) -> mgp.Record(max_flow=mgp.Number):
 
-    graph = create_igraph_from_ctx(ctx, directed=True)
-    max_flow = graph.maxflow(start_v.id, end_v.id, capacity=edge_property)
+    graph, id_mapping = create_igraph_from_ctx(ctx, directed=True)
+    max_flow = graph.maxflow(id_mapping[start_v.id], id_mapping[end_v.id], capacity=edge_property)
 
     return mgp.Record(max_flow=max_flow.value)
 
@@ -24,13 +25,13 @@ def pagerank(
     damping: mgp.Number = 0.85,
     max_iter: int = 100,
     tol: mgp.Number = 1e-06,
-    weight: mgp.Nullable[str] = "weight",
-) -> mgp.Record(node=mgp.Vertex, rank=float):
+    weight: mgp.Nullable[str] = None,
+) -> mgp.Record(node_id=int, rank=float):
 
-    graph = create_igraph_from_ctx(ctx, directed=False)
-    pg = graph.pagerank(weights=weight, niter=max_iter, damping=damping, eps=tol)
+    graph, _ = create_igraph_from_ctx(ctx, directed=True)
+    pg = graph.pagerank(weights=weight, directed=True, niter=max_iter, damping=damping, eps=tol)
 
-    return [mgp.Record(node=k, rank=v) for k, v in enumerate(pg)]
+    return [mgp.Record(node_id=node_id, rank=rank) for node_id, rank in enumerate(pg)]
 
 
 @mgp.read_proc
@@ -40,66 +41,94 @@ def all_simple_paths(
     target: mgp.Vertex,
     cutoff: mgp.Nullable[int] = None,
 ) -> mgp.Record(paths=mgp.List[mgp.List[mgp.Vertex]]):
-    graph = create_igraph_from_ctx(ctx, directed=True)
+    graph, id_mapping = create_igraph_from_ctx(ctx, directed=True)
 
     return mgp.Record(
-        paths=list(graph.get_all_simple_paths(v=source.id, to=target.id, cutoff=cutoff))
+        paths=list(graph.get_all_simple_paths(v=id_mapping[source.id], to=id_mapping[target.id], cutoff=cutoff))
     )
 
 
 @mgp.read_proc
-def min_cuts(
+def min_cut(
     ctx: mgp.ProcCtx,
     source: mgp.Vertex,
     target: mgp.Vertex,
-    edge_property: str = "weights"
-) -> mgp.Record(partition=List[mgp.Vertex], cut=List[mgp.Edge], value=float):
-    graph = create_igraph_from_ctx(ctx, directed=True)
-    mincut = graph.mincut(source=source.id, target=target.id, capacity=edge_property)
+    edge_property: mgp.Nullable[int] = None
+) -> mgp.Record(partition_vertex_ids=List[int], cut_edge_vertex_ids=List[int], value=float):
+    graph, id_mapping = create_igraph_from_ctx(ctx, directed=True)
+    mincut = graph.mincut(source=id_mapping[source.id], target=id_mapping[target.id], capacity=edge_property)
 
-    return mgp.Record(partition=mincut.partition, cut=mincut.cut, value=mincut.value)
+    return mgp.Record(partition_vertex_ids=mincut.partition, cut_edge_vertex_ids=mincut.cut, value=mincut.value)
 
 
 @mgp.read_proc
-def community_leiden(ctx: mgp.ProcCtx, edge_property: str = "weigths",
+def community_leiden(ctx: mgp.ProcCtx, edge_property: mgp.Nullable[str] = None,
                      resolution_parameter: float = 0.6,
                      number_of_iterations: int = -1,
-                     ) -> mgp.Record(community_index=int, community_members=List[mgp.Vertex]):
-    graph = create_igraph_from_ctx(ctx, directed=True)
+                     ) -> mgp.Record(community_id=int, community_members=List[int]):
+    graph, _ = create_igraph_from_ctx(ctx, directed=False)
+
     communities = graph.community_leiden(resolution_parameter=resolution_parameter, weights=edge_property, n_iterations=number_of_iterations)
 
-    return [mgp.Record(community_index=i, community_members=members) for i, members in enumerate(communities)]
+    return [mgp.Record(community_id=i, community_members=members) for i, members in enumerate(communities)]
 
 
 @mgp.read_proc
 def spanning_tree(ctx: mgp.ProcCtx,
-                  edge_property: str = "weigths"
-                  ) -> mgp.Record(tree=List[mgp.Vertex]):
-    graph = create_igraph_from_ctx(ctx, directed=True)
+                  edge_property: mgp.Nullable[str] = None,
+                  directed : bool = True) -> mgp.Record(tree=List[List[int]]):
+    graph, id_mapping = create_igraph_from_ctx(ctx, directed=directed)
+    edge_weights = None
+    if edge_property:
+        edge_weights = graph.es[edge_property]
 
-    return mgp.Record(tree=graph.spanning_tree(edge_property, return_tree=False))
+    min_span_tree_graph = graph.spanning_tree(weights=edge_weights)
+    min_span_tree = get_min_span_tree_vertex_pairs(ctx=ctx, id_mapping=id_mapping, min_span_tree_graph=min_span_tree_graph)
 
-def create_igraph_from_ctx(ctx: mgp.ProcCtx, directed: bool = False) -> igraph.Graph:
+    return mgp.Record(tree=min_span_tree)
+
+
+def get_min_span_tree_vertex_pairs(ctx: mgp.ProcCtx, id_mapping: Dict[int, int], min_span_tree_graph: igraph.Graph) -> List[List[int]]:
+    unique_edges_dict = dict()
+    not_isolated_vertex_directed = dict()
+    min_span_tree = []
+    for vertex in ctx.graph.vertices:
+        isolated = True
+        for neighbor in min_span_tree_graph.neighbors(id_mapping[vertex.id]):
+            if (id_mapping[vertex.id],neighbor) not in unique_edges_dict and (neighbor,id_mapping[vertex.id]) not in unique_edges_dict:
+                min_span_tree.append([id_mapping[vertex.id], neighbor])
+                unique_edges_dict[(neighbor, id_mapping[vertex.id])] = True
+                isolated = False
+                not_isolated_vertex_directed[neighbor] = 1
+
+        if isolated and id_mapping[vertex.id] not in not_isolated_vertex_directed:
+            min_span_tree.append([id_mapping[vertex.id]])
+
+    return min_span_tree
+
+
+def create_igraph_from_ctx(ctx: mgp.ProcCtx, directed: bool = False) -> Tuple[igraph.Graph, Dict[int, int]]:
     vertex_attrs = defaultdict(list)
     edge_list = []
     edge_attrs = defaultdict(list)
+    id_mapping = {vertex.id: i for i, vertex in enumerate(ctx.graph.vertices)}
     for vertex in ctx.graph.vertices:
         for name, value in vertex.properties.items():
             vertex_attrs[name].append(value)
         for edge in vertex.out_edges:
             for name, value in edge.properties.items():
                 edge_attrs[name].append(value)
-            edge_list.append((edge.from_vertex.id, edge.to_vertex.id))
+            edge_list.append((id_mapping[edge.from_vertex.id], id_mapping[edge.to_vertex.id]))
 
     graph = igraph.Graph(
         directed=directed,
-        n=len(vertex_attrs),
+        n=len(ctx.graph.vertices),
         edges=edge_list,
         edge_attrs=edge_attrs,
         vertex_attrs=vertex_attrs,
     )
 
-    return graph
+    return graph, id_mapping
 
 
 def create_igraph_from_matrix(
