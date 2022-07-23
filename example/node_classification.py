9e155b0d5acb8f13b775088cc01d58b34831c07f
mdujic <mdujic212@gmail.com>
2022-07-23 20:20:11 +0200

    organized program to load, train, test, and implemented randomization of masks

--- example/node_classification.py
+++ example/node_classification.py
@@ -6,6 +6,7 @@ from torch_geometric.transforms import NormalizeFeatures
 import mgp
 import numpy as np
 from tqdm import tqdm
+import random
 
 class Data(object):
     def __init__(self, x, y, edge_index, train_mask, val_mask, test_mask):
@@ -35,7 +36,7 @@ class GCN(torch.nn.Module):
 
 
 
-def train(data):
+def train_model():
     model.train()
     optimizer.zero_grad()  # Clear gradients.
     out = model(data.x, data.edge_index)  # Perform a single forward pass.
@@ -46,7 +47,7 @@ def train(data):
 
 
 
-def test(data, mask):
+def accuracy(mask):
     model.eval()
     out = model(data.x, data.edge_index)
     pred = out.argmax(dim=1)  # Use the class with highest probability.
@@ -54,27 +55,59 @@ def test(data, mask):
     acc = int(correct.sum()) / int(mask.sum())  # Derive ratio of correct predictions.
     return acc
 
-def convert_data(nodes, edges):
-    if len(nodes) == 0:
-        return "Insert data in database first!"
-    x = np.zeros((len(nodes),len(nodes[0].properties.get("feature"))))
+def convert_data(nodes, edges, train_ratio, val_ratio, test_ratio):
+    
+    assert len(nodes) > 0, "dataset is empty"
+    
+    x = np.zeros((len(nodes),len(nodes[0].properties.get("features"))))
     y = np.zeros((len(nodes)))
     edge_index = np.zeros((2,len(edges)))
     train_mask = np.zeros((len(nodes)))
     val_mask = np.zeros((len(nodes)))
     test_mask = np.zeros((len(nodes)))
     
+    masks = np.zeros((len(nodes)))
+    
+    
+    # reindexing = {} # for unordered ids
+    # for i in range(len(nodes)): 
+    #     reindexing[nodes[i].properties.get("id")] = i
+
+
+    assert train_ratio > 0 and val_ratio > 0 and test_ratio > 0, "ratios are positive numbers in [0,1]"
+    assert train_ratio + val_ratio + test_ratio == 1, "data ratios don't add up to 100%"
+
+
+    for i in range(len(nodes)):
+        if i < train_ratio * len(nodes):
+            masks[i] = 1
+        elif i >= train_ratio * len(nodes) and i < (train_ratio + val_ratio) * len(nodes):
+            masks[i] = 2
+        else:
+            masks[i] = 3
+
+    random.shuffle(masks) # this way we have randomized 80%/10%/10% train/valuation/test data
+
     print("Structuring data:")
     for i in tqdm(range(len(nodes))):
-        x[i] = np.add(x[i], np.array(nodes[i].properties.get("feature")))
-        y[i] += nodes[i].properties.get("class")
-        train_mask[i] += nodes[i].properties.get("train_mask")
-        val_mask[i] += nodes[i].properties.get("val_mask")
-        test_mask[i] += nodes[i].properties.get("test_mask")
+        x[i] = np.add(x[i], np.array(nodes[i].properties.get("features")))
+        # y[i] = random.randint(0, num_of_classes - 1) 
+        # # because of homophility, we cannot initialize classes randomly
+        # label for class is missing for temporary CORA dataset in Memgraph lab
+
+        y[i] = nodes[i].properties.get("class")
+
+        if masks[i] == 1:
+            train_mask[i] = 1
+        elif masks[i] == 2:
+            val_mask[i] = 1
+        else:
+            test_mask[i] = 1
 
     for i in tqdm(range(len(edges))):
-        edge_index[0][i] = edges[i].properties.get("first")
-        edge_index[1][i] = edges[i].properties.get("second")
+        edge_index[0][i] = edges[i].from_vertex.properties.get("id")
+        edge_index[1][i] = edges[i].to_vertex.properties.get("id")
+        
     print("Finished converting data.")
 
     # print("=============================")
@@ -98,34 +131,73 @@ def convert_data(nodes, edges):
     return data
 
 
+model = GCN(hidden_channels=16)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+criterion = torch.nn.CrossEntropyLoss()
+total_no_epoches = 0
+
 @mgp.read_proc
-def work(
-    ctx: mgp.ProcCtx
+def set_parameters(
+    hidden_channels: int,
+    lr: float,
+    weight_decay: float
+) -> mgp.Record(hidden_channels=int, lr=float, weight_decay=float):
+    """
+    set parameters of model and optimizer
+
+    optimizer is Adam, criterion is cross entropy loss
+    """
+    model.hidden_channels = hidden_channels
+    optimizer.lr = lr
+    optimizer.weight_decay = weight_decay
+
+    return mgp.Record(hidden_channels=model.hidden_channels, lr=optimizer.lr, weight_decay=optimizer.weight_decay)
+
+@mgp.read_proc
+def load_data(
+    ctx: mgp.ProcCtx,
+    train_ratio: float = 0.8,
+    val_ratio: float = 0.1,
+    test_ratio: float = 0.1
 ) -> mgp.Record(no_nodes=int, no_edges=int):
-    
+    """
+    loading data, must be executed before training
+    """
+
     nodes = list(iter(ctx.graph.vertices))
     edges = []
     for vertex in ctx.graph.vertices:
         for edge in vertex.out_edges:
             edges.append(edge)
     
-    data = convert_data(nodes, edges)
-    if data == "Insert data in database first!":
-        print(data)
-        return mgp.Record(len_nodes=len(nodes), len_edges=len(edges))
-    
-    global model 
-    model = GCN(hidden_channels=16)
-    global optimizer 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
-    global criterion 
-    criterion = torch.nn.CrossEntropyLoss()
-    
-    for epoch in range(1, 101):
-        loss = train(data)
-        print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}')
-    
-    test_acc = test(data, data.test_mask)
-    print(f'Test Accuracy: {test_acc:.4f}')
-    
+    global data
+    data = convert_data(nodes, edges, train_ratio, val_ratio, test_ratio)
+
     return mgp.Record(no_nodes=len(nodes), no_edges=len(edges))
+
+
+@mgp.read_proc
+def train(
+    no_epoches: int = 100
+) -> mgp.Record(total_epoches=int, validation=float):
+    """
+    training
+    """
+
+    for epoch in range(1, no_epoches+1):
+        loss = train_model()
+        val_acc = accuracy(data.val_mask)
+        print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, Validation: {val_acc:.4f}' )
+    
+    global total_no_epoches
+    total_no_epoches += no_epoches
+    return mgp.Record(total_epoches=total_no_epoches, validation=val_acc)
+
+@mgp.read_proc
+def test() -> mgp.Record(test_acc=float):
+    """
+    testing
+    """
+    test_acc = accuracy(data.test_mask)
+    
+    return mgp.Record(test_acc=test_acc)
