bd10415e5f903078ab17e4091bdca73d5abd5126
Andi Skrgat <andi.skrgat@gdi.net>
2022-07-30 10:54:48 +0200

    Added module for deleting and adding edges from memgraph

---
+++ python/link_prediction/manipulate_database.py
@@ -0,0 +1,139 @@
+from multiprocessing.connection import wait
+from operator import mod
+from platform import node
+import time
+from gqlalchemy import Memgraph, Create, Node, Relationship
+import numpy as np
+import ast
+
+
+class MemgraphPrintDict():
+    """Class that wraps dictionary by printing repr of value and key without quotes to be Cypher compatible. 
+    """
+
+    def __init__(self, my_dict) -> None:
+        self.my_dict = my_dict
+
+    
+    def __str__(self) -> str:
+        s = ""
+        items_len = len(self.my_dict.keys())
+        for i, (key, value) in enumerate(self.my_dict.items()):
+            s += key + ": " + repr(value)
+            if i != items_len - 1:
+                s += ", "
+        return s
+
+def labels_list_to_cypher(labels: list) -> str:
+    """Generates Cypher labels list representation command. 
+
+    Args:
+        labels (list): List of labels
+
+    Returns:
+        str: Cypher list representation
+    """
+    command = ""
+    for label in labels:
+        command += f":{label}"
+
+    return command
+
+def delete_edges(num_delete_edges: int, file_name: str, node_id_property: str) -> None:
+    """Deletes num_delete_edges random relationships. Creates CREATE commands and saves it to the file so later relationships can be recreated. 
+
+    Args:
+        num_delete_edges (int): Number of edges to be deleted. 
+        file_name (str): Name of the file where it should be saved. 
+        node_id_property (str): Property name where the id is saved. 
+    Returns:
+        nothing
+    """    
+
+    results = memgraph.execute_and_fetch(
+        f"""
+        MATCH (v1)-[e]->(v2) 
+        RETURN v1, e, v2
+        ORDER BY rand()
+        LIMIT {num_delete_edges};
+        """
+
+    )
+
+    create_commands = []
+    vertex_ids_delete = []
+
+    for result in results:
+        # Handle first node
+        v1_properties, v1_labels = MemgraphPrintDict(result["v1"]._properties), labels_list_to_cypher(list(result["v1"]._labels))
+        # del v1_properties.my_dict["features"]
+        vertex_ids_delete.append(v1_properties.my_dict["id"])
+
+        # Handle second node
+        v2_properties, v2_labels = MemgraphPrintDict(result["v2"]._properties), labels_list_to_cypher(list(result["v2"]._labels))
+ 
+        # del v2_properties.my_dict["features"]
+        # Handle edge
+        edge_properties, edge_labels = MemgraphPrintDict(result["e"]._properties), result["e"]._type
+
+
+        # Create command for later, and save it to some my file. 
+        create_command = f"""MATCH (v1{v1_labels} {{{node_id_property}: {v1_properties.my_dict[node_id_property]}}})
+            MATCH (v2{v2_labels} {{{node_id_property}: {v2_properties.my_dict[node_id_property]}}}) 
+            CREATE (v1)-[e:{edge_labels} {{{edge_properties}}}]->(v2)
+            RETURN v1, e, v2;
+            """
+        
+        # print(create_command)
+        create_commands.append(create_command)
+        
+        # Delete command
+        delete_command = f"""
+            MATCH (v1{v1_labels} {{{node_id_property}: {v1_properties.my_dict[node_id_property]}}})-[e]->(v2{v2_labels} {{{node_id_property}: {v2_properties.my_dict[node_id_property]}}}) 
+            DELETE e;
+        """
+        print("DELETE COMMAND: ")
+        print(delete_command)
+
+        memgraph.execute(delete_command)
+
+
+    with open(file=file_name, mode="w") as f:
+        for comm in create_commands:
+            f.write("%s\n" % comm)
+
+
+    return results
+    
+
+def read_commands_script(file_name: str) -> None:
+    """Reads commands from file name and executes them. 
+
+    Args:
+        file_name (str): _description_
+    """
+    with open(file=file_name, mode="r+") as f:
+        command = ""
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            elif line.isspace() is True: # empty line so execute command
+                print("Command: ")
+                print(command)
+                memgraph.execute(command)
+                time.sleep(5)
+                command = ""  # annulate old command
+            else:
+                command += line
+        
+        f.truncate(0)
+
+
+
+if __name__ == "__main__":
+    memgraph = Memgraph("127.0.0.1", 7687)
+    file_name = "./cora_creation_commands.txt"
+    # delete_edges(num_delete_edges=5, file_name=file_name, node_id_property="id")
+    read_commands_script(file_name=file_name)
+    
