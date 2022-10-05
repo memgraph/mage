476d1ca1612eec1ea3937fe79a84fa4f41a0270c
Andi Skrgat <andi8647@gmail.com>
2022-10-05 13:35:29 +0200

    Added update trigger, options for reindexing and scanning

--- python/elastic_search_serialization.py
+++ python/elastic_search_serialization.py
@@ -1,7 +1,7 @@
 import json
 import elasticsearch
 import mgp
-from typing import Any, List, Dict, Tuple
+from typing import Any, Collection, List, Dict, Tuple, Union
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import streaming_bulk
 from datetime import datetime
@@ -310,6 +310,23 @@ def generate_documents(
     return nodes, edges
 
 
+def get_created_updated_objects(context_objects: List[Dict[str, Any]]) -> Tuple[List[mgp.Vertex], List[mgp.Edge]]:
+    """
+    Extracts nodes and edges from context_objects.
+    Args:
+        context_objects (List[Dict[str, Any]]): Objects that are sent as parameters because of some trigger that was called. Trigger can be for update or for create.
+    Returns:
+        Tuple[List[mgp.Vertex], List[mgp.Edge]]: Extracted vertices and egdes.
+    """
+    context_vertices, context_edges = [], []
+    for context_object in context_objects:
+        if context_object[EVENT_TYPE] == CREATED_VERTEX:
+            context_vertices.append(context_object[VERTEX])
+        elif context_object[EVENT_TYPE] == CREATED_EDGE:
+            context_edges.append(context_object[EDGE])
+    return context_vertices, context_edges
+
+
 @mgp.read_proc
 def index(
     context: mgp.ProcCtx,
@@ -318,8 +335,8 @@ def index(
     ca_certs: str,
     elastic_user: str,
     elastic_password: str,
-    node_index_name: str = "node_index",
-    edge_index_name: str = "edge_index",
+    node_index: str,
+    edge_index: str,
     number_of_shards: int = 1,
     number_of_replicas: int = 1,
     analyzer: str = "lk_analyzer",
@@ -328,13 +345,13 @@ def index(
     The method serializes all vertices and relationships that are in Memgraph DB to an ElasticSearch schema.
     Args:
         context (mgp.ProcCtx): Reference to the executing context.
-        createdObjects (List[Dict[str, Any]]): List of all objects that were created and then sent as arguments to this method with the help of "create trigger".
+        createdObjects (List[Dict[str, Any]]): List of all objects that were created andthen sent as arguments to this method with the help of "create trigger".
         elastic_url (str): URL for connecting to the Elasticsearch instance.
         ca_certs (str): Path to the certificate file.
         elastic_user (str): The user trying to connect to the Elasticsearch.
         elastic_password (str): User's password for connecting to the Elasticsearch.
-        node_index_name (str): The name of the node index.
-        edge_index_name (str): The name of the edge index.
+        node_index (str): The name of the node index.
+        edge_index (str): The name of the edge index.
         number_of_shards (int): A number of shards you want to use in your index.
         number_of_replicas (int): A number of replicas you want to use in your index.
 
@@ -360,12 +377,7 @@ def index(
     # )
 
     # Created objects can be vertices and edges
-    created_vertices, created_edges = [], []
-    for createdObject in createdObjects:
-        if createdObject[EVENT_TYPE] == CREATED_VERTEX:
-            created_vertices.append(createdObject[VERTEX])
-        elif createdObject[EVENT_TYPE] == CREATED_EDGE:
-            created_edges.append(createdObject[EDGE])
+    created_vertices, created_edges = get_created_updated_objects(createdObjects)
 
     # Now create iterable of documents that need to be indexed
     nodes, edges = generate_documents(created_vertices, created_edges)
@@ -374,12 +386,60 @@ def index(
 
     # Send documents on indexing
     print("Indexing vertices...")
-    for ok, action in streaming_bulk(client=client, index=node_index_name, actions=nodes):
+    for ok, action in streaming_bulk(client=client, index=node_index, actions=nodes):
         print(f"OK: {ok} Action: {action}")
     print()
-    for ok, action in streaming_bulk(client=client, index=edge_index_name, actions=edges):
+    for ok, action in streaming_bulk(client=client, index=edge_index, actions=edges):
         print(f"OK: {ok} Action: {action}")
     return mgp.Record(nodes=nodes, edges=edges)
+
+
+@mgp.read_proc
+def reindex(
+    context: mgp.ProcCtx,
+    updatedObjects: mgp.List[mgp.Map],
+    elastic_url: str,
+    ca_certs: str,
+    elastic_user: str,
+    elastic_password: str,
+    source_index: mgp.Any,
+    target_index: str,
+    query: str,
+    chunk_size: int = 500,
+    scroll: str = "5m",
+    op_type: mgp.Nullable[str] = None,
+) -> mgp.Record(response=str):
+    """
+    Reindex all documents from one index that satisfy a given query to another, potentia    lly (if target_client is specified) on a different cluster. If you don’t specify the    query you will reindex all the documents.
+    Args:
+        context (mgp.ProcCtx): Reference to the executing context.
+        updatatedObjects (List[Dict[str, Any]]): List of all objects that were updated and then sent as arguments to this method with the help of the "update trigger".
+        elastic_url (str): URL for connecting to the Elasticsearch instance.
+        ca_certs (str): Path to the certificate file.
+        elastic_user (str): The user trying to connect to the Elasticsearch.
+        elastic_password str: User's password for connecting to the Elasticsearch.
+        source_index (Union[str, List[str]]): Identifies source index(or more of them) from where documents need to be indexed.
+        target_index (str): Identifies target index to where documents need to be indexed.
+        query (str): Query written as JSON.
+        chunk_size (int): number of docs in one chunk sent to es (default: 500).
+        scroll (str): Specify how long a consistent view of the index should be maintained for scrolled search.
+        op_type (Optional[str]): Explicit operation type. Defaults to ‘_index’. Data streams must be set to ‘create’. If not specified, will auto-detect if target_index is a data stream.
+    Returns:
+        response (str): Response of the query.
+    """
+    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
+    query_obj = json.loads(query)
+    response = elasticsearch.helpers.reindex(
+        client=client,
+        source_index=source_index,
+        target_index=target_index,
+        query=query_obj,
+        chunk_size=chunk_size,
+        scroll=scroll,
+        op_type=op_type,
+    )
+    print("Reindexing finished")
+    return mgp.Record(response=str(response))
 
 
 @mgp.read_proc
@@ -391,6 +451,12 @@ def scan(
     elastic_password: str,
     index_name: str,
     query: str,
+    scroll: str = "5m",
+    raise_on_error: bool = True,
+    preserve_order: bool = False,
+    size: int = 1000,
+    request_timeout: mgp.Nullable[float] = None,
+    clear_scroll: bool = True,
 ) -> mgp.Record(items=mgp.List[mgp.Map]):
     """
     Runs a query on a index specified by the index_name.
@@ -411,6 +477,12 @@ def scan(
         client,
         query=query_obj,
         index=index_name,
+        scroll=scroll,
+        raise_on_error=raise_on_error,
+        preserve_order=preserve_order,
+        size=size,
+        request_timeout=request_timeout,
+        clear_scroll=clear_scroll,
     )
     items = []
     for item in response:
