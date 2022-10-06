b5d8d6899b6cb5085593c68d5f7e47f6471f21ea
Andi Skrgat <andi8647@gmail.com>
2022-10-06 20:27:31 +0200

    Fixed PR comments

--- python/elastic_search_serialization.py
+++ python/elastic_search_serialization.py
@@ -35,7 +35,6 @@ LK_CATEGORIES_HAS_RAW = "lk_categories_has_raw"
 LK_TYPE_HAS_RAW = "lk_type_has_raw"
 LK_CATEGORIES = "lk_categories"
 
-
 # Mappings of our data types
 lke_mapping: Dict[type, str] = {}
 lke_mapping[str] = LKE_STRING
@@ -43,6 +42,9 @@ lke_mapping[int] = LKE_NUMBER
 lke_mapping[float] = LKE_NUMBER
 lke_mapping[bool] = LKE_BOOLEAN
 lke_mapping[datetime] = LKE_DATE
+
+# Create global logger object
+logger = mgp.Logger()
 
 # Create node index
 node_index_body: Dict[str, Any] = {
@@ -197,12 +199,9 @@ def serialize_vertex(vertex: mgp.Vertex) -> Dict[str, Any]:
     Returns:
         Dict[str, Any]: ElasticSearch object representation.
     """
-    node: Dict[str, Any] = {}  # whole part
-    node[ACTION] = {INDEX: {ID: str(vertex.id)}}
     source = serialize_properties(vertex.properties.items())
     source[LK_CATEGORIES] = [label.name for label in vertex.labels]
-    node[SOURCE] = source
-    return node
+    return {ACTION: {INDEX: {ID: str(vertex.id)}}, SOURCE: source}
 
 
 def serialize_edge(edge: mgp.Edge) -> Dict[str, Any]:
@@ -213,12 +212,9 @@ def serialize_edge(edge: mgp.Edge) -> Dict[str, Any]:
     Returns:
         Dict[str, Any]: ElasticSearch object representation.
     """
-    rel: Dict[str, Any] = {}
-    rel[ACTION] = {INDEX: {ID: str(edge.id)}}
     source = serialize_properties(edge.properties.items())
     source[LK_TYPE] = edge.type.name
-    rel[SOURCE] = source
-    return rel
+    return {ACTION: {INDEX: {ID: str(edge.id)}}, SOURCE: source}
 
 
 def serialize_properties(properties: Dict[str, Any]) -> Dict[str, Any]:
@@ -235,13 +231,14 @@ def serialize_properties(properties: Dict[str, Any]) -> Dict[str, Any]:
             # Convert datetime to str, replace microsecond and add Z suffix(Zulu or zero offset) manually because Python doesn't support it out of the box
             prop_value = prop_value.replace(microsecond=0).isoformat() + "Z"
             source[prop_key + LKE_DATE] = prop_value
-            print(f"Prop value: {prop_value} {type(prop_value)}")
         elif type(prop_value) in lke_mapping:
             source[prop_key + lke_mapping[type(prop_value)]] = prop_value
     return source
 
 
-def connect_to_elasticsearch(elastic_url: str, ca_certs: str, elastic_user: str, elastic_password) -> Elasticsearch:
+def connect_to_elasticsearch(
+    elastic_url: str, ca_certs: str, elastic_user: str, elastic_password
+) -> Elasticsearch:
     """
     Establishes connection with the Elasticsearch. This configuration needs to be specific to the Elasticsearch deployment.
     Args:
@@ -253,11 +250,11 @@ def connect_to_elasticsearch(elastic_url: str, ca_certs: str, elastic_user: str,
         Elasticsearch: Client reference.
     """
     client = Elasticsearch(
-        elastic_url,
+        hosts=elastic_url,
         ca_certs=ca_certs,
         basic_auth=(elastic_user, elastic_password),
     )
-    print(f"Client info: {client.info()}")
+    logger.info(f"Client info: {client.info()}")
     return client
 
 
@@ -279,8 +276,12 @@ def create_node_index(
     global node_index_body
     node_index_body[SETTINGS][INDEX][NUMBER_OF_SHARDS] = number_of_shards
     node_index_body[SETTINGS][INDEX][NUMBER_OF_REPLICAS] = number_of_replicas
-    node_index_body[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_CATEGORIES_HAS_RAW][MAPPING][ANALYZER] = analyzer
-    node_index_body[MAPPINGS][DYNAMIC_TEMPLATES][1][STRING][MAPPING][ANALYZER] = analyzer
+    node_index_body[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_CATEGORIES_HAS_RAW][MAPPING][
+        ANALYZER
+    ] = analyzer
+    node_index_body[MAPPINGS][DYNAMIC_TEMPLATES][1][STRING][MAPPING][
+        ANALYZER
+    ] = analyzer
     client.indices.create(index=index_name, body=node_index_body, ignore=400)
 
 
@@ -302,8 +303,12 @@ def create_edge_index(
     global edge_index_body
     edge_index_body[SETTINGS][INDEX][NUMBER_OF_SHARDS] = number_of_shards
     edge_index_body[SETTINGS][INDEX][NUMBER_OF_REPLICAS] = number_of_replicas
-    edge_index_body[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_TYPE_HAS_RAW][MAPPING][ANALYZER] = analyzer
-    edge_index_body[MAPPINGS][DYNAMIC_TEMPLATES][1][STRING][MAPPING][ANALYZER] = analyzer
+    edge_index_body[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_TYPE_HAS_RAW][MAPPING][
+        ANALYZER
+    ] = analyzer
+    edge_index_body[MAPPINGS][DYNAMIC_TEMPLATES][1][STRING][MAPPING][
+        ANALYZER
+    ] = analyzer
     client.indices.create(index=index_name, body=edge_index_body, ignore=400)
 
 
@@ -326,7 +331,9 @@ def generate_documents(
     return nodes, edges
 
 
-def get_created_updated_objects(context_objects: List[Dict[str, Any]]) -> Tuple[List[mgp.Vertex], List[mgp.Edge]]:
+def get_created_updated_objects(
+    context_objects: List[Dict[str, Any]]
+) -> Tuple[List[mgp.Vertex], List[mgp.Edge]]:
     """
     Extracts nodes and edges from context_objects.
     Args:
@@ -375,7 +382,9 @@ def index(
         mgp.Record(): Returns JSON of all nodes and edges.
     """
     # First establish connection with ElasticSearch service.
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
+    client = connect_to_elasticsearch(
+        elastic_url, ca_certs, elastic_user, elastic_password
+    )
 
     # Crete indexes if they don't exist
     create_node_index(
@@ -396,16 +405,14 @@ def index(
 
     # Now create iterable of documents that need to be indexed
     nodes, edges = generate_documents(created_vertices, created_edges)
-    print(f"Nodes: {nodes}")
-    print(f"Edges: {edges}")
 
     # Send documents on indexing
-    print("Indexing vertices...")
+    logger.info("Indexing vertices...")
     for ok, action in streaming_bulk(client=client, index=node_index, actions=nodes):
-        print(f"OK: {ok} Action: {action}")
-    print()
+        logger.info(f"OK: {ok} Action: {action}")
+    logger.info("Indexing edges...")
     for ok, action in streaming_bulk(client=client, index=edge_index, actions=edges):
-        print(f"OK: {ok} Action: {action}")
+        logger.info(f"OK: {ok} Action: {action}")
     return mgp.Record(nodes=nodes, edges=edges)
 
 
@@ -442,7 +449,9 @@ def reindex(
     Returns:
         response (str): Response of the query.
     """
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
+    client = connect_to_elasticsearch(
+        elastic_url, ca_certs, elastic_user, elastic_password
+    )
     query_obj = json.loads(query)
     response = elasticsearch.helpers.reindex(
         client=client,
@@ -453,7 +462,7 @@ def reindex(
         scroll=scroll,
         op_type=op_type,
     )
-    print("Reindexing finished")
+    logger.info("Reindexing finished")
     return mgp.Record(response=str(response))
 
 
@@ -486,7 +495,9 @@ def scan(
     Returns:
          mgp.Record(items=mgp.List[mgp.Map]): List of all items matched by the specific query.
     """
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
+    client = connect_to_elasticsearch(
+        elastic_url, ca_certs, elastic_user, elastic_password
+    )
     query_obj = json.loads(query)
     response = elasticsearch.helpers.scan(
         client,
@@ -499,7 +510,5 @@ def scan(
         request_timeout=request_timeout,
         clear_scroll=clear_scroll,
     )
-    items = []
-    for item in response:
-        items.append(item)
+    items = [item for item in response]
     return mgp.Record(items=items)
