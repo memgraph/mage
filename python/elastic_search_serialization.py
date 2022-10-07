d982f20e5a4a10c02fbc799055512a9b1eddf42a
Andi Skrgat <andi8647@gmail.com>
2022-10-07 13:58:28 +0200

    connect method, PEP8, PR fixes, for loops optimization

--- python/elastic_search_serialization.py
+++ python/elastic_search_serialization.py
@@ -1,11 +1,11 @@
 import json
-import elasticsearch
-import mgp
 from typing import Any, List, Dict, Tuple
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import streaming_bulk
 from datetime import datetime
-import pathlib
+
+import elasticsearch
+
+import mgp
+
 
 # Elasticsearch constants
 ACTION = "action"
@@ -25,6 +25,7 @@ CREATED_VERTEX = "created_vertex"
 CREATED_EDGE = "created_edge"
 VERTEX = "vertex"
 EDGE = "edge"
+INDEX_TYPE = "index_type"
 
 # Linkurious constants
 LK_TYPE = "lk_type"
@@ -45,12 +46,15 @@ lke_mapping[bool] = LKE_BOOLEAN
 lke_mapping[datetime] = LKE_DATE
 
 # Create global logger object
-logger = mgp.Logger()
+logger: mgp.Logger = mgp.Logger()
+
+# Global client object
+client: elasticsearch.Elasticsearch
 
 
+# Helper method
 def serialize_vertex(vertex: mgp.Vertex) -> Dict[str, Any]:
-    """
-    Serializes vertex to specified ElasticSearch schema.
+    """Serializes vertex to specified ElasticSearch schema.
     Args:
         vertex (mgp.Vertex): Reference to the vertex in Memgraph DB
     Returns:
@@ -62,8 +66,7 @@ def serialize_vertex(vertex: mgp.Vertex) -> Dict[str, Any]:
 
 
 def serialize_edge(edge: mgp.Edge) -> Dict[str, Any]:
-    """
-    Serializes edge to specified ElasticSearch schema.
+    """Serializes edge to specified ElasticSearch schema.
     Args:
         edge (mgp.Edge): Reference to the edge in Memgraph DB.
     Returns:
@@ -75,8 +78,7 @@ def serialize_edge(edge: mgp.Edge) -> Dict[str, Any]:
 
 
 def serialize_properties(properties: Dict[str, Any]) -> Dict[str, Any]:
-    """
-    The method used to serialize properties of vertices and relationships.
+    """The method used to serialize properties of vertices and relationships.
     Args:
         properties (Dict[str, Any]]): Properties of nodes and relationships.
     Returns:
@@ -84,44 +86,23 @@ def serialize_properties(properties: Dict[str, Any]) -> Dict[str, Any]:
     """
     source: Dict[str, Any] = {}
     for prop_key, prop_value in properties:
-        if type(prop_value) == datetime:
+        if isinstance(prop_value, datetime):
             # Convert datetime to str, replace microsecond and add Z suffix(Zulu or zero offset) manually because Python doesn't support it out of the box
-            prop_value = prop_value.replace(microsecond=0).isoformat() + "Z"
-            source[prop_key + LKE_DATE] = prop_value
+            prop_value = f"{prop_value.replace(microsecond=0).isoformat()}Z"
+            source[f"{prop_key}{LKE_DATE}"] = prop_value
         elif type(prop_value) in lke_mapping:
-            source[prop_key + lke_mapping[type(prop_value)]] = prop_value
+            source[f"{prop_key}{lke_mapping[type(prop_value)]}"] = prop_value
     return source
-
-
-def connect_to_elasticsearch(elastic_url: str, ca_certs: str, elastic_user: str, elastic_password) -> Elasticsearch:
-    """
-    Establishes connection with the Elasticsearch. This configuration needs to be specific to the Elasticsearch deployment.
-    Args:
-        elastic_url (str): URL for connecting to the Elasticsearch instance.
-        ca_certs (str): Path to the certificate file.
-        elastic_user (str): The user trying to connect to the Elasticsearch.
-        elastic_password (str): User's password for connecting to the Elasticsearch.
-    Returns:
-        Elasticsearch: Client reference.
-    """
-    client = Elasticsearch(
-        hosts=elastic_url,
-        ca_certs=ca_certs,
-        basic_auth=(elastic_user, elastic_password),
-    )
-    logger.info(f"Client info: {client.info()}")
-    return client
 
 
 def generate_documents_from_context_objects(
     context_objects: List[Dict[str, Any]]
 ) -> Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]:
-    """
-    Generates nodes and edges documents for indexing and returns them as lists.
+    """Generates nodes and edges documents for indexing and returns them as lists.
     Args:
         context_objects (List[Dict[str, Any]]): Objects that are sent as parameters because of some trigger that was called. Trigger can be for update or for create.
     Returns:
-        Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]: serialized nodes and edges
+        Tuple[List[Dict[str, Any]], List[Dict[str, Any]]]: Serialized nodes and edges.
     """
     nodes, edges = [], []
     for context_object in context_objects:
@@ -133,43 +114,90 @@ def generate_documents_from_context_objects(
 
 
 @mgp.read_proc
-def create_index(
-    context: mgp.ProcCtx,
-    index_name: str,
-    schema_path: str,
-    elastic_url: str,
-    ca_certs: str,
-    elastic_user: str,
-    elastic_password: str,
-) -> mgp.Record(response=mgp.Map):
-    """ 
-    Creates index with the given index name.
+def connect(
+    elastic_url: str, ca_certs: str, elastic_user: str, elastic_password
+) -> mgp.Record(connection_status=mgp.Map):
+    """Establishes connection with the Elasticsearch. This configuration needs to be specific to the Elasticsearch deployment. Uses basic authentication
     Args:
-        index_name(str): Name of the index that needs to be created.
         elastic_url (str): URL for connecting to the Elasticsearch instance.
         ca_certs (str): Path to the certificate file.
         elastic_user (str): The user trying to connect to the Elasticsearch.
         elastic_password (str): User's password for connecting to the Elasticsearch.
     Returns:
-       mgp.Map: response message from Elasticsearch service.  
+        mgp.Record(connection_status=mgp.Map): Connection info.
     """
-    # First establish connection with ElasticSearch service.
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
+    global client
+    client = elasticsearch.Elasticsearch(
+        hosts=elastic_url,
+        ca_certs=ca_certs,
+        basic_auth=(elastic_user, elastic_password),
+    )
+    logger.info(f"Client info: {client.info()}")
+    return mgp.Record(connection_status=dict(client.info()))
+
+
+@mgp.read_proc
+def create_index(
+    context: mgp.ProcCtx,
+    index_name: str,
+    schema_path: str,
+    schema_parameters: mgp.Map,
+) -> mgp.Record(response=mgp.Map):
+    """Creates index with the given index name.
+    Args:
+        index_name (str): Name of the index that needs to be created.
+        schema_path (str): Path to the schema from where it will be loaded.
+        schema_parameters: Dict[str, Any]
+            number_of_shards (int): Number of shards index will use.
+            number_of_replicas (int): Number of replicas index will use.
+        analyzer (str): Custom analyzer, can be set to any legal Elasticsearch analyzer.
+    Returns:
+       mgp.Map: Response message from Elasticsearch service.
+    """
+    global client
     # Read schema from the path given
     with open(schema_path, "r") as schema_file:
         schema_json = json.loads(schema_file.read())
-    response = dict(client.indices.create(index=index_name, body=schema_json, ignore=400))
-    return mgp.Record(response=response)
+    # Update default schema if specified
+    if NUMBER_OF_SHARDS in schema_parameters:
+        schema_json[SETTINGS][INDEX][NUMBER_OF_SHARDS] = schema_parameters[
+            NUMBER_OF_SHARDS
+        ]
+        logger.info(
+            f"Number of shards updated to: {schema_parameters[NUMBER_OF_SHARDS]}"
+        )
+    if NUMBER_OF_REPLICAS in schema_parameters:
+        schema_json[SETTINGS][INDEX][NUMBER_OF_REPLICAS] = schema_parameters[
+            NUMBER_OF_REPLICAS
+        ]
+        logger.info(
+            f"Number of replicas updated to: {schema_parameters[NUMBER_OF_REPLICAS]}"
+        )
+    if ANALYZER in schema_parameters and INDEX_TYPE in schema_parameters:
+        schema_json[MAPPINGS][DYNAMIC_TEMPLATES][1][STRING][MAPPING][
+            ANALYZER
+        ] = schema_parameters[ANALYZER]
+        if schema_parameters[INDEX_TYPE] == VERTEX:
+            schema_json[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_CATEGORIES_HAS_RAW][MAPPING][
+                ANALYZER
+            ] = schema_parameters[ANALYZER]
+        else:
+            schema_json[MAPPINGS][DYNAMIC_TEMPLATES][0][LK_TYPE_HAS_RAW][MAPPING][
+                ANALYZER
+            ] = schema_parameters[ANALYZER]
+        logger.info(f"Analyzer set to: {schema_parameters[ANALYZER]}")
+
+    return mgp.Record(
+        response=dict(
+            client.indices.create(index=index_name, body=schema_json, ignore=400)
+        )
+    )
 
 
 @mgp.read_proc
 def index(
     context: mgp.ProcCtx,
     createdObjects: mgp.List[mgp.Map],
-    elastic_url: str,
-    ca_certs: str,
-    elastic_user: str,
-    elastic_password: str,
     node_index: str,
     edge_index: str,
     chunk_size: int = 500,
@@ -181,41 +209,34 @@ def index(
     max_backoff: float = 600.0,
     yield_ok: bool = True,
 ) -> mgp.Record(nodes=mgp.List[mgp.Map], edges=mgp.List[mgp.Map]):
-    """
-    The method serializes all vertices and relationships that are in Memgraph DB to an ElasticSearch schema.
+    """The method serializes all vertices and relationships that are in Memgraph DB to an ElasticSearch schema.
     Args:
         context (mgp.ProcCtx): Reference to the executing context.
         createdObjects (List[Dict[str, Any]]): List of all objects that were created andthen sent as arguments to this method with the help of "create trigger".
-        elastic_url (str): URL for connecting to the Elasticsearch instance.
-        ca_certs (str): Path to the certificate file.
-        elastic_user (str): The user trying to connect to the Elasticsearch.
-        elastic_password (str): User's password for connecting to the Elasticsearch.
         node_index (str): The name of the node index.
         edge_index (str): The name of the edge index.
         number_of_shards (int): A number of shards you want to use in your index.
         number_of_replicas (int): A number of replicas you want to use in your index.
-        chunk_size (int): number of docs in one chunk sent to es (default: 500).
-        max_chunk_bytes (int): the maximum size of the request in bytes (default: 100MB).
-        raise_on_error (bool): raise BulkIndexError containing errors (as .errors) from the execution of the last chunk when some occur. By default we raise.
-        raise_on_exception (bool): if False then don’t propagate exceptions from call to bulk and just report the items that failed as failed.
-        max_retries (int): maximum number of times a document will be retried when 429 is received, set to 0 (default) for no retries on 429.
-        initial_backoff (float): number of seconds we should wait before the first retry. Any subsequent retries will be powers of initial_backoff * 2**retry_number
-        max_backoff (float): maximum number of seconds a retry will wait
-        yield_ok (float): if set to False will skip successful documents in the output
+        chunk_size (int): The number of docs in one chunk sent to es (default: 500).
+        max_chunk_bytes (int): The maximum size of the request in bytes (default: 100MB).
+        raise_on_error (bool): Raise BulkIndexError containing errors (as .errors) from the execution of the last chunk when some occur. By default we raise.
+        raise_on_exception (bool): If False then don’t propagate exceptions from call to bulk and just report the items that failed as failed.
+        max_retries (int): Maximum number of times a document will be retried when 429 is received, set to 0 (default) for no retries on 429.
+        initial_backoff (float): The number of seconds we should wait before the first retry. Any subsequent retries will be powers of initial_backoff * 2**retry_number.
+        max_backoff (float): The maximum number of seconds a retry will wait.
+        yield_ok (float): If set to False will skip successful documents in the output.
     Returns:
         mgp.Record(): Returns JSON of all nodes and edges.
     """
-    # First establish connection with ElasticSearch service.
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
-
+    global client
     # Now create iterable of documents that need to be indexed
     nodes, edges = generate_documents_from_context_objects(createdObjects)
     logger.info(f"Nodes: {nodes}")
     logger.info(f"Edges: {edges}")
 
-    # Send documents on indexing
+    # Send nodes on indexing
     logger.info("Indexing vertices...")
-    for ok, action in streaming_bulk(
+    for ok, action in elasticsearch.helpers.streaming_bulk(
         client=client,
         index=node_index,
         actions=nodes,
@@ -229,8 +250,9 @@ def index(
         max_retries=max_retries,
     ):
         logger.info(f"OK: {ok} Action: {action}")
+    # Send edges on indexing
     logger.info("Indexing edges...")
-    for ok, action in streaming_bulk(
+    for ok, action in elasticsearch.helpers.streaming_bulk(
         client=client,
         index=edge_index,
         actions=edges,
@@ -250,26 +272,17 @@ def index(
 @mgp.read_proc
 def reindex(
     context: mgp.ProcCtx,
-    elastic_url: str,
-    ca_certs: str,
-    elastic_user: str,
-    elastic_password: str,
     source_index: mgp.Any,
     target_index: str,
     query: str,
     chunk_size: int = 500,
     scroll: str = "5m",
     op_type: mgp.Nullable[str] = None,
-) -> mgp.Record(response=str):
-    """
-    Reindex all documents from one index that satisfy a given query to another, potentia    lly (if target_client is specified) on a different cluster. If you don’t specify the    query you will reindex all the documents.
+) -> mgp.Record(response=mgp.Map):
+    """Reindex all documents from one index that satisfy a given query to another, potentially (if target_client is specified) on a different cluster. If you don’t specify the query you will reindex all the documents.
     Args:
         context (mgp.ProcCtx): Reference to the executing context.
         updatatedObjects (List[Dict[str, Any]]): List of all objects that were updated and then sent as arguments to this method with the help of the "update trigger".
-        elastic_url (str): URL for connecting to the Elasticsearch instance.
-        ca_certs (str): Path to the certificate file.
-        elastic_user (str): The user trying to connect to the Elasticsearch.
-        elastic_password str: User's password for connecting to the Elasticsearch.
         source_index (Union[str, List[str]]): Identifies source index(or more of them) from where documents need to be indexed.
         target_index (str): Identifies target index to where documents need to be indexed.
         query (str): Query written as JSON.
@@ -279,28 +292,25 @@ def reindex(
     Returns:
         response (str): Response of the query.
     """
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
-    query_obj = json.loads(query)
-    response = elasticsearch.helpers.reindex(
-        client=client,
-        source_index=source_index,
-        target_index=target_index,
-        query=query_obj,
-        chunk_size=chunk_size,
-        scroll=scroll,
-        op_type=op_type,
+    global client
+    response = dict(
+        elasticsearch.helpers.reindex(
+            client=client,
+            source_index=source_index,
+            target_index=target_index,
+            query=json.loads(query),
+            chunk_size=chunk_size,
+            scroll=scroll,
+            op_type=op_type,
+        )
     )
     logger.info("Reindexing finished")
-    return mgp.Record(response=str(response))
+    return mgp.Record(response=response)
 
 
 @mgp.read_proc
 def scan(
     context: mgp.ProcCtx,
-    elastic_url: str,
-    ca_certs: str,
-    elastic_user: str,
-    elastic_password: str,
     index_name: str,
     query: str,
     scroll: str = "5m",
@@ -310,24 +320,24 @@ def scan(
     request_timeout: mgp.Nullable[float] = None,
     clear_scroll: bool = True,
 ) -> mgp.Record(items=mgp.List[mgp.Map]):
-    """
-    Runs a query on a index specified by the index_name.
+    """Runs a query on a index specified by the index_name.
     Args:
         context (mgp.ProcCtx): Reference to the executing context.
-        elastic_url (str): URL for connecting to the Elasticsearch instance.
-        ca_certs (str): Path to the certificate file.
-        elastic_user (str): The user trying to connect to the Elasticsearch.
-        elastic_password (str): User's password for connecting to the Elasticsearch.
         index_name (str): A name of the index.
         query (str): Query written as JSON.
+        scroll (int): Specify how long a consistent view of the index should be maintained for scrolled search.
+        raise_on_error (bool): Raises an exception (ScanError) if an error is encountered (some shards fail to execute). By default we raise.
+        preserve_order (bool): Don’t set the search_type to scan - this will cause the scroll to paginate with preserving the order. Note that this can be an extremely expensive operation and can easily lead to unpredictable results, use with caution.
+        size (int): Size (per shard) of the batch send at each iteration.
+        request_timeout (mgp.Nullable[float]): Explicit timeout for each call to scan.
+        clear_scroll (bool): Explicitly calls delete on the scroll id via the clear scroll API at the end of the method on completion or error, defaults to true.
     Returns:
          mgp.Record(items=mgp.List[mgp.Map]): List of all items matched by the specific query.
     """
-    client = connect_to_elasticsearch(elastic_url, ca_certs, elastic_user, elastic_password)
-    query_obj = json.loads(query)
+    global client
     response = elasticsearch.helpers.scan(
         client,
-        query=query_obj,
+        query=json.loads(query),
         index=index_name,
         scroll=scroll,
         raise_on_error=raise_on_error,
@@ -336,5 +346,4 @@ def scan(
         request_timeout=request_timeout,
         clear_scroll=clear_scroll,
     )
-    items = [item for item in response]
-    return mgp.Record(items=items)
+    return mgp.Record(items=[item for item in response])
