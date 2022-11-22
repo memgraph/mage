4072eb9a50e0f26b6f251f5125973d0099485f51
Josip Mrden <josip.mrden@memgraph.io>
2022-11-22 12:23:44 +0100

    Added topological sort and e2e tests

--- cpp/graph_util_module/graph_util_module.cpp
+++ cpp/graph_util_module/graph_util_module.cpp
@@ -129,6 +129,63 @@ void Ancestors(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mg
   }
 }
 
+void TopologicalSort(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  try {
+    mgp::memory = memory;
+    const auto record_factory = mgp::RecordFactory(result);
+    const auto graph = mgp::Graph(memgraph_graph);
+
+    std::map<mgp::Node, uint64_t> in_degrees;
+
+    for (const auto node : graph.Nodes()) {
+      in_degrees[node] = 0;
+    }
+
+    for (const auto node : graph.Nodes()) {
+      for (const auto relationship : node.OutRelationships()) {
+        const auto to_node = relationship.To();
+        in_degrees[to_node] += 1;
+      }
+    }
+
+    std::list<mgp::Node> nodes_with_no_incoming_edges;
+    for (const auto node : graph.Nodes()) {
+      if (in_degrees[node] == 0) {
+        nodes_with_no_incoming_edges.emplace_back(node);
+      }
+    }
+
+    mgp::List topological_ordering = mgp::List();
+
+    while (nodes_with_no_incoming_edges.size() > 0) {
+      const auto node = nodes_with_no_incoming_edges.back();
+      nodes_with_no_incoming_edges.pop_back();
+      topological_ordering.AppendExtend(mgp::Value(node));
+
+      for (const auto relationship : node.OutRelationships()) {
+        const auto to_node = relationship.To();
+        in_degrees[to_node] -= 1;
+
+        if (in_degrees[to_node] == 0) {
+          nodes_with_no_incoming_edges.emplace_back(to_node);
+        }
+      }
+    }
+
+    if (topological_ordering.Size() == graph.Order()) {
+      auto record = record_factory.NewRecord();
+      record.Insert("sorted_nodes", topological_ordering);
+      return;
+    }
+
+    throw std::runtime_error("Graph has a cycle! No topological ordering exists.");
+
+  } catch (const std::exception &e) {
+    mgp::result_set_error_msg(result, e.what());
+    return;
+  }
+}
+
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
@@ -152,6 +209,12 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
 
     AddProcedure(Descendants, "descendants", mgp::ProdecureType::Read, {mgp::Parameter("node", mgp::Type::Node)},
                  {mgp::Return("descendants", descendants_return)}, module, memory);
+
+    // Register topological sort procedure
+    const auto topological_sort_return = std::make_pair(mgp::Type::List, mgp::Type::Node);
+
+    AddProcedure(TopologicalSort, "topological_sort", mgp::ProdecureType::Read, {},
+                 {mgp::Return("sorted_nodes", topological_sort_return)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
