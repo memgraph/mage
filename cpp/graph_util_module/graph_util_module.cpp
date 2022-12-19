2675f349b121a158016048e8fed4a609ff882bed
Josipmrden <josip.mrden@memgraph.io>
2022-12-19 09:45:56 +0100

    [T316 > master] Add graph utility of chain nodes function (#185)

--- cpp/graph_util_module/graph_util_module.cpp
+++ cpp/graph_util_module/graph_util_module.cpp
@@ -1,11 +1,13 @@
 #include <mgp.hpp>
 
 #include "algorithms/ancestors.hpp"
+#include "algorithms/chain_nodes.hpp"
 #include "algorithms/connect_nodes.hpp"
 #include "algorithms/descendants.hpp"
 #include "algorithms/topological_sort.hpp"
 
 const char *kProcedureAncestors = "ancestors";
+const char *kProcedureChainNodes = "chain_nodes";
 const char *kProcedureConnectNodes = "connect_nodes";
 const char *kProcedureDescendants = "descendants";
 const char *kProcedureTopologicalSort = "topological_sort";
@@ -15,9 +17,9 @@ const char *kReturnConnections = "connections";
 const char *kReturnDescendants = "descendants";
 const char *kReturnSortedNodes = "sorted_nodes";
 
+const char *kArgumentEdgeType = "edge_type";
 const char *kArgumentNode = "node";
 const char *kArgumentNodes = "nodes";
-
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
@@ -29,6 +31,15 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     AddProcedure(Ancestors, kProcedureAncestors, mgp::ProcedureType::Read,
                  {mgp::Parameter(kArgumentNode, mgp::Type::Node)}, {mgp::Return(kReturnAncestors, ancestors_return)},
                  module, memory);
+
+    // Register chain nodes procedure
+    const auto chain_nodes_input_nodes = std::make_pair(mgp::Type::List, mgp::Type::Node);
+    const auto chain_nodes_output_connections = std::make_pair(mgp::Type::List, mgp::Type::Relationship);
+
+    AddProcedure(
+        ChainNodes, kProcedureChainNodes, mgp::ProcedureType::Write,
+        {mgp::Parameter(kArgumentNodes, chain_nodes_input_nodes), mgp::Parameter(kArgumentEdgeType, mgp::Type::String)},
+        {mgp::Return(kResultConnections, chain_nodes_output_connections)}, module, memory);
 
     // Register connect nodes procedure
     const auto connect_nodes_input = std::make_pair(mgp::Type::List, mgp::Type::Node);
