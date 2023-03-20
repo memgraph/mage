d46005f7ea4ca98b757e6704270d90d8d80d9ce9
Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
2023-03-20 11:35:50 +0100

    Add CSV export query results module (#199)

--- python/export_util.py
+++ python/export_util.py
@@ -1,9 +1,14 @@
+import csv
+import io
+import json as js
+import mgp
+
 from dataclasses import dataclass
 from datetime import datetime, date, time, timedelta
-import json as js
+from gqlalchemy import Memgraph
+from typing import Any, Dict, List, Union
+
 from mage.export_import_util.parameters import Parameter
-import mgp
-from typing import Union, List, Dict, Any
 
 
 @dataclass
@@ -124,3 +129,99 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
         raise OSError("Could not open or write to the file.")
 
     return mgp.Record()
+
+
+def save_file(file_path: str, data_list: list):
+    try:
+        with open(
+            file_path,
+            "w",
+            newline="",
+            encoding="utf8",
+        ) as f:
+            writer = csv.writer(f)
+            writer.writerows(data_list)
+    except PermissionError:
+        raise PermissionError(
+            "You don't have permissions to write into that file. Make sure to give the necessary permissions to user memgraph."
+        )
+    except csv.Error as e:
+        raise csv.Error(
+            "Could not write to the file {}, stopped at line {}: {}".format(
+                file_path, writer.line_num, e
+            )
+        )
+    except Exception:
+        raise OSError("Could not open or write to the file.")
+
+
+def csv_to_stream(data_list: list) -> str:
+    output = io.StringIO()
+    try:
+        writer = csv.writer(output, quoting=csv.QUOTE_NONNUMERIC)
+        writer.writerows(data_list)
+    except csv.Error as e:
+        raise csv.Error(
+            "Could not write a stream, stopped at line {}: {}".format(
+                writer.line_num, e
+            )
+        )
+    return output.getvalue()
+
+
+@mgp.read_proc
+def csv_query(
+    context: mgp.ProcCtx,
+    query: str,
+    file_path: str = "",
+    stream: bool = False,
+) -> mgp.Record(file_path=str, data=str):
+    """
+    Procedure to export query results to a CSV file.
+    Args:
+        context (mgp.ProcCtx): Reference to the context execution.
+        query (str): A query from which the results will be saved to a CSV file.
+        file_path (str, optional): A path to the CSV file where the query results will be exported. Defaults to an empty string.
+        stream (bool, optional): A value which determines whether a stream of query results in a CSV format will be returned.
+    Returns:
+        mgp.Record(
+            file_path (str): A path to the CSV file where the query results are exported. If file_path is not provided, the output will be an empty string.
+            data (str): A stream of query results in a CSV format.
+        )
+    Raises:
+        Exception: If neither file nor config are provided, or if only config is provided with stream set to False. Also if query yields no results or if the database is empty.
+        PermissionError: If you provided file path that you have no permissions to write at.
+        csv.Error: If an error occurred while writing into stream or CSV file.
+        OSError: If the file can't be opened or written to.
+    """
+
+    # file or config have to be provided
+    if not file_path and not stream:
+        raise Exception("Please provide file name and/or config.")
+
+    # only config provided with stream set to false
+    if not file_path and not stream:
+        raise Exception(
+            "If you provided only stream value, it has to be set to True to get any results."
+        )
+
+    memgraph = Memgraph()
+    results = list(memgraph.execute_and_fetch(query))
+
+    # if query yields no result
+    if not len(results):
+        raise Exception(
+            "Your query yields no results. Check if the database is empty or rewrite the provided query."
+        )
+
+    result_keys = list(results[0])
+    data_list = [result_keys] + [list(result.values()) for result in results]
+    data = ""
+
+    if file_path:
+        save_file(file_path, data_list)
+
+    if stream:
+        data = csv_to_stream(data_list)
+
+    return mgp.Record(file_path=file_path, data=data)
