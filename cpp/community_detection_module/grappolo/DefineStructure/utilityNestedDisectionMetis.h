33cac9be94efe9c15e9e64fb6b262bf92c324cda
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-14 14:08:22 -0400

    spelling: dissect
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- cpp/community_detection_module/grappolo/DefineStructure/utilityNestedDisectionMetis.h
+++ cpp/community_detection_module/grappolo/DefineStructure/utilityNestedDisectionMetis.h
@@ -39,8 +39,8 @@
 //
 // ************************************************************************
 
-#ifndef _graph_NestDisect_
-#define _graph_NestDisect_
+#ifndef _graph_NestDissect_
+#define _graph_NestDissect_
 
 /*
  int METIS NodeND(idx t *nvtxs, idx t *xadj, idx t *adjncy, idx t *vwgt, idx t *options,
