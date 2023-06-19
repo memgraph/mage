0c153a5275602691e43921666f619f3867406175
katarinasupe <supe.katarina@gmail.com>
2023-06-19 13:20:35 +0200

    Update llm util

--- python/llm_util.py
+++ python/llm_util.py
@@ -12,119 +12,134 @@ class Parameter(Enum):
     TYPE = "type"
 
 
-@mgp.read_proc
-def schema(
-    context: mgp.ProcCtx, verbose: bool = False
-) -> mgp.Record(simple_schema=mgp.Map, verbose_schema=str):
-    """
-    Procedure to generate the graph database schema for LangChain.
-
-    Args:
-        context (mgp.ProcCtx): Reference to the context execution.
-        verbose (bool): If set to True, the graph schema will include additional context.
-
-    Returns:
-        mgp.Record containing a mgp.Map of node properties, relationship properties and all relationships in the database and a string representing verbose schema especially useful for LangChain.
-
-    Example:
-        Get a simple schema without verbose schema:
-            `CALL llm_util.schema() YIELD simple_schema, verbose_schema RETURN simple_schema, verbose_schema;`
-            or
-            `CALL llm_util.schema() YIELD simple_schema, verbose_schema RETURN simple_schema;`
-        Get graph schema with simple and verbose schema:
-            `CALL llm_util.schema(true) YIELD simple_schema, verbose_schema RETURN simple_schema, verbose_schema;`
-        Get only verbose schema:
-            `CALL llm_util.schema(true) YIELD simple_schema, verbose_schema RETURN verbose_schema;`
-    """
-
-    node_counter = 0
-    all_node_properties_dict = {}
-    all_relationship_properties_dict = {}
-    all_relationships_list = []
-    for node in context.graph.vertices:
-        node_counter += 1
-        labels = ":".join(tuple(sorted(label.name for label in node.labels)))
-
-        all_node_properties_dict = get_properties_dict(
-            node, all_node_properties_dict, labels
-        )
-
-        for relationship in node.out_edges:
-            target_labels = tuple(
-                sorted(label.name for label in relationship.to_vertex.labels)
-            )
-
-            full_relationship = {
-                Parameter.START.value: labels,
-                Parameter.TYPE.value: relationship.type.name,
-                Parameter.END.value: ":".join(target_labels),
-            }
-            if full_relationship not in all_relationships_list:
-                all_relationships_list.append(full_relationship)
-            all_relationship_properties_dict = get_properties_dict(
-                relationship, all_relationship_properties_dict, relationship.type.name
-            )
-
-    if node_counter == 0:
-        raise Exception(
-            "Can't generate a graph schema since there is no data in the database."
-        )
-
-    if verbose:
-        verbose_schema = "Node properties are the following:\n"
-        for label in all_node_properties_dict.keys():
-            verbose_schema += f"Node name: '{label}', Node properties: {all_node_properties_dict.get(label)}\n"
-
-        verbose_schema += "\nRelationship properties are the following:\n"
-        for type in all_relationship_properties_dict.keys():
-            verbose_schema += f"Relationship Name: '{type}', Relationship Properties: {all_relationship_properties_dict.get(type)}\n"
-
-        verbose_schema += "\nThe relationships are the following:\n"
-
-        for relationship in all_relationships_list:
-            verbose_schema += f"['(:{relationship[Parameter.START.value]})-[:{relationship[Parameter.TYPE.value]}]->(:{relationship[Parameter.END.value]})']\n"
-
-        return mgp.Record(
-            simple_schema={
-                Parameter.NODE_PROPS.value: all_relationship_properties_dict,
-                Parameter.REL_PROPS.value: all_relationship_properties_dict,
-                Parameter.RELATIONSHIPS.value: all_relationships_list,
-            },
-            verbose_schema=verbose_schema,
-        )
-
-    else:
-        return mgp.Record(
-            simple_schema={
-                Parameter.NODE_PROPS.value: all_relationship_properties_dict,
-                Parameter.REL_PROPS.value: all_relationship_properties_dict,
-                Parameter.RELATIONSHIPS.value: all_relationships_list,
-            },
-            verbose_schema="",
-        )
+class OutputType(Enum):
+    RAW = "raw"
+    PROMPT_READY = "prompt_ready"
 
 
-def get_properties_dict(graph_object, all_properties_dict, key):
-    for property_name in graph_object.properties.keys():
-        if not all_properties_dict.get(key):
-            all_properties_dict[key] = [
-                {
-                    Parameter.PROPERTY.value: property_name,
-                    Parameter.TYPE.value: type(
-                        graph_object.properties.get(property_name)
-                    ).__name__,
-                }
-            ]
+class SchemaGenerator(object):
+    def __init__(self, context, output_type):
+        self.type = output_type
+        self.node_counter = 0
+        self._all_node_properties_dict = {}
+        self._all_relationship_properties_dict = {}
+        self._all_relationships_list = []
+
+        self._generate_schema(context)
+
+    def get_schema(self):
+        if self.type == OutputType.RAW.value:
+            return self._get_raw_schema()
+        elif self.type == OutputType.PROMPT_READY.value:
+            return self._get_prompt_ready_schema()
         else:
-            if property_name not in [
-                d[Parameter.PROPERTY.value] for d in all_properties_dict[key]
-            ]:
-                all_properties_dict[key].append(
+            raise Exception(
+                "Can't generate a graph schema since the provided output_type is not correct. Please choose 'raw' or 'prompt_ready'."
+            )
+
+    def _generate_schema(self, context):
+        for node in context.graph.vertices:
+            self.node_counter += 1
+            labels = ":".join(tuple(sorted(label.name for label in node.labels)))
+
+            self._update_properties_dict(node, self._all_node_properties_dict, labels)
+
+            for relationship in node.out_edges:
+                target_labels = tuple(
+                    sorted(label.name for label in relationship.to_vertex.labels)
+                )
+
+                full_relationship = {
+                    Parameter.START.value: labels,
+                    Parameter.TYPE.value: relationship.type.name,
+                    Parameter.END.value: ":".join(target_labels),
+                }
+                if full_relationship not in self._all_relationships_list:
+                    self._all_relationships_list.append(full_relationship)
+
+                self._update_properties_dict(
+                    relationship,
+                    self._all_relationship_properties_dict,
+                    relationship.type.name,
+                )
+
+    def _get_raw_schema(self):
+        return {
+            Parameter.NODE_PROPS.value: self._all_node_properties_dict,
+            Parameter.REL_PROPS.value: self._all_relationship_properties_dict,
+            Parameter.RELATIONSHIPS.value: self._all_relationships_list,
+        }
+
+    def _get_prompt_ready_schema(self):
+        prompt_ready_schema = "Node properties are the following:\n"
+        for label in self._all_node_properties_dict.keys():
+            prompt_ready_schema += f"Node name: '{label}', Node properties: {self._all_node_properties_dict.get(label)}\n"
+
+        prompt_ready_schema += "\nRelationship properties are the following:\n"
+        for rel in self._all_relationship_properties_dict.keys():
+            prompt_ready_schema += f"Relationship Name: '{rel}', Relationship Properties: {self._all_relationship_properties_dict.get(rel)}\n"
+
+        prompt_ready_schema += "\nThe relationships are the following:\n"
+
+        for relationship in self._all_relationships_list:
+            prompt_ready_schema += f"['(:{relationship[Parameter.START.value]})-[:{relationship[Parameter.TYPE.value]}]->(:{relationship[Parameter.END.value]})']\n"
+
+        return prompt_ready_schema
+
+    def _update_properties_dict(self, graph_object, all_properties_dict, key):
+        for property_name in graph_object.properties.keys():
+            if not all_properties_dict.get(key):
+                all_properties_dict[key] = [
                     {
                         Parameter.PROPERTY.value: property_name,
                         Parameter.TYPE.value: type(
                             graph_object.properties.get(property_name)
                         ).__name__,
                     }
-                )
-    return all_properties_dict
+                ]
+            else:
+                if property_name not in [
+                    d[Parameter.PROPERTY.value] for d in all_properties_dict[key]
+                ]:
+                    all_properties_dict[key].append(
+                        {
+                            Parameter.PROPERTY.value: property_name,
+                            Parameter.TYPE.value: type(
+                                graph_object.properties.get(property_name)
+                            ).__name__,
+                        }
+                    )
+
+
+@mgp.read_proc
+def schema(
+    context: mgp.ProcCtx,
+    output_type: str = OutputType.PROMPT_READY.value,
+) -> mgp.Record(schema=mgp.Any):
+    """
+    Procedure to generate the graph database schema in raw or prompt-ready format.
+
+    Args:
+        context (mgp.ProcCtx): Reference to the context execution.
+        output_type (str): By default (set to 'prompt_ready'), the graph schema will include additional context and it will be prompt-ready. If set to 'raw', it will produce a simple version which can be adjusted for prompt.
+
+    Returns:
+        mgp.Record containing a mgp.Map of node properties, relationship properties and all relationships in the database and a string representing prompt-ready schema.
+
+    Example:
+        Get raw graph schema:
+            `CALL llm_util.schema('raw') YIELD schema RETURN schema;`
+        Get prompt-ready graph schema:
+            `CALL llm_util.schema() YIELD schema RETURN schema;`
+            or
+            `CALL llm_util.schema('prompt_ready') YIELD schema RETURN schema;`
+    """
+
+    schema_generator = SchemaGenerator(context, output_type)
+
+    if schema_generator.node_counter == 0:
+        raise Exception(
+            "Can't generate a graph schema since there is no data in the database."
+        )
+
+    return mgp.Record(schema=schema_generator.get_schema())
