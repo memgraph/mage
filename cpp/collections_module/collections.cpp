77d0c9727b7e6b716257e0d47142799a0af9be86
idoraban <idora.ban@memgraph.io>
2023-07-19 08:15:42 +0000

    implement sum procedure and e2e tests

--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -1,9 +1,12 @@
 #include <mgp.hpp>
 
+#include "algorithms/sum.hpp"
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  mgp::memory = memory;
   try {
-   
+    AddProcedure(Sum, "sum", mgp::ProcedureType::Read, {mgp::Parameter("inputList", {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return("sum", mgp::Type::Double)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
