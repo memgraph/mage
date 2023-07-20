b5346302e4f452750e5487a7f4ca684e1bc8d33e
Matija <pintamato@gmail.com>
2023-07-20 11:27:57 +0200

    Minor changes to C++ styling

--- cpp/collections_module/algorithms/collections.hpp
+++ cpp/collections_module/algorithms/collections.hpp
@@ -2,25 +2,28 @@
 
 
 void contains(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory){
-  mgp::memory=memory;
-  const auto arguments=mgp::List(args);
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
   try{
-    mgp::List list=arguments[0].ValueList();
-    mgp::Value value=arguments[1];
-    bool contains_value=false;
-    if(list.Size()==0 || list.Empty()){
-      contains_value=false;
-    }else{
-      for(size_t i=0;i<list.Size();i++){
-        if(list[i]==value){
-          contains_value=true;
-          break;
-        }
-      }
+    const mgp::List &list = arguments[0].ValueList();
+    const mgp::Value &value = arguments[1];
+    bool contains_value{false};
+    auto record = record_factory.NewRecord();
+    if(list.Empty()){
+      record.Insert("output", contains_value);
+      return;
     }
-    auto record=record_factory.NewRecord();
-    record.Insert("output",contains_value);
+    for(size_t i = 0;i < list.Size(); i++){
+      if(list[i] == value){
+        contains_value = true;
+        break;
+      }  
+    }
+    record.Insert("output", contains_value);
+    
+    
+  
     
   }catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
