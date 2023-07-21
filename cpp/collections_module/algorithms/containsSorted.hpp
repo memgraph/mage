28b2a5e137d0a13e53f9df6d29f1570cb2945c30
idoraban <idora.ban@memgraph.io>
2023-07-21 10:11:56 +0000

    implement collections.containsSorted procedure and e2e tests for it

---
+++ cpp/collections_module/algorithms/containsSorted.hpp
@@ -0,0 +1,39 @@
+#include <list>
+
+#include <mgp.hpp>
+
+constexpr std::string_view kResultCS = "contains";
+
+void ContainsSorted(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    bool contains{false};
+    const auto &list = arguments[0].ValueList();
+    const auto &element = arguments[1];
+
+    int left{0};
+    int right{list.Size() - 1};
+    int check;
+
+    while (left <= right) {
+      check = (left + right) / 2;
+      if (list[check] == element) {
+        contains = true;
+        break;
+      } else if (element < list[check]) {
+        right = check - 1;
+      } else {
+        left = check + 1;
+      }
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultCS).c_str(), contains);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -1,9 +1,21 @@
 #include <mgp.hpp>
 
+#include "algorithms/containsSorted.hpp"
+
+constexpr std::string_view kReturnCS = "contains";
+
+constexpr std::string_view kProcedureCS = "containsSorted";
+
+constexpr std::string_view kArgumentInputList = "input_list";
+constexpr std::string_view kArgumentElement = "element";
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  mgp::memory = memory;
   try {
-   
+    AddProcedure(ContainsSorted, kProcedureCS, mgp::ProcedureType::Read,
+                 {mgp::Parameter(kArgumentInputList, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(kArgumentElement, mgp::Type::Any)},
+                 {mgp::Return(kReturnCS, mgp::Type::Bool)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
