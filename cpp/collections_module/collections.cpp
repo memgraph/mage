0ee631c3ee64006017e4969d2435c045c4f63139
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-07-24 09:37:13 +0200

    Implement sum procedure (#236)

--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -1,9 +1,19 @@
 #include <mgp.hpp>
 
+#include "algorithms/sum.hpp"
+
+constexpr std::string_view kReturnSum = "sum";
+
+constexpr std::string_view kProcedureSum = "sum";
+
+constexpr std::string_view kInputList = "input_list";
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  mgp::memory = memory;
   try {
-   
+    AddProcedure(Sum, kProcedureSum, mgp::ProcedureType::Read,
+                 {mgp::Parameter(kInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(kReturnSum, mgp::Type::Double)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
