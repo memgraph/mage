eb91fabb0403def4578f9369e81479d9138801fa
Matija <pintamato@gmail.com>
2023-07-24 10:26:28 +0200

    Ran clang-format

--- cpp/collections_module/algorithms/collections.hpp
+++ cpp/collections_module/algorithms/collections.hpp
@@ -5,25 +5,22 @@ constexpr std::string_view kProcedureUnionAll = "unionAll";
 constexpr std::string_view kArgumentList1UnionAll = "list1";
 constexpr std::string_view kArgumentList2UnionAll = "list2";
 
-
-
-void unionAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory){
+void unionAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
-  try{
+  try {
     mgp::List list1 = arguments[0].ValueList();
     mgp::List list2 = arguments[1].ValueList();
-    
-    for (size_t i = 0; i < list2.Size(); i++){
-        list1.AppendExtend(std::move(list2[i]));
+
+    for (size_t i = 0; i < list2.Size(); i++) {
+      list1.AppendExtend(std::move(list2[i]));
     }
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kReturnValueUnionAll).c_str(), list1);
 
-  }catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
     return;
   }
 }
-
