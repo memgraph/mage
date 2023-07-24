4e59a86c7d456fa1d3f457ceecbe41a25955b7aa
idoraban <idora.ban@memgraph.io>
2023-07-24 12:26:52 +0000

    change structure and edit by review comments

--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -1,19 +1,13 @@
 #include <mgp.hpp>
 
-#include "algorithms/pairs.hpp"
-
-constexpr std::string_view kReturnPairs = "pairs";
-
-constexpr std::string_view kProcedurePairs = "pairs";
-
-constexpr std::string_view kInputList = "inputList";
+#include "algorithms/algorithms.hpp"
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   mgp::memory = memory;
   try {
-    AddProcedure(Pairs, kProcedurePairs, mgp::ProcedureType::Read,
-                 {mgp::Parameter(kInputList, {mgp::Type::List, mgp::Type::Any})},
-                 {mgp::Return(kReturnPairs, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+    AddProcedure(Collections::Pairs, Collections::kProcedurePairs, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnPairs, {mgp::Type::List, mgp::Type::Any})}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
