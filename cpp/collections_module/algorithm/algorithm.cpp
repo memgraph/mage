d2fa944eb4bfff5c0e1fd862dfa2581e9e21ba11
Matija <pintamato@gmail.com>
2023-07-25 10:08:23 +0200

    Run clang-format

--- cpp/collections_module/algorithm/algorithm.cpp
+++ cpp/collections_module/algorithm/algorithm.cpp
@@ -1,25 +1,25 @@
 #include "algorithm.hpp"
 #include <unordered_set>
 
-void Collections::toSet(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory){
-    mgp::memory = memory;
-    auto arguments = mgp::List(args);
-    const auto record_factory = mgp::RecordFactory(result);
-    try {
-        mgp::List list = arguments[0].ValueList();
-        std::unordered_set<mgp::Value, std::hash<mgp::Value>> set;
-        mgp::List return_list;
-        for(auto list_elem: list){
-            if(set.find(list_elem) == set.end()){
-                set.insert(list_elem);
-                return_list.AppendExtend(std::move(list_elem));
-            }
-        }
-        auto record = record_factory.NewRecord();
-        record.Insert(std::string(kReturnToSet).c_str(), std::move(return_list));
+void Collections::toSet(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    mgp::List list = arguments[0].ValueList();
+    std::unordered_set<mgp::Value, std::hash<mgp::Value>> set;
+    mgp::List return_list;
+    for (auto list_elem : list) {
+      if (set.find(list_elem) == set.end()) {
+        set.insert(list_elem);
+        return_list.AppendExtend(std::move(list_elem));
+      }
+    }
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kReturnToSet).c_str(), std::move(return_list));
 
-    }catch (const std::exception &e) {
-        record_factory.SetErrorMessage(e.what());
-        return;
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
   }
 }
--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -3,11 +3,10 @@
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
-     mgp::memory = memory;
-     AddProcedure(Collections::toSet, Collections::kProcedureToSet, mgp::ProcedureType::Read,
+    mgp::memory = memory;
+    AddProcedure(Collections::toSet, Collections::kProcedureToSet, mgp::ProcedureType::Read,
                  {mgp::Parameter(Collections::kArgumentListToSet, {mgp::Type::List, mgp::Type::Any})},
                  {mgp::Return(Collections::kReturnToSet, {mgp::Type::List, mgp::Type::Any})}, module, memory);
-   
 
   } catch (const std::exception &e) {
     return 1;
