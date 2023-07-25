0fa7d6b29a3d7c23431b29033a59bbaac7b8aa8a
Matija <pintamato@gmail.com>
2023-07-25 09:03:02 +0200

    Implement collections.toSet and tests

---
+++ cpp/collections_module/algorithm/algorithm.hpp
@@ -0,0 +1,10 @@
+#include <mgp.hpp>
+#include <string>
+
+namespace Collections{
+
+    constexpr std::string_view kReturnToSet= "result";
+    constexpr std::string_view kProcedureToSet= "toSet";
+    constexpr std::string_view kArgumentListToSet= "list";
+    void toSet(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+}
--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -1,8 +1,12 @@
 #include <mgp.hpp>
-
+#include "algorithm/algorithm.hpp"
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
+     mgp::memory = memory;
+     AddProcedure(Collections::toSet, Collections::kProcedureToSet, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentListToSet, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnToSet, {mgp::Type::List, mgp::Type::Any})}, module, memory);
    
 
   } catch (const std::exception &e) {
