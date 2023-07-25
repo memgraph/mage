e5a1ab7f4dec572983c8cbe0f260f47bafd91026
Matija <pintamato@gmail.com>
2023-07-25 15:51:21 +0200

    Implement PR changes

--- cpp/collections_module/algorithm/algorithm.cpp
+++ cpp/collections_module/algorithm/algorithm.cpp
@@ -7,13 +7,10 @@ void Collections::toSet(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *r
   const auto record_factory = mgp::RecordFactory(result);
   try {
     mgp::List list = arguments[0].ValueList();
-    std::unordered_set<mgp::Value, std::hash<mgp::Value>> set;
+    std::unordered_set<mgp::Value> set(list.begin(), list.end());
     mgp::List return_list;
-    for (auto list_elem : list) {
-      if (set.find(list_elem) == set.end()) {
-        set.insert(list_elem);
-        return_list.AppendExtend(std::move(list_elem));
-      }
+    for (auto elem : set) {
+      return_list.AppendExtend(std::move(elem));
     }
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kReturnToSet).c_str(), std::move(return_list));
--- cpp/collections_module/algorithm/algorithm.hpp
+++ cpp/collections_module/algorithm/algorithm.hpp
@@ -4,7 +4,7 @@
 namespace Collections {
 
 constexpr std::string_view kReturnToSet = "result";
-constexpr std::string_view kProcedureToSet = "toSet";
+constexpr std::string_view kProcedureToSet = "to_set";
 constexpr std::string_view kArgumentListToSet = "list";
 void toSet(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Collections
