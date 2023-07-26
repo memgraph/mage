296870a1cb29276d41c9f8da7047049bfb66ae5a
Matija <pintamato@gmail.com>
2023-07-26 10:45:04 +0200

    Style change

--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -5,7 +5,7 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
-
+  
   } catch (const std::exception &e) {
     return 1;
   }
