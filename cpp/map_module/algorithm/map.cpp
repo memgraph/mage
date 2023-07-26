9567a56aecaee9dd43e6d203acbebf46786a0be6
idoraban <idora.ban@memgraph.io>
2023-07-26 12:39:56 +0000

    edit by review comments

--- cpp/map_module/algorithm/map.cpp
+++ cpp/map_module/algorithm/map.cpp
@@ -3,27 +3,29 @@
 
 #include "map.hpp"
 
+const auto number_of_elements_in_pair = 2;
+
 void Map::FromPairs(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
   try {
-    const auto &list = arguments[0].ValueList();
+    const auto list = arguments[0].ValueList();
 
-    mgp::Map pairsMap;
+    mgp::Map pairs_map;
 
     for (const auto inside_list : list) {
-      if (inside_list.ValueList().Size() != 2) {
-        throw mgp::IndexException("Pairs must consist of 2 elements exactly.");
+      if (inside_list.ValueList().Size() != number_of_elements_in_pair) {
+        throw mgp::IndexException("Number of elements in a pair is not right.");
       }
       if (!inside_list.ValueList()[0].IsString()) {
         throw mgp::ValueException("All keys have to be type string.");
       }
-      pairsMap.Update(inside_list.ValueList()[0].ValueString(), std::move(inside_list.ValueList()[1]));
+      pairs_map.Update(inside_list.ValueList()[0].ValueString(), std::move(inside_list.ValueList()[1]));
     }
 
     auto record = record_factory.NewRecord();
-    record.Insert(std::string(kResultFromPairs).c_str(), std::move(pairsMap));
+    record.Insert(std::string(kResultFromPairs).c_str(), std::move(pairs_map));
 
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
