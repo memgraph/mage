060bfe97a100e0ea64e86c801e92a064af2a1607
Matija <pintamato@gmail.com>
2023-07-26 11:04:40 +0200

    Implement fromLists

--- cpp/map_module/algorithm/map.cpp
+++ cpp/map_module/algorithm/map.cpp
@@ -1,1 +1,28 @@
 #include "map.hpp"
+
+
+void Map::FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    mgp::List list1 = arguments[0].ValueList();
+    mgp::List list2 = arguments[1].ValueList();
+
+    const size_t n = list1.Size();    
+    if(n != list2.Size()){
+        throw mgp::ValueException("Lists must be of same size");
+    }
+    mgp::Map result = mgp::Map();
+    for(size_t i = 0; i < n; i++){
+        result.Update(std::move(list1[i].ValueString()), std::move(list2[i]));
+    }
+    std::cout<<list1.Size()<<std::endl;
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(Map::kReturnListFromLists).c_str(), std::move(result));
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -2,4 +2,10 @@
 
 #include <mgp.hpp>
 
-namespace Map {}  // namespace Map
+namespace Map {
+    constexpr std::string_view kProcedureFromLists = "from_lists";
+    constexpr std::string_view kArgumentListKeysFromLists = "list_keys";
+    constexpr std::string_view kArgumentListValuesFromLists = "list_values";
+    constexpr std::string_view kReturnListFromLists = "result";
+    void FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+}  // namespace Map
