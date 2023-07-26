499e0e4f3a9c58cd693312c18e8101545943adde
idoraban <idora.ban@memgraph.io>
2023-07-26 14:04:54 +0000

    implement map.removeKey and e2e tests for it

--- cpp/map_module/algorithm/map.cpp
+++ cpp/map_module/algorithm/map.cpp
@@ -1,11 +1,19 @@
 #include "map.hpp"
 
-void Map::removeRecursion(mgp::Map &removed, bool recursive, std::string_view key) {
-  for (auto element : removed) {
+void Map::removeRecursion(mgp::Map &result, bool recursive, std::string_view key) {
+  for (auto element : result) {
     if (element.key == key) {
-      removed.Erase(key);
-    } else if (element.value.IsMap() && recursive) {
-      // removeRecursion(, recursive, key);
+      result.Erase(element.key);
+      continue;
+    }
+    if (element.value.IsMap() && recursive) {
+      mgp::Map non_const_value_map = mgp::Map(std::move(element.value.ValueMap()));
+      removeRecursion(non_const_value_map, recursive, key);
+      if (non_const_value_map.Empty()) {
+        result.Erase(element.key);
+        continue;
+      }
+      result.Update(element.key, mgp::Value(std::move(non_const_value_map)));
     }
   }
 }
@@ -15,10 +23,11 @@ void Map::RemoveKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *resul
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
   try {
-    auto map = arguments[0].ValueMap();
+    const auto map = arguments[0].ValueMap();
     const auto key = arguments[1].ValueString();
     const auto recursive_map = arguments[2].ValueMap();
 
+    mgp::Map map_removed = mgp::Map(std::move(map));
     bool recursive = false;
 
     if (!recursive_map.Empty() && !recursive_map.At("recursive").IsBool()) {
@@ -30,10 +39,10 @@ void Map::RemoveKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *resul
       recursive = true;
     }
 
-    removeRecursion(map, recursive, key);
+    removeRecursion(map_removed, recursive, key);
 
     auto record = record_factory.NewRecord();
-    record.Insert(std::string(kResultRemoveKey).c_str(), std::move(map));
+    record.Insert(std::string(kResultRemoveKey).c_str(), std::move(map_removed));
 
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -15,6 +15,6 @@ constexpr std::string_view kArgumentsRecursiveMap = "recursive_map";
 constexpr std::string_view kResultRemoveKey = "removed";
 
 void RemoveKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-void removeRecursion(mgp::Map &removed, bool recursive, std::string_view key);
+void removeRecursion(mgp::Map &result, bool recursive, std::string_view key);
 
 }  // namespace Map
