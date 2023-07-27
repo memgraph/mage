7211cd4183cf84a0425cb18ca57bae47c807f28a
imilinovic <44698587+imilinovic@users.noreply.github.com>
2023-07-27 15:08:40 +0200

    [E-ivan < T422] Implement collections.containsAll (#251)
    implement containsAll

--- cpp/collections_module/algorithm/algorithm.cpp
+++ cpp/collections_module/algorithm/algorithm.cpp
@@ -1,5 +1,6 @@
 #include "algorithm.hpp"
 #include <sstream>
+#include <unordered_set>
 #include "mgp.hpp"
 
 void Collections::SumLongs(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
@@ -49,6 +50,27 @@ void Collections::Avg(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *res
 
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kResultAverage).c_str(), average);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Collections::ContainsAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto list1{arguments[0].ValueList()};
+    std::unordered_set<mgp::Value> set{list1.begin(), list1.end()};
+    const auto list2{arguments[1].ValueList()};
+    std::unordered_set<mgp::Value> values{list2.begin(), list2.end()};
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultContainsAll).c_str(),
+                  std::all_of(values.begin(), values.end(), [&](const auto &x) { return set.contains(x); }));
 
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
--- cpp/collections_module/collections.cpp
+++ cpp/collections_module/collections.cpp
@@ -15,6 +15,11 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     AddProcedure(Collections::Avg, Collections::kProcedureAvg, mgp::ProcedureType::Read, {parameter_list_of_any},
                  {mgp::Return(Collections::kReturnAvg, mgp::Type::Double)}, module, memory);
 
+    AddProcedure(Collections::ContainsAll, Collections::kProcedureContainsAll, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kAnyList, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kAnyList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnContainsAll, mgp::Type::Bool)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
