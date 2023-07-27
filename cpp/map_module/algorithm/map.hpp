ae0bae6698f01980019d50f2503f548db7aa42b0
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-07-27 11:49:22 +0200

    [E-idora < T394] Implement merge for maps (#258)
    Implement map.merge function and e2e tests for it

--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -2,4 +2,17 @@
 
 #include <mgp.hpp>
 
-namespace Map {}  // namespace Map
+namespace Map {
+
+constexpr std::string_view kReturnMerge = "merged";
+
+constexpr std::string_view kProcedureMerge = "merge";
+
+constexpr std::string_view kArgumentsInputMap1 = "input_map1";
+constexpr std::string_view kArgumentsInputMap2 = "input_map2";
+
+constexpr std::string_view kResultMerge = "merged";
+
+void Merge(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Map
--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -5,7 +5,10 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
-
+    AddProcedure(Map::Merge, Map::kProcedureMerge, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kArgumentsInputMap1, mgp::Type::Map),
+                  mgp::Parameter(Map::kArgumentsInputMap2, mgp::Type::Map)},
+                 {mgp::Return(Map::kReturnMerge, mgp::Type::Map)}, module, memory);
   } catch (const std::exception &e) {
     return 1;
   }
