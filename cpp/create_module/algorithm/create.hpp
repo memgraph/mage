4021a2b23851a19e62304dab7e870329886ba525
imilinovic <ivan.milinovic123@gmail.com>
2023-07-27 12:42:25 +0200

    implement create.relationship

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -1,5 +1,19 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <string_view>
 
-namespace Create {}  // namespace Create
+namespace Create {
+
+constexpr const std::string_view kProcedureRelationship = "relationship";
+
+constexpr const std::string_view kRelationshipArg1 = "from";
+constexpr const std::string_view kRelationshipArg2 = "relationshipType";
+constexpr const std::string_view kRelationshipArg3 = "properties";
+constexpr const std::string_view kRelationshipArg4 = "to";
+
+constexpr const std::string_view kResultRelationship = "relationship";
+
+void Relationship(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Create
--- cpp/create_module/create_module.cpp
+++ cpp/create_module/create_module.cpp
@@ -6,6 +6,13 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    AddProcedure(Create::Relationship, Create::kProcedureRelationship, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kRelationshipArg1, mgp::Type::Node),
+                  mgp::Parameter(Create::kRelationshipArg2, mgp::Type::String),
+                  mgp::Parameter(Create::kRelationshipArg3, mgp::Type::Map),
+                  mgp::Parameter(Create::kRelationshipArg4, mgp::Type::Node)},
+                 {mgp::Return(Create::kResultRelationship, mgp::Type::Relationship)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
