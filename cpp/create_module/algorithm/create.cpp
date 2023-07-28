f4ebcea99f581e40ea41933f306547e7f282b1bd
Matija <pintamato@gmail.com>
2023-07-28 16:11:50 +0200

    Minor fix

--- cpp/create_module/algorithm/create.cpp
+++ cpp/create_module/algorithm/create.cpp
@@ -8,12 +8,12 @@ void Create::SetProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *
     mgp::Graph graph = mgp::Graph(memgraph_graph);
     const mgp::Node node_input = arguments[0].ValueNode();
     const std::string string(arguments[1].ValueString());
-    auto record = record_factory.NewRecord();
     mgp::Value value = arguments[2];
     const int64_t id = node_input.Id().AsInt();
     for (auto node : graph.Nodes()) {
       if (node.Id().AsInt() == id) {
         node.SetProperty(string, std::move(value));
+        auto record = record_factory.NewRecord();
         record.Insert(std::string(Create::kReturntSetProperty).c_str(), node);
       }
     }
