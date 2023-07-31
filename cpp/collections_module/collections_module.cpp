ddea07f0fe4b462be698419dd15d1d2cb782bf19
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-07-31 09:05:06 +0200

    [E-add-collections < E-idora-collections] Add collections implementations part 3 (#265)
    Implement collections procedures

---
+++ cpp/collections_module/collections_module.cpp
@@ -0,0 +1,52 @@
+#include <mgp.hpp>
+
+#include "algorithm/collections.hpp"
+
+extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  try {
+    mgp::memory = memory;
+
+    AddProcedure(Collections::RemoveAll, Collections::kProcedureRemoveAll, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentsInputList, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kArgumentsRemoveList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnRemoveAll, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(Collections::Sum, Collections::kProcedureSum, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnSum, mgp::Type::Double)}, module, memory);
+
+    AddProcedure(Collections::Union, Collections::kProcedureUnion, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentsInputList1, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kArgumentsInputList2, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnUnion, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(Collections::Sort, Collections::kProcedureSort, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentsInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnSort, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(Collections::ContainsSorted, Collections::kProcedureCS, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentInputList, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kArgumentElement, mgp::Type::Any)},
+                 {mgp::Return(Collections::kReturnCS, mgp::Type::Bool)}, module, memory);
+
+    AddProcedure(Collections::Max, Collections::kProcedureMax, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentsInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnMax, mgp::Type::Any)}, module, memory);
+
+    AddProcedure(Collections::Split, Collections::kProcedureSplit, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentInputList, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kArgumentDelimiter, mgp::Type::Any)},
+                 {mgp::Return(Collections::kReturnSplit, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(Collections::Pairs, Collections::kProcedurePairs, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kInputList, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnPairs, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+  } catch (const std::exception &e) {
+    return 1;
+  }
+
+  return 0;
+}
+
+extern "C" int mgp_shutdown_module() { return 0; }
