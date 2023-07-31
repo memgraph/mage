44c92dacabdebc51197dff5221780026006fc157
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-07-31 15:08:32 +0200

    [E-idora < T455-MAGE] Implement node for create functions (#271)
    Implement create.node procedure

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -2,4 +2,17 @@
 
 #include <mgp.hpp>
 
-namespace Create {}  // namespace Create
+namespace Create {
+
+constexpr std::string_view kReturnNode = "node";
+
+constexpr std::string_view kProcedureNode = "node";
+
+constexpr std::string_view kArgumentsLabelsList = "labels";
+constexpr std::string_view kArgumentsProperties = "properties";
+
+constexpr std::string_view kResultNode = "node";
+
+void Node(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Create
