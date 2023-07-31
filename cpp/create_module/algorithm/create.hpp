8b484c11e9f379ec392fa17206e3c5e150a4d36e
imilinovic <ivan.milinovic123@gmail.com>
2023-07-31 15:53:07 +0200

    add tests for create.relationship

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -5,13 +5,12 @@
 
 namespace Create {
 
+/* relationship constants */
 constexpr const std::string_view kProcedureRelationship = "relationship";
-
 constexpr const std::string_view kRelationshipArg1 = "from";
 constexpr const std::string_view kRelationshipArg2 = "relationshipType";
 constexpr const std::string_view kRelationshipArg3 = "properties";
 constexpr const std::string_view kRelationshipArg4 = "to";
-
 constexpr const std::string_view kResultRelationship = "relationship";
 
 void Relationship(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
