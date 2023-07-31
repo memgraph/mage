39fc1f528f13f068d2bde5127b504db2bd948372
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-07-31 09:06:27 +0200

    [E-add-map-functions < E-idora] Merge all map implementations part 1 (#266)
    Add map implementations (TO-DO remove non const map in removeKey)

--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -2,4 +2,29 @@
 
 #include <mgp.hpp>
 
-namespace Map {}  // namespace Map
+namespace Map {
+
+constexpr std::string_view kReturnRemoveKey = "removed";
+constexpr std::string_view kProcedureRemoveKey = "remove_key";
+constexpr std::string_view kArgumentsInputMap = "input_map";
+constexpr std::string_view kArgumentsKey = "key";
+constexpr std::string_view kArgumentsIsRecursive = "recursive_map";
+constexpr std::string_view kResultRemoveKey = "removed";
+
+constexpr std::string_view kReturnFromPairs = "map";
+constexpr std::string_view kProcedureFromPairs = "from_pairs";
+constexpr std::string_view kArgumentsInputList = "input_list";
+constexpr std::string_view kResultFromPairs = "map";
+
+constexpr std::string_view kReturnMerge = "merged";
+constexpr std::string_view kProcedureMerge = "merge";
+constexpr std::string_view kArgumentsInputMap1 = "input_map1";
+constexpr std::string_view kArgumentsInputMap2 = "input_map2";
+constexpr std::string_view kResultMerge = "merged";
+
+void RemoveKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void RemoveRecursion(mgp::Map &result, bool recursive, std::string_view key);
+void FromPairs(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void Merge(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Map
--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -6,6 +6,21 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    AddProcedure(
+        Map::RemoveKey, Map::kProcedureRemoveKey, mgp::ProcedureType::Read,
+        {mgp::Parameter(Map::kArgumentsInputMap, mgp::Type::Map), mgp::Parameter(Map::kArgumentsKey, mgp::Type::String),
+         mgp::Parameter(Map::kArgumentsIsRecursive, mgp::Type::Bool, false)},
+        {mgp::Return(Map::kReturnRemoveKey, mgp::Type::Map)}, module, memory);
+
+    AddProcedure(Map::FromPairs, Map::kProcedureFromPairs, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kArgumentsInputList, {mgp::Type::List, mgp::Type::List})},
+                 {mgp::Return(Map::kReturnFromPairs, {mgp::Type::Map, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(Map::Merge, Map::kProcedureMerge, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kArgumentsInputMap1, mgp::Type::Map),
+                  mgp::Parameter(Map::kArgumentsInputMap2, mgp::Type::Map)},
+                 {mgp::Return(Map::kReturnMerge, mgp::Type::Map)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
