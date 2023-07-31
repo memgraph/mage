a31573ba3487eca8a55996e4d433203eb6211c8a
Matija <pintamato@gmail.com>
2023-07-31 14:08:55 +0200

    Prepare for merge

--- cpp/map_module/algorithm/map.cpp
+++ cpp/map_module/algorithm/map.cpp
@@ -1,11 +1,11 @@
 #include "map.hpp"
 
-void Map::flattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key,
+void Map::FlattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key,
                            const std::string &delimiter) {
   for (auto element : input) {
     std::string el_key(element.key);
     if (element.value.IsMap()) {
-      flattenRecursion(result, element.value.ValueMap(), key + el_key + delimiter, delimiter);
+      FlattenRecursion(result, element.value.ValueMap(), key + el_key + delimiter, delimiter);
     } else {
       result.Insert(key + el_key, element.value);
     }
@@ -20,7 +20,7 @@ void Map::Flatten(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result,
     const mgp::Map map = arguments[0].ValueMap();
     const std::string delimiter(arguments[1].ValueString());
     mgp::Map result_map = mgp::Map();
-    flattenRecursion(result_map, map, "", delimiter);
+    FlattenRecursion(result_map, map, "", delimiter);
     auto record = record_factory.NewRecord();
     record.Insert(std::string(Map::kReturnValueFlatten).c_str(), std::move(result_map));
 
--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -4,25 +4,25 @@
 #include <string>
 #include <unordered_set>
 namespace Map {
+//Flatten constants
 constexpr std::string_view kReturnValueFlatten = "result";
 constexpr std::string_view kProcedureFlatten = "flatten";
 constexpr std::string_view kArgumentMapFlatten = "map";
 constexpr std::string_view kArgumentDelimiterFlatten = "delimiter";
-void Flatten(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-void flattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key, const std::string &delimiter);
+//fromLists constants
 constexpr std::string_view kProcedureFromLists = "from_lists";
 constexpr std::string_view kArgumentListKeysFromLists = "list_keys";
 constexpr std::string_view kArgumentListValuesFromLists = "list_values";
 constexpr std::string_view kReturnListFromLists = "result";
-void FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-
+//removeKeys constants
 constexpr std::string_view kReturnRemoveKeys = "result";
-
 constexpr std::string_view kProcedureRemoveKeys = "remove_keys";
-
 constexpr std::string_view kArgumentsInputMapRemoveKeys = "input_map";
 constexpr std::string_view kArgumentsKeysListRemoveKeys = "keys_list";
 constexpr std::string_view kArgumentsRecursiveRemoveKeys = "recursive";
+void Flatten(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void FlattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key, const std::string &delimiter);
+void FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void RemoveRecursionSet(mgp::Map &result, bool recursive, std::unordered_set<std::string_view> &set);
 void RemoveKeys(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Map
