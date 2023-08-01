9743b9139ce3ce7994a45f195689ca24536f8a1f
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-01 11:32:38 +0200

    [E-matija < T462] implement setProperty for create module (#269)
    * Implement create.setProperty
    
    * Minor fix
    
    * Fix O(n) to O(logn) complexity when getting node

--- cpp/create_module/algorithm/create.cpp
+++ cpp/create_module/algorithm/create.cpp
@@ -1,1 +1,22 @@
 #include "create.hpp"
+
+void Create::SetProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    mgp::Graph graph = mgp::Graph(memgraph_graph);
+    const mgp::Node node_input = arguments[0].ValueNode();
+    const std::string string(arguments[1].ValueString());
+    mgp::Value value = arguments[2];
+    const mgp::Id id = node_input.Id();
+    mgp::Node node = graph.GetNodeById(id);
+    node.SetProperty(string, std::move(value));
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(Create::kReturntSetProperty).c_str(), node);
+     
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -2,4 +2,12 @@
 
 #include <mgp.hpp>
 
-namespace Create {}  // namespace Create
+#include <string>
+namespace Create {
+constexpr std::string_view kProcedureSetProperty = "set_property";
+constexpr std::string_view kArgumentNodeSetProperty = "node";
+constexpr std::string_view kArgumentKeySetProperty = "key";
+constexpr std::string_view kArgumentValueSetProperty = "value";
+constexpr std::string_view kReturntSetProperty = "node";
+void SetProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+}  // namespace Create
