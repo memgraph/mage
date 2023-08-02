ef6a1200a1ee6b5f27d19e4579db22a8c997734a
imilinovic <44698587+imilinovic@users.noreply.github.com>
2023-08-02 12:46:19 +0200

    [E-map < E-ivan] Implement map procedures (#272)
    * implement map functions

--- cpp/map_module/algorithm/map.cpp
+++ cpp/map_module/algorithm/map.cpp
@@ -6,6 +6,105 @@
 
 const auto number_of_elements_in_pair = 2;
 
+void Map::FromNodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto label{arguments[0].ValueString()};
+    const auto property{arguments[1].ValueString()};
+    mgp::Map map_result{};
+
+    const auto all_nodes = mgp::Graph(memgraph_graph).Nodes();
+    for (const auto node : all_nodes) {
+      if (!node.HasLabel(label) || !node.Properties().contains(std::string(property))) continue;
+
+      std::ostringstream oss;
+      oss << node.GetProperty(std::string(property));
+      const auto key = oss.str();
+
+      mgp::Map map{};
+      map.Update("identity", mgp::Value(node.Id().AsInt()));
+
+      mgp::List labels{};
+      for (const auto &label : node.Labels()) {
+        labels.AppendExtend(mgp::Value(label));
+      }
+      map.Update("labels", mgp::Value(std::move(labels)));
+
+      const auto property_map = node.Properties();
+      mgp::Map properties{};
+      for (const auto &[key, value] : property_map) {
+        properties.Insert(key, value);
+      }
+      map.Update("properties", mgp::Value(std::move(properties)));
+
+      map_result.Update(key, mgp::Value(std::move(map)));
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultFromNodes).c_str(), map_result);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Map::FromValues(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto values{arguments[0].ValueList()};
+    mgp::Map map{};
+
+    if (values.Size() % 2) {
+      throw mgp::ValueException("List needs to have an even number of elements");
+    }
+
+    auto iterator = values.begin();
+    while (iterator != values.end()) {
+      std::ostringstream oss;
+      oss << *iterator;
+      const auto key = oss.str();
+
+      ++iterator;
+      map.Update(key, *iterator);
+      ++iterator;
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultFromValues).c_str(), map);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Map::SetKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    auto map = arguments[0].ValueMap();
+    const auto key{arguments[1].ValueString()};
+    const auto value{arguments[2]};
+    map.Update(key, std::move(value));
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultSetKey).c_str(), std::move(map));
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
 void Map::RemoveRecursion(mgp::Map &result, bool recursive, std::string_view key) {
   for (auto element : result) {
     if (element.key == key) {
@@ -136,15 +235,15 @@ void Map::FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *resul
   auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
   try {
-    mgp::List list1 = arguments[0].ValueList(); 
+    mgp::List list1 = arguments[0].ValueList();
     mgp::List list2 = arguments[1].ValueList();
 
     const auto expected_list_size = list1.Size();
-    if (expected_list_size  != list2.Size() || expected_list_size  == 0) {
+    if (expected_list_size != list2.Size() || expected_list_size == 0) {
       throw mgp::ValueException("Lists must be of same size and not empty");
     }
     mgp::Map result = mgp::Map();
-    for (size_t i = 0; i < expected_list_size ; i++) {
+    for (size_t i = 0; i < expected_list_size; i++) {
       result.Update(std::move(list1[i].ValueString()), std::move(list2[i]));
     }
     auto record = record_factory.NewRecord();
--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -39,6 +39,20 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
                   mgp::Parameter(Map::kArgumentsInputMap2, mgp::Type::Map)},
                  {mgp::Return(Map::kReturnMerge, mgp::Type::Map)}, module, memory);
 
+    AddProcedure(Map::FromNodes, Map::kProcedureFromNodes, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kFromNodesArg1, mgp::Type::String),
+                  mgp::Parameter(Map::kFromNodesArg2, mgp::Type::String)},
+                 {mgp::Return(Map::kResultFromNodes, mgp::Type::Map)}, module, memory);
+
+    AddProcedure(Map::FromValues, Map::kProcedureFromValues, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kFromValuesArg1, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Map::kResultFromValues, mgp::Type::Map)}, module, memory);
+
+    AddProcedure(Map::SetKey, Map::kProcedureSetKey, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Map::kSetKeyArg1, mgp::Type::Map), mgp::Parameter(Map::kSetKeyArg2, mgp::Type::String),
+                  mgp::Parameter(Map::kSetKeyArg3, mgp::Type::Any)},
+                 {mgp::Return(Map::kResultSetKey, mgp::Type::Map)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
