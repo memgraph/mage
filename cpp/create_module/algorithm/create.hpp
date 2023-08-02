4b8aa3949c67ee876f4e775f22ca2775d090e370
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-02 14:04:40 +0200

    [E-matija < T456] Implement nodes function for create module (#275)
    * Implement create.nodes
    
    * PR changes

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -4,6 +4,12 @@
 
 #include <string>
 namespace Create {
+constexpr std::string_view kProcedureNodes = "nodes";
+constexpr std::string_view kArgumentLabelsNodes = "labels";
+constexpr std::string_view kArgumentPropertiesNodes = "properties";
+constexpr std::string_view kReturnNodes = "node";
+void Nodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
 constexpr std::string_view kProcedureSetProperty = "set_property";
 constexpr std::string_view kArgumentNodeSetProperty = "node";
 constexpr std::string_view kArgumentKeySetProperty = "key";
--- cpp/create_module/create_module.cpp
+++ cpp/create_module/create_module.cpp
@@ -5,6 +5,11 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+    AddProcedure(
+        Create::Nodes, std::string(Create::kProcedureNodes).c_str(), mgp::ProcedureType::Write,
+        {mgp::Parameter(std::string(Create::kArgumentLabelsNodes).c_str(), {mgp::Type::List, mgp::Type::String}),
+         mgp::Parameter(std::string(Create::kArgumentPropertiesNodes).c_str(), {mgp::Type::List, mgp::Type::Map})},
+        {mgp::Return(std::string(Create::kReturnNodes).c_str(), mgp::Type::Node)}, module, memory);
     AddProcedure(Create::RemoveProperties, std::string(Create::kProcedureRemoveProperties).c_str(),
                  mgp::ProcedureType::Write,
                  {mgp::Parameter(std::string(Create::kArgumentNodeRemoveProperties).c_str(), mgp::Type::Node),
