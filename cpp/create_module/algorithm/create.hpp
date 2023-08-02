a8f1e082f4d53b9f5d0dbd16ec482327e2a11914
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-02 12:47:37 +0200

    [E-matija < T459] Implement remove properties function for create module (#274)
    * Implement removeKeys
    
    * Write tests
    
    * Implement create.removeKeys
    
    * Add RemoveProperty instead of SetProperty
    
    * Add empty line

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -9,5 +9,12 @@ constexpr std::string_view kArgumentNodeSetProperty = "node";
 constexpr std::string_view kArgumentKeySetProperty = "key";
 constexpr std::string_view kArgumentValueSetProperty = "value";
 constexpr std::string_view kReturntSetProperty = "node";
+
+constexpr std::string_view kProcedureRemoveProperties = "remove_properties";
+constexpr std::string_view kArgumentNodeRemoveProperties = "node";
+constexpr std::string_view kArgumentKeysRemoveProperties = "list_keys";
+constexpr std::string_view kReturntRemoveProperties = "node";
+
 void SetProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void RemoveProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Create
--- cpp/create_module/create_module.cpp
+++ cpp/create_module/create_module.cpp
@@ -5,6 +5,11 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+    AddProcedure(Create::RemoveProperties, std::string(Create::kProcedureRemoveProperties).c_str(),
+                 mgp::ProcedureType::Write,
+                 {mgp::Parameter(std::string(Create::kArgumentNodeRemoveProperties).c_str(), mgp::Type::Node),
+                 mgp::Parameter(std::string(Create::kArgumentKeysRemoveProperties).c_str(),{mgp::Type::List, mgp::Type::String})},
+                 {mgp::Return(std::string(Create::kReturntRemoveProperties).c_str(), mgp::Type::Node)}, module, memory);
     AddProcedure(Create::SetProperty, std::string(Create::kProcedureSetProperty).c_str(), mgp::ProcedureType::Write,
                  {mgp::Parameter(std::string(Create::kArgumentNodeSetProperty).c_str(), mgp::Type::Node),
                   mgp::Parameter(std::string(Create::kArgumentKeySetProperty).c_str(), mgp::Type::String),
