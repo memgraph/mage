9b6e8ddc47115746c5938d911ac7eece6efe742d
Matija <pintamato@gmail.com>
2023-08-02 11:14:57 +0200

    Pr changes

--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -4,22 +4,28 @@
 #include <string>
 #include <unordered_set>
 namespace Map {
-//Flatten constants
+/* flatten constants */
 constexpr std::string_view kReturnValueFlatten = "result";
 constexpr std::string_view kProcedureFlatten = "flatten";
 constexpr std::string_view kArgumentMapFlatten = "map";
 constexpr std::string_view kArgumentDelimiterFlatten = "delimiter";
-//fromLists constants
+
+
+/* from_lists constants */
 constexpr std::string_view kProcedureFromLists = "from_lists";
 constexpr std::string_view kArgumentListKeysFromLists = "list_keys";
 constexpr std::string_view kArgumentListValuesFromLists = "list_values";
 constexpr std::string_view kReturnListFromLists = "result";
-//removeKeys constants
+
+
+/*  remove_keys constants */
 constexpr std::string_view kReturnRemoveKeys = "result";
 constexpr std::string_view kProcedureRemoveKeys = "remove_keys";
 constexpr std::string_view kArgumentsInputMapRemoveKeys = "input_map";
 constexpr std::string_view kArgumentsKeysListRemoveKeys = "keys_list";
 constexpr std::string_view kArgumentsRecursiveRemoveKeys = "recursive";
+
+
 void Flatten(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void FlattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key, const std::string &delimiter);
 void FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -20,7 +20,7 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     AddProcedure(
         Map::RemoveKeys, std::string(Map::kProcedureRemoveKeys).c_str(), mgp::ProcedureType::Read,
         {mgp::Parameter(std::string(Map::kArgumentsInputMapRemoveKeys).c_str(), mgp::Type::Map),
-         mgp::Parameter(std::string(Map::kArgumentsKeysListRemoveKeys).c_str(), {mgp::Type::List, mgp::Type::Any}),
+         mgp::Parameter(std::string(Map::kArgumentsKeysListRemoveKeys).c_str(), {mgp::Type::List, mgp::Type::String}),
          mgp::Parameter(std::string(Map::kArgumentsRecursiveRemoveKeys).c_str(), mgp::Type::Bool, false)},
         {mgp::Return(std::string(Map::kReturnRemoveKeys).c_str(), mgp::Type::Map)}, module, memory);
 
