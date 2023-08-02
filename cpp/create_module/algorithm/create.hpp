0c48719867d0e7aa8f9757f0b25892733c87c6bf
Matija <pintamato@gmail.com>
2023-08-02 14:20:26 +0200

    Prepare create for merge

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -4,18 +4,20 @@
 
 #include <string>
 namespace Create {
+/*nodes constants*/
 constexpr std::string_view kProcedureNodes = "nodes";
 constexpr std::string_view kArgumentLabelsNodes = "labels";
 constexpr std::string_view kArgumentPropertiesNodes = "properties";
 constexpr std::string_view kReturnNodes = "node";
-void Nodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
+/*set_property constants*/
 constexpr std::string_view kProcedureSetProperty = "set_property";
 constexpr std::string_view kArgumentNodeSetProperty = "node";
 constexpr std::string_view kArgumentKeySetProperty = "key";
 constexpr std::string_view kArgumentValueSetProperty = "value";
 constexpr std::string_view kReturntSetProperty = "node";
 
+/*remove_properties constants*/
 constexpr std::string_view kProcedureRemoveProperties = "remove_properties";
 constexpr std::string_view kArgumentNodeRemoveProperties = "node";
 constexpr std::string_view kArgumentKeysRemoveProperties = "list_keys";
@@ -23,4 +25,5 @@ constexpr std::string_view kReturntRemoveProperties = "node";
 
 void SetProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void RemoveProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void Nodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Create
