2eb319cdca89f636965a35841a1406b46f809f64
imilinovic <ivan.milinovic123@gmail.com>
2023-08-02 10:50:29 +0200

    clang-format

--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -10,7 +10,7 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
                  {mgp::Parameter(Map::kFromNodesArg1, mgp::Type::String),
                   mgp::Parameter(Map::kFromNodesArg2, mgp::Type::String)},
                  {mgp::Return(Map::kResultFromNodes, mgp::Type::Map)}, module, memory);
-                 
+
     AddProcedure(Map::FromValues, Map::kProcedureFromValues, mgp::ProcedureType::Read,
                  {mgp::Parameter(Map::kFromValuesArg1, {mgp::Type::List, mgp::Type::Any})},
                  {mgp::Return(Map::kResultFromValues, mgp::Type::Map)}, module, memory);
