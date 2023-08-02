2250c8f81b1ab322698a4dcd641519be3bcee5f4
imilinovic <44698587+imilinovic@users.noreply.github.com>
2023-08-02 11:20:03 +0200

    [E-collections < E-ivan-collections] Add part of collections functions (#234)
    * add collections procedures

--- cpp/collections_module/algorithm/collections.cpp
+++ cpp/collections_module/algorithm/collections.cpp
@@ -1,9 +1,115 @@
+#include "collections.hpp"
 #include <algorithm>
 #include <list>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
-#include <unordered_map>
-#include "collections.hpp"
+
+void Collections::SumLongs(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    int64_t sum{0};
+    const auto list{arguments[0].ValueList()};
+
+    for (const auto list_item : list) {
+      if (!list_item.IsNumeric()) {
+        std::ostringstream oss;
+        oss << list_item.Type();
+        throw mgp::ValueException("Unsupported type for this operation, received type: " + oss.str());
+      }
+      sum += static_cast<int64_t>(list_item.ValueNumeric());
+    }
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultSumLongs).c_str(), sum);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Collections::Avg(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    double average{0};
+    const auto list{arguments[0].ValueList()};
+
+    for (const auto list_item : list) {
+      if (!list_item.IsNumeric()) {
+        std::ostringstream oss;
+        oss << list_item.Type();
+        throw mgp::ValueException("Unsupported type for this operation, received type: " + oss.str());
+      }
+      average += list_item.ValueNumeric();
+    }
+    average /= list.Size();
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultAvg).c_str(), average);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Collections::ContainsAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto list1{arguments[0].ValueList()};
+    std::unordered_set<mgp::Value> set{list1.begin(), list1.end()};
+    const auto list2{arguments[1].ValueList()};
+    std::unordered_set<mgp::Value> values{list2.begin(), list2.end()};
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultContainsAll).c_str(),
+                  std::all_of(values.begin(), values.end(), [&](const auto &x) { return set.contains(x); }));
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Collections::Intersection(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto list1{arguments[0].ValueList()};
+    std::unordered_set<mgp::Value> set1{list1.begin(), list1.end()};
+    const auto list2{arguments[1].ValueList()};
+    std::unordered_set<mgp::Value> set2{list2.begin(), list2.end()};
+
+    if (set1.size() > set2.size()) {
+      std::swap(set1, set2);
+    }
+
+    mgp::List intersection{};
+    for (const auto &element : set1) {
+      if (set2.contains(element)) {
+        intersection.AppendExtend(std::move(element));
+      }
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultIntersection).c_str(), intersection);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
 
 void Collections::RemoveAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
@@ -297,7 +403,6 @@ void Collections::Contains(mgp_list *args, mgp_graph *memgraph_graph, mgp_result
     return;
   }
 }
-
 
 void Collections::UnionAll(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
--- cpp/collections_module/collections_module.cpp
+++ cpp/collections_module/collections_module.cpp
@@ -6,6 +6,23 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    AddProcedure(Collections::SumLongs, Collections::kProcedureSumLongs, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kSumLongsArg1, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kResultSumLongs, mgp::Type::Int)}, module, memory);
+
+    AddProcedure(Collections::Avg, Collections::kProcedureAvg, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kAvgArg1, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kResultAvg, mgp::Type::Double)}, module, memory);
+
+    AddProcedure(Collections::ContainsAll, Collections::kProcedureContainsAll, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kContainsAllArg1, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kContainsAllArg2, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kResultContainsAll, mgp::Type::Bool)}, module, memory);
+
+    AddProcedure(Collections::Intersection, Collections::kProcedureIntersection, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kIntersectionArg1, {mgp::Type::List, mgp::Type::Any}),
+                  mgp::Parameter(Collections::kIntersectionArg2, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kResultIntersection, {mgp::Type::List, mgp::Type::Any})}, module, memory);
     AddProcedure(Collections::RemoveAll, Collections::kProcedureRemoveAll, mgp::ProcedureType::Read,
                  {mgp::Parameter(Collections::kArgumentsInputList, {mgp::Type::List, mgp::Type::Any}),
                   mgp::Parameter(Collections::kArgumentsRemoveList, {mgp::Type::List, mgp::Type::Any})},
@@ -41,29 +58,33 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     AddProcedure(Collections::Pairs, Collections::kProcedurePairs, mgp::ProcedureType::Read,
                  {mgp::Parameter(Collections::kInputList, {mgp::Type::List, mgp::Type::Any})},
                  {mgp::Return(Collections::kReturnPairs, {mgp::Type::List, mgp::Type::Any})}, module, memory);
-    AddProcedure(Collections::Contains, std::string(Collections::kProcedureContains).c_str(), mgp::ProcedureType::Read,
-                {mgp::Parameter(std::string(Collections::kArgumentListContains).c_str(), {mgp::Type::List, mgp::Type::Any}),
-                mgp::Parameter(std::string(Collections::kArgumentValueContains).c_str(), mgp::Type::Any)},
-                {mgp::Return(std::string(Collections::kReturnValueContains).c_str(), mgp::Type::Bool)}, module, memory);
- 
-    AddProcedure(Collections::Min, std::string(Collections::kProcedureMin).c_str(), mgp::ProcedureType::Read,
-                {mgp::Parameter(std::string(Collections::kArgumentListMin).c_str(), {mgp::Type::List, mgp::Type::Any})},
-                {mgp::Return(std::string(Collections::kReturnValueMin).c_str(), mgp::Type::Any)}, module, memory);
-    AddProcedure(Collections::UnionAll, std::string(Collections::kProcedureUnionAll).c_str(), mgp::ProcedureType::Read,
-                {mgp::Parameter(std::string(Collections::kArgumentList1UnionAll).c_str(), {mgp::Type::List, mgp::Type::Any}),
-                mgp::Parameter(std::string(Collections::kArgumentList2UnionAll).c_str(), {mgp::Type::List, mgp::Type::Any})},
-                {mgp::Return(std::string(Collections::kReturnValueUnionAll).c_str(), {mgp::Type::List, mgp::Type::Any})},
-                module, memory);
-      
-    AddProcedure(Collections::ToSet, Collections::kProcedureToSet, mgp::ProcedureType::Read,
-              {mgp::Parameter(Collections::kArgumentListToSet, {mgp::Type::List, mgp::Type::Any})},
-              {mgp::Return(Collections::kReturnToSet, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+    AddProcedure(
+        Collections::Contains, std::string(Collections::kProcedureContains).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Collections::kArgumentListContains).c_str(), {mgp::Type::List, mgp::Type::Any}),
+         mgp::Parameter(std::string(Collections::kArgumentValueContains).c_str(), mgp::Type::Any)},
+        {mgp::Return(std::string(Collections::kReturnValueContains).c_str(), mgp::Type::Bool)}, module, memory);
 
-    AddProcedure(Collections::Partition, std::string(Collections::kProcedurePartition).c_str(), mgp::ProcedureType::Read,
-              {mgp::Parameter(std::string(Collections::kArgumentListPartition).c_str(), {mgp::Type::List, mgp::Type::Any}),
-              mgp::Parameter(std::string(Collections::kArgumentSizePartition).c_str(), mgp::Type::Int)},
-              {mgp::Return(std::string(Collections::kReturnValuePartition).c_str(), {mgp::Type::List, mgp::Type::Any})},
-              module, memory);
+    AddProcedure(
+        Collections::Min, std::string(Collections::kProcedureMin).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Collections::kArgumentListMin).c_str(), {mgp::Type::List, mgp::Type::Any})},
+        {mgp::Return(std::string(Collections::kReturnValueMin).c_str(), mgp::Type::Any)}, module, memory);
+    AddProcedure(
+        Collections::UnionAll, std::string(Collections::kProcedureUnionAll).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Collections::kArgumentList1UnionAll).c_str(), {mgp::Type::List, mgp::Type::Any}),
+         mgp::Parameter(std::string(Collections::kArgumentList2UnionAll).c_str(), {mgp::Type::List, mgp::Type::Any})},
+        {mgp::Return(std::string(Collections::kReturnValueUnionAll).c_str(), {mgp::Type::List, mgp::Type::Any})},
+        module, memory);
+
+    AddProcedure(Collections::ToSet, Collections::kProcedureToSet, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Collections::kArgumentListToSet, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Collections::kReturnToSet, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
+    AddProcedure(
+        Collections::Partition, std::string(Collections::kProcedurePartition).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Collections::kArgumentListPartition).c_str(), {mgp::Type::List, mgp::Type::Any}),
+         mgp::Parameter(std::string(Collections::kArgumentSizePartition).c_str(), mgp::Type::Int)},
+        {mgp::Return(std::string(Collections::kReturnValuePartition).c_str(), {mgp::Type::List, mgp::Type::Any})},
+        module, memory);
 
   } catch (const std::exception &e) {
     return 1;
