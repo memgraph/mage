f4114a2986eca5fb8646f58a83110f611f9ed8f8
idoraban <idora.ban@memgraph.io>
2023-08-02 09:26:30 +0000

    PR changes

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -15,6 +15,7 @@ constexpr std::string_view kArgumentsValues = "input_values";
 
 constexpr std::string_view kResultProperties = "nodes";
 
+void ProcessElement(std::unordered_set<mgp::Id> &result_set, const mgp::Value &element);
 const std::unordered_set<mgp::Id> GetIds(const mgp::Value &argument);
 void SetProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
