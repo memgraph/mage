d91ae342a4367e6af7c5b8b52ff4ecdb173f2739
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-02 12:28:26 +0200

    [E-map < E-matija] Merge E-matija-add-map-functions into E-map (#277)
    * Implement map.flatten and tests
    
    * Start implementing removeKeys
    
    * Style change
    
    * Implement fromLists
    
    * Add std::move in Insert
    
    * Add std::move to result
    
    * Remove obsolete std::move
    
    * Add const arguments
    
    * Remove reference
    
    * Implement map.fromLists
    
    * Add empty line
    
    * Implement removeKeys
    
    * Fix PR changes
    
    * Fix review changes
    
    * Add empty line
    
    * Prepare for merge
    
    * Pr changes

--- cpp/map_module/algorithm/map.hpp
+++ cpp/map_module/algorithm/map.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <string>
+#include <unordered_set>
 
 namespace Map {
 
@@ -22,9 +24,37 @@ constexpr std::string_view kArgumentsInputMap1 = "input_map1";
 constexpr std::string_view kArgumentsInputMap2 = "input_map2";
 constexpr std::string_view kResultMerge = "merged";
 
+/* flatten constants */
+constexpr std::string_view kReturnValueFlatten = "result";
+constexpr std::string_view kProcedureFlatten = "flatten";
+constexpr std::string_view kArgumentMapFlatten = "map";
+constexpr std::string_view kArgumentDelimiterFlatten = "delimiter";
+
+
+/* from_lists constants */
+constexpr std::string_view kProcedureFromLists = "from_lists";
+constexpr std::string_view kArgumentListKeysFromLists = "list_keys";
+constexpr std::string_view kArgumentListValuesFromLists = "list_values";
+constexpr std::string_view kReturnListFromLists = "result";
+
+
+/*  remove_keys constants */
+constexpr std::string_view kReturnRemoveKeys = "result";
+constexpr std::string_view kProcedureRemoveKeys = "remove_keys";
+constexpr std::string_view kArgumentsInputMapRemoveKeys = "input_map";
+constexpr std::string_view kArgumentsKeysListRemoveKeys = "keys_list";
+constexpr std::string_view kArgumentsRecursiveRemoveKeys = "recursive";
+
+
 void RemoveKey(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void RemoveRecursion(mgp::Map &result, bool recursive, std::string_view key);
 void FromPairs(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void Merge(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+void Flatten(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void FlattenRecursion(mgp::Map &result, const mgp::Map &input, const std::string &key, const std::string &delimiter);
+void FromLists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void RemoveRecursionSet(mgp::Map &result, bool recursive, std::unordered_set<std::string_view> &set);
+void RemoveKeys(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 }  // namespace Map
--- cpp/map_module/map_module.cpp
+++ cpp/map_module/map_module.cpp
@@ -5,6 +5,24 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+    AddProcedure(Map::Flatten, std::string(Map::kProcedureFlatten).c_str(), mgp::ProcedureType::Read,
+                 {mgp::Parameter(std::string(Map::kArgumentMapFlatten).c_str(), {mgp::Type::Map, mgp::Type::Any}),
+                  mgp::Parameter(std::string(Map::kArgumentDelimiterFlatten).c_str(), {mgp::Type::String}, ".")},
+                 {mgp::Return(std::string(Map::kReturnValueFlatten).c_str(), {mgp::Type::Map, mgp::Type::Any})}, module,
+                 memory);
+    AddProcedure(
+        Map::FromLists, std::string(Map::kProcedureFromLists).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Map::kArgumentListKeysFromLists).c_str(), {mgp::Type::List, mgp::Type::String}),
+         mgp::Parameter(std::string(Map::kArgumentListValuesFromLists).c_str(), {mgp::Type::List, mgp::Type::Any})},
+        {mgp::Return(std::string(Map::kReturnListFromLists).c_str(), {mgp::Type::Map, mgp::Type::Any})}, module,
+        memory);
+
+    AddProcedure(
+        Map::RemoveKeys, std::string(Map::kProcedureRemoveKeys).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Map::kArgumentsInputMapRemoveKeys).c_str(), mgp::Type::Map),
+         mgp::Parameter(std::string(Map::kArgumentsKeysListRemoveKeys).c_str(), {mgp::Type::List, mgp::Type::String}),
+         mgp::Parameter(std::string(Map::kArgumentsRecursiveRemoveKeys).c_str(), mgp::Type::Bool, false)},
+        {mgp::Return(std::string(Map::kReturnRemoveKeys).c_str(), mgp::Type::Map)}, module, memory);
 
     AddProcedure(
         Map::RemoveKey, Map::kProcedureRemoveKey, mgp::ProcedureType::Read,
