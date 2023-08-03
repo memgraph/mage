59e1cf11233af97c46ca0c1fa33d831d068b72fa
Matija <pintamato@gmail.com>
2023-08-03 18:46:08 +0200

    Add empty lines and formatting change

--- cpp/util_module/algorithm/util.cpp
+++ cpp/util_module/algorithm/util.cpp
@@ -8,7 +8,7 @@ void Util::Md5(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mg
   try {
     const mgp::List arg_list = arguments[0].ValueList();
     std::string return_string = "";
-    for(auto value : arg_list){
+    for (auto value : arg_list) {
       return_string += value.ToString();
     }
     const std::string md5_hash = md5(return_string);
