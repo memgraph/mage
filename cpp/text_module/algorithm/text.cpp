d1585903699f15bafd9af6dca16dee5b77bb7ebc
imilinovic <ivan.milinovic123@gmail.com>
2023-08-03 11:29:41 +0200

    implement text.join

---
+++ cpp/text_module/algorithm/text.cpp
@@ -0,0 +1,29 @@
+#include "text.hpp"
+
+void Text::Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const auto list{arguments[0].ValueList()};
+    const auto delimiter{arguments[1].ValueString()};
+
+    std::string result;
+    if (!list.Empty()) {
+      auto iterator = list.begin();
+      result += (*iterator).ValueString();
+
+      for (++iterator; iterator != list.end(); ++iterator) {
+        result += delimiter;
+        result += (*iterator).ValueString();
+      }
+    }
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultJoin).c_str(), result);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
