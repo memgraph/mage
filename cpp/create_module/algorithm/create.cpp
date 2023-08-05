19638a2f98543fd2712cde26f9e9d501a2b77ef3
idoraban <idora.ban@memgraph.io>
2023-08-05 07:32:08 +0000

    remove unnecessery complexity

--- cpp/create_module/algorithm/create.cpp
+++ cpp/create_module/algorithm/create.cpp
@@ -1,27 +1,27 @@
 #include "create.hpp"
 
-void Create::ProcessElement(std::unordered_set<mgp::Id> &result_set, const mgp::Value &element) {
-  if (element.IsNode()) {
-    result_set.insert(std::move(element.ValueNode().Id()));
-  } else if (element.IsRelationship()) {
-    result_set.insert(std::move(element.ValueRelationship().Id()));
-  } else if (element.IsInt()) {
-    result_set.insert(std::move(mgp::Id::FromInt(element.ValueInt())));
-  } else {
-    throw mgp::ValueException("First argument must be node, relationship, id or a list of those.");
+void Create::SetElementProp(mgp::Relationship &element, const mgp::List &prop_key_list,
+                            const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory) {
+  for (size_t i = 0; i < prop_key_list.Size(); i++) {
+    element.SetProperty(std::string(prop_key_list[i].ValueString()), prop_value_list[i]);
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultRelProp).c_str(), std::move(element));
   }
 }
 
-const std::unordered_set<mgp::Id> Create::GetIds(const mgp::Value &argument) {
-  std::unordered_set<mgp::Id> result_set;
-  if (argument.IsList()) {
-    for (const auto element : argument.ValueList()) {
-      ProcessElement(result_set, element);
-    }
-    return result_set;
+void Create::ProcessElement(const mgp::Value &element, const mgp::Graph graph, const mgp::List &prop_key_list,
+                            const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory,
+                            std::unordered_set<mgp::Id> &relIds) {
+  if (element.IsRelationship()) {
+    auto rel = element.ValueRelationship();
+    SetElementProp(rel, prop_key_list, prop_value_list, record_factory);
+    return;
   }
-  ProcessElement(result_set, argument);
-  return result_set;
+  if (element.IsInt()) {
+    relIds.insert(mgp::Id::FromInt(element.ValueInt()));
+    return;
+  }
+  throw mgp::ValueException("First argument must be node, relationship, id or a list of those.");
 }
 
 void Create::SetRelProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
@@ -30,16 +30,23 @@ void Create::SetRelProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_resul
   const auto graph = mgp::Graph(memgraph_graph);
   const auto record_factory = mgp::RecordFactory(result);
   try {
-    const auto relIds = GetIds(arguments[0]);
+    mgp::List prop_key_list = mgp::List();
+    prop_key_list.AppendExtend(arguments[1]);
+    mgp::List prop_value_list = mgp::List();
+    prop_value_list.AppendExtend(arguments[2]);
 
-    const auto prop_key = arguments[1].ValueString();
-    const auto prop_value = arguments[2];
+    std::unordered_set<mgp::Id> relIds;
 
+    if (arguments[0].IsList()) {
+      for (const auto element : arguments[0].ValueList()) {
+        ProcessElement(element, graph, prop_key_list, prop_value_list, record_factory, relIds);
+      }
+    } else {
+      ProcessElement(arguments[0], graph, prop_key_list, prop_value_list, record_factory, relIds);
+    }
     for (auto rel : graph.Relationships()) {
       if (relIds.contains(rel.Id())) {
-        rel.SetProperty((std::string)prop_key, prop_value);
-        auto record = record_factory.NewRecord();
-        record.Insert(std::string(kResultRelProp).c_str(), std::move(rel));
+        SetElementProp(rel, prop_key_list, prop_value_list, record_factory);
       }
     }
   } catch (const std::exception &e) {
--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -15,8 +15,11 @@ constexpr std::string_view kArgumentsValue = "input_value";
 
 constexpr std::string_view kResultRelProp = "relationship";
 
-void ProcessElement(std::unordered_set<mgp::Id> &result_set, const mgp::Value &element);
-const std::unordered_set<mgp::Id> GetIds(const mgp::Value &argument);
+void SetElementProp(mgp::Relationship &element, const mgp::List &prop_key_list, const mgp::List &prop_value_list,
+                    const mgp::RecordFactory &record_factory);
+void ProcessElement(const mgp::Value &element, const mgp::Graph graph, const mgp::List &prop_key_list,
+                    const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory,
+                    std::unordered_set<mgp::Id> &relIds);
 void SetRelProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 }  // namespace Create
