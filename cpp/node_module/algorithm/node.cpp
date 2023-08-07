3ff012eefd3c97d4472e91a87c6075d8bb7d6ef6
Matija <pintamato@gmail.com>
2023-08-07 17:08:27 +0200

    Implement node.relationshipsExist

--- cpp/node_module/algorithm/node.cpp
+++ cpp/node_module/algorithm/node.cpp
@@ -1,1 +1,48 @@
 #include "node.hpp"
+
+bool Node::RelationshipExist(const mgp::Node &node, std::string &rel_type) {
+  char direction{' '};
+  if (rel_type[rel_type.size() - 1] == '<' || rel_type[rel_type.size() - 1] == '>') {
+    direction = rel_type[rel_type.size() - 1];
+    rel_type.pop_back();
+  }
+  for (auto rel : node.OutRelationships()) {
+    if (std::string(rel.Type()) == rel_type && direction != '<') {
+      return true;
+    }
+  }
+  for (auto rel : node.InRelationships()) {
+    if (std::string(rel.Type()) == rel_type && direction != '>') {
+      return true;
+    }
+  }
+  return false;
+}
+
+void Node::RelationshipsExist(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    const mgp::Node node = arguments[0].ValueNode();
+    const mgp::List relationships = arguments[1].ValueList();
+    if (relationships.Size() == 0) {
+      throw mgp::ValueException("Input relationships list must not be empty!");
+    }
+    mgp::Map relationship_map = mgp::Map();
+    for (auto rel : relationships) {
+      std::string rel_type{rel.ValueString()};
+      if (RelationshipExist(node, rel_type)) {
+        relationship_map.Insert(rel.ValueString(), mgp::Value(true));
+      } else {
+        relationship_map.Insert(rel.ValueString(), mgp::Value(false));
+      }
+    }
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kReturnRelationshipsExist).c_str(), std::move(relationship_map));
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
--- cpp/node_module/algorithm/node.hpp
+++ cpp/node_module/algorithm/node.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <string>
 
-namespace Node {}  // namespace Node
+namespace Node {
+/*relationships_exist constants*/
+constexpr std::string_view kProcedureRelationshipsExist = "relationships_exist";
+constexpr std::string_view kReturnRelationshipsExist = "result";
+constexpr std::string_view kArgumentNodesRelationshipsExist = "node";
+constexpr std::string_view kArgumentRelationshipsRelationshipsExist = "relationships";
+void RelationshipsExist(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+bool RelationshipExist(const mgp::Node &node, std::string &rel_type);
+}  // namespace Node
