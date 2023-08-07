9157fe6992e41b9d801dfe59f9255faa1209cb5c
Matija <pintamato@gmail.com>
2023-08-07 11:43:45 +0200

    Add online test

--- cpp/util_module/algorithm/util.cpp
+++ cpp/util_module/algorithm/util.cpp
@@ -7,13 +7,12 @@ void Util::Md5(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mg
   const auto record_factory = mgp::RecordFactory(result);
   try {
     const mgp::List arg_list = arguments[0].ValueList();
-    std::string return_string = "";
+    std::string return_string{""};
     for (auto value : arg_list) {
       return_string += value.ToString();
     }
-    const std::string md5_hash = md5(return_string);
     auto record = record_factory.NewRecord();
-    record.Insert(std::string(kArgumentResultMd5).c_str(), md5_hash);
+    record.Insert(std::string(kArgumentResultMd5).c_str(), md5(return_string));
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
     return;
