8d21f130cf2bd0b77e73b6e28b6adb825ec6f921
idoraban <idora.ban@memgraph.io>
2023-08-08 12:05:45 +0000

    initial structure

---
+++ cpp/neighbors_module/algorithm/neighbors.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <mgp.hpp>
+
+namespace Neighbors {}  // namespace Neighbors
---
+++ cpp/neighbors_module/neighbors_module.cpp
@@ -0,0 +1,16 @@
+#include <mgp.hpp>
+
+#include "algorithm/neighbors.hpp"
+
+extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
+  try {
+    mgp::memory = memory;
+
+  } catch (const std::exception &e) {
+    return 1;
+  }
+
+  return 0;
+}
+
+extern "C" int mgp_shutdown_module() { return 0; }
