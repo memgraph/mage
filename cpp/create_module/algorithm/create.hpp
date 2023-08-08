c48c9f5161a12f1640c517d60b0a526d3e3d1e83
imilinovic <44698587+imilinovic@users.noreply.github.com>
2023-08-08 08:28:48 +0200

    [E-ivan < T457] Implement create.relationship (#280)
    Implement create.relationship

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -1,5 +1,18 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <string_view>
 
-namespace Create {}  // namespace Create
+namespace Create {
+
+/* relationship constants */
+constexpr const std::string_view kProcedureRelationship = "relationship";
+constexpr const std::string_view kRelationshipArg1 = "from";
+constexpr const std::string_view kRelationshipArg2 = "relationshipType";
+constexpr const std::string_view kRelationshipArg3 = "properties";
+constexpr const std::string_view kRelationshipArg4 = "to";
+constexpr const std::string_view kResultRelationship = "relationship";
+
+void Relationship(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Create
