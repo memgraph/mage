4c7143895c8179c3d37edee7d669425f6be92139
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-08-09 17:57:33 +0200

    [E-idora-create < T461-MAGE] Implement set properties for create functions (#273)
    Implement create.set_properties

--- cpp/create_module/create_module.cpp
+++ cpp/create_module/create_module.cpp
@@ -5,6 +5,13 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+
+    AddProcedure(Create::SetProperties, Create::kProcedureSetProperties, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kArgumentsNodes, mgp::Type::Any),
+                  mgp::Parameter(Create::kArgumentsKeys, {mgp::Type::List, mgp::Type::String}),
+                  mgp::Parameter(Create::kArgumentsValues, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Create::kReturnProperties, mgp::Type::Node)}, module, memory);
+
     AddProcedure(Create::Node, Create::kProcedureNode, mgp::ProcedureType::Write,
                  {mgp::Parameter(Create::kArgumentsLabelsList, {mgp::Type::List, mgp::Type::String}),
                   mgp::Parameter(Create::kArgumentsProperties, mgp::Type::Map)},
