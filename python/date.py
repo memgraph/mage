5a863a376b9fdf3781a001b391c671616148ed48
idoraban <idora.ban@memgraph.io>
2023-08-09 15:54:54 +0000

    format files with black

--- python/date.py
+++ python/date.py
@@ -2,18 +2,32 @@ import mgp
 import pytz
 import datetime
 
-from mage.date.constants import (
-    Conversion,
-    Epoch
-)
+from mage.date.constants import Conversion, Epoch
+
 
 def getOffset(timezone, date):
     offset = pytz.timezone(timezone).utcoffset(date)
-    if (offset.days == 1):
-        return datetime.timedelta(minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE + Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR), False
-    elif (offset.days == -1):
-        return datetime.timedelta(minutes=Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR - offset.seconds // Conversion.SECONDS_IN_MINUTE), True
-    return datetime.timedelta(minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE), False
+    if offset.days == 1:
+        return (
+            datetime.timedelta(
+                minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE
+                + Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+            ),
+            False,
+        )
+    elif offset.days == -1:
+        return (
+            datetime.timedelta(
+                minutes=Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+                - offset.seconds // Conversion.SECONDS_IN_MINUTE
+            ),
+            True,
+        )
+    return (
+        datetime.timedelta(minutes=offset.seconds // Conversion.SECONDS_IN_MINUTE),
+        False,
+    )
+
 
 @mgp.read_proc
 def parse(
@@ -25,26 +39,51 @@ def parse(
     first_date = Epoch.UNIX_EPOCH
     input_date = datetime.datetime.strptime(time, format)
 
-    if (timezone not in pytz.all_timezones):
-        raise Exception("Timezone doesn't exist. Check documentation to see available timezones.")
+    if timezone not in pytz.all_timezones:
+        raise Exception(
+            "Timezone doesn't exist. Check documentation to see available timezones."
+        )
 
     offset, add = getOffset(timezone, input_date)
     tz_input = input_date + offset if add else input_date - offset
 
     time_since = tz_input - first_date
 
-    if (unit == "ms"):
-        parsed = time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR * Conversion.SECONDS_IN_MINUTE * Conversion.MILLISECONDS_IN_SECOND + time_since.seconds * Conversion.MILLISECONDS_IN_SECOND
-    elif (unit == "s"):
-        parsed = time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR * Conversion.SECONDS_IN_MINUTE + time_since.seconds
-    elif (unit == "m"):
-        parsed = time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR + time_since.seconds // Conversion.SECONDS_IN_MINUTE
-    elif (unit == "h"):
-        parsed = time_since.days * Conversion.HOURS_IN_DAY + time_since.seconds // Conversion.SECONDS_IN_MINUTE // Conversion.MINUTES_IN_HOUR
-    elif (unit == "d"):
+    if unit == "ms":
+        parsed = (
+            time_since.days
+            * Conversion.HOURS_IN_DAY
+            * Conversion.MINUTES_IN_HOUR
+            * Conversion.SECONDS_IN_MINUTE
+            * Conversion.MILLISECONDS_IN_SECOND
+            + time_since.seconds * Conversion.MILLISECONDS_IN_SECOND
+        )
+    elif unit == "s":
+        parsed = (
+            time_since.days
+            * Conversion.HOURS_IN_DAY
+            * Conversion.MINUTES_IN_HOUR
+            * Conversion.SECONDS_IN_MINUTE
+            + time_since.seconds
+        )
+    elif unit == "m":
+        parsed = (
+            time_since.days * Conversion.HOURS_IN_DAY * Conversion.MINUTES_IN_HOUR
+            + time_since.seconds // Conversion.SECONDS_IN_MINUTE
+        )
+    elif unit == "h":
+        parsed = (
+            time_since.days * Conversion.HOURS_IN_DAY
+            + time_since.seconds
+            // Conversion.SECONDS_IN_MINUTE
+            // Conversion.MINUTES_IN_HOUR
+        )
+    elif unit == "d":
         parsed = time_since.days
     else:
-        raise Exception("Unit doesn't exist. Check documentation to see available units.")
+        raise Exception(
+            "Unit doesn't exist. Check documentation to see available units."
+        )
 
     return mgp.Record(parsed=parsed)
 
@@ -58,22 +97,28 @@ def format(
 ) -> mgp.Record(formatted=str):
     first_date = Epoch.UNIX_EPOCH
 
-    if (unit == "ms"):
+    if unit == "ms":
         new_date = first_date + datetime.timedelta(milliseconds=time)
-    elif (unit == "s"):
+    elif unit == "s":
         new_date = first_date + datetime.timedelta(seconds=time)
-    elif (unit == "m"):
+    elif unit == "m":
         new_date = first_date + datetime.timedelta(minutes=time)
-    elif (unit == "h"):
+    elif unit == "h":
         new_date = first_date + datetime.timedelta(hours=time)
-    elif (unit == "d"):
+    elif unit == "d":
         new_date = first_date + datetime.timedelta(days=time)
     else:
-        raise Exception("Unit doesn't exist. Check documentation to see available units.")
+        raise Exception(
+            "Unit doesn't exist. Check documentation to see available units."
+        )
 
-    if (timezone not in pytz.all_timezones):
-        raise Exception("Timezone doesn't exist. Check documentation to see available timezones.")
+    if timezone not in pytz.all_timezones:
+        raise Exception(
+            "Timezone doesn't exist. Check documentation to see available timezones."
+        )
     offset, subtract = getOffset(timezone, new_date)
     tz_new = new_date - offset if subtract else new_date + offset
 
-    return mgp.Record(formatted=pytz.timezone(timezone).localize(tz_new).strftime(format))
+    return mgp.Record(
+        formatted=pytz.timezone(timezone).localize(tz_new).strftime(format)
+    )
--- python/mage/date/constants.py
+++ python/mage/date/constants.py
@@ -1,10 +1,12 @@
 import datetime
 
+
 class Conversion(int):
     MINUTES_IN_HOUR = 60
     SECONDS_IN_MINUTE = 60
     MILLISECONDS_IN_SECOND = 1000
     HOURS_IN_DAY = 24
+
 
 class Epoch(datetime.datetime):
     UNIX_EPOCH = datetime.datetime(1970, 1, 1, 0, 0, 0)
