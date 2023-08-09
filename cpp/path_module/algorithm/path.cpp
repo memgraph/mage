3cff90d675538cb12dfc16eeb65642e2a302f113
imilinovic <ivan.milinovic123@gmail.com>
2023-08-09 16:29:12 +0200

    change list to map

--- cpp/path_module/algorithm/path.cpp
+++ cpp/path_module/algorithm/path.cpp
@@ -1,4 +1,5 @@
 #include "path.hpp"
+#include "mgp.hpp"
 
 void Path::Create(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
@@ -6,10 +7,19 @@ void Path::Create(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result,
   const auto record_factory = mgp::RecordFactory(result);
   try {
     auto start_node{arguments[0].ValueNode()};
-    auto relationships{arguments[1].ValueList()};
+    auto relationships{arguments[1].ValueMap()};
 
     mgp::Path path{start_node};
-    for (const auto &relationship : relationships) {
+    for (const auto &relationship : relationships["key"].ValueList()) {
+      if (relationship.IsNull()) {
+        break;
+      }
+      if (!relationship.IsRelationship()) {
+        std::ostringstream oss;
+        oss << relationship.Type();
+        throw mgp::ValueException("Expected relationship or null type, got " + oss.str());
+      }
+
       path.Expand(relationship.ValueRelationship());
     }
 
--- cpp/path_module/path_module.cpp
+++ cpp/path_module/path_module.cpp
@@ -6,10 +6,14 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    auto empty_list = mgp::Value(mgp::List{});
+    auto empty_map = mgp::Map{};
+    empty_map.Insert("key", empty_list);
+
     AddProcedure(Path::Create, Path::kProcedureCreate, mgp::ProcedureType::Read,
-                {mgp::Parameter(Path::kCreateArg1, mgp::Type::Node),
-                mgp::Parameter(Path::kCreateArg2, {mgp::Type::List, mgp::Type::Relationship}, mgp::Value(mgp::List{}))},
-                {mgp::Return(Path::kResultCreate, mgp::Type::Path)}, module, memory);
+                 {mgp::Parameter(Path::kCreateArg1, mgp::Type::Node),
+                  mgp::Parameter(Path::kCreateArg2, {mgp::Type::Map, mgp::Type::List}, mgp::Value(empty_map))},
+                 {mgp::Return(Path::kResultCreate, mgp::Type::Path)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
