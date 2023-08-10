f70e5555f561b1846ce1a7dde6de68ccef6056e7
idoraban <idora.ban@memgraph.io>
2023-08-10 13:22:40 +0000

    add multiple relationships possibility

--- cpp/node_module/algorithm/node.cpp
+++ cpp/node_module/algorithm/node.cpp
@@ -1,11 +1,11 @@
 #include "node.hpp"
 
-bool Node::FindRelationship(std::string_view type, mgp::Relationships relationships) {
-  if (type.size() == 0 && relationships.cbegin() != relationships.cend()) {
+bool Node::FindRelationship(std::unordered_set<std::string_view> types, mgp::Relationships relationships) {
+  if (types.contains("") && relationships.cbegin() != relationships.cend()) {
     return true;
   }
   for (auto relationship : relationships) {
-    if (relationship.Type() == type) {
+    if (types.contains(relationship.Type())) {
       return true;
     }
   }
@@ -18,24 +18,40 @@ void Node::RelExists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *resu
   const auto record_factory = mgp::RecordFactory(result);
   try {
     const auto node = arguments[0].ValueNode();
-    const auto pattern = arguments[1].ValueString();
-    bool exists = false;
+    const auto input_pattern = arguments[1].ValueString();
 
-    if (pattern[0] == '<' && pattern[pattern.size() - 1] == '>') {
-      throw mgp::ValueException("Invalid relationship specification!");
+    std::unordered_set<std::string_view> patterns;
+    auto i{0};
+    auto j{input_pattern.find("|")};
+    while (j < input_pattern.size()) {
+      patterns.insert(input_pattern.substr(i, j - i));
+      i = j + 1;
+      j = input_pattern.find("|", i);
     }
-    if (pattern[0] == '<') {
-      exists = FindRelationship(pattern.substr(1, pattern.size()), node.InRelationships());
-    } else if (pattern[pattern.size() - 1] == '>') {
-      exists = FindRelationship(pattern.substr(0, pattern.size() - 1), node.OutRelationships());
-    } else {
-      exists =
-          (FindRelationship(pattern, node.InRelationships()) || FindRelationship(pattern, node.OutRelationships()));
+    patterns.insert(input_pattern.substr(i));
+
+    std::unordered_set<std::string_view> in_rels;
+    std::unordered_set<std::string_view> out_rels;
+
+    for (auto pattern : patterns) {
+      if (pattern[0] == '<' && pattern[pattern.size() - 1] == '>') {
+        throw mgp::ValueException("Invalid relationship specification!");
+      }
+      if (pattern[0] == '<') {
+        in_rels.insert(pattern.substr(1, pattern.size()));
+        continue;
+      }
+      if (pattern[pattern.size() - 1] == '>') {
+        out_rels.insert(pattern.substr(0, pattern.size() - 1));
+        continue;
+      }
+      in_rels.insert(pattern);
+      out_rels.insert(pattern);
     }
 
     auto record = record_factory.NewRecord();
-    record.Insert(std::string(kResultRelExists).c_str(), std::move(exists));
-
+    record.Insert(std::string(kResultRelExists).c_str(), FindRelationship(in_rels, node.InRelationships()) ||
+                                                             FindRelationship(out_rels, node.OutRelationships()));
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
     return;
--- cpp/node_module/algorithm/node.hpp
+++ cpp/node_module/algorithm/node.hpp
@@ -1,17 +1,19 @@
 #pragma once
 
+#include <unordered_set>
+
 #include <mgp.hpp>
 
 namespace Node {
 
 /* relationship.exists constants */
-constexpr std::string_view kReturnRelExists = "rel_exists";
+constexpr std::string_view kReturnRelExists = "exists";
 constexpr std::string_view kProcedureRelExists = "relationship_exists";
 constexpr std::string_view kArgumentsNode = "node";
 constexpr std::string_view kArgumentsPattern = "pattern";
-constexpr std::string_view kResultRelExists = "rel_exists";
+constexpr std::string_view kResultRelExists = "exists";
 
-bool FindRelationship(std::string_view type, mgp::Relationships relationships);
+bool FindRelationship(std::unordered_set<std::string_view> types, mgp::Relationships relationships);
 void RelExists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 }  // namespace Node
