fa9216e93afc9e387389bcb87424800a4ec13865
Matija <pintamato@gmail.com>
2023-08-10 17:09:34 +0200

    Implement path.expand

--- cpp/path_module/path_module.cpp
+++ cpp/path_module/path_module.cpp
@@ -5,15 +5,14 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
-    AddProcedure(Path::Expand, std::string(Path::kProcedureExpand).c_str(),
-                 mgp::ProcedureType::Read,
-                 {mgp::Parameter(std::string(Path::kArgumentStartExpand).c_str(),mgp::Type::Node), 
-                 mgp::Parameter(std::string(Path::kArgumentRelationshipsExpand).c_str(),{mgp::Type::List, mgp::Type::String}),
-                 mgp::Parameter(std::string(Path::kArgumentLabelsExpand).c_str(),{mgp::Type::List, mgp::Type::String}),
-                 mgp::Parameter(std::string(Path::kArgumentMinHopsExpand).c_str(),mgp::Type::Int),
-                 mgp::Parameter(std::string(Path::kArgumentMaxHopsExpand).c_str(),mgp::Type::Int)},
-                 {mgp::Return(std::string(Path::kResultExpand).c_str(), mgp::Type::Path)},
-                 module, memory);
+    AddProcedure(
+        Path::Expand, std::string(Path::kProcedureExpand).c_str(), mgp::ProcedureType::Read,
+        {mgp::Parameter(std::string(Path::kArgumentStartExpand).c_str(), mgp::Type::Node),
+         mgp::Parameter(std::string(Path::kArgumentRelationshipsExpand).c_str(), {mgp::Type::List, mgp::Type::String}),
+         mgp::Parameter(std::string(Path::kArgumentLabelsExpand).c_str(), {mgp::Type::List, mgp::Type::String}),
+         mgp::Parameter(std::string(Path::kArgumentMinHopsExpand).c_str(), mgp::Type::Int),
+         mgp::Parameter(std::string(Path::kArgumentMaxHopsExpand).c_str(), mgp::Type::Int)},
+        {mgp::Return(std::string(Path::kResultExpand).c_str(), mgp::Type::Path)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
