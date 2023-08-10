63cd0cb50bf9c3aca668c49daef0f1e1db1b8ac9
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-10 17:12:26 +0200

    [E-util < T580] Implement util.md5 (#290)
    * Implement util.md5 functuon
    
    * Add online test
    
    * CHange gitmodules
    
    * Add memgraph submodule

--- cpp/util_module/algorithm/util.hpp
+++ cpp/util_module/algorithm/util.hpp
@@ -3,4 +3,11 @@
 
 #include <mgp.hpp>
 
-namespace Util {}  // namespace Create
+namespace Util {
+/*md5 constants*/
+constexpr std::string_view kProcedureMd5 = "md5";
+constexpr std::string_view kArgumentValuesMd5 = "values";
+constexpr std::string_view kArgumentResultMd5 = "result";
+
+void Md5(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+}  // namespace Util
--- cpp/util_module/util_module.cpp
+++ cpp/util_module/util_module.cpp
@@ -5,6 +5,9 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+    AddProcedure(Util::Md5, std::string(Util::kProcedureMd5).c_str(), mgp::ProcedureType::Write,
+                 {mgp::Parameter(std::string(Util::kArgumentValuesMd5).c_str(), {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(std::string(Util::kArgumentResultMd5).c_str(), mgp::Type::String)}, module, memory);
 
   } catch (const std::exception &e) {
     return 1;
