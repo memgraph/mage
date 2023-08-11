fe9873332cedd8950f745ec99babc61abac03f12
imilinovic <44698587+imilinovic@users.noreply.github.com>
2023-08-11 10:07:13 +0200

    [E-nodes < T502] Implement nodes.relationship_types (#301)
    * Implement relationship_types

--- cpp/nodes_module/nodes_module.cpp
+++ cpp/nodes_module/nodes_module.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <mgp.hpp>
 
 #include "algorithm/nodes.hpp"
@@ -5,6 +6,12 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+
+    AddProcedure(
+        Nodes::RelationshipTypes, Nodes::kProcedureRelationshipTypes, mgp::ProcedureType::Read,
+        {mgp::Parameter(Nodes::kRelationshipTypesArg1, mgp::Type::Any),
+         mgp::Parameter(Nodes::kRelationshipTypesArg2, {mgp::Type::List, mgp::Type::String}, mgp::Value(mgp::List{}))},
+        {mgp::Return(Nodes::kResultRelationshipTypes, {mgp::Type::List, mgp::Type::Map})}, module, memory);
 
     AddProcedure(Nodes::Delete, Nodes::kProcedureDelete, mgp::ProcedureType::Write,
                  {mgp::Parameter(Nodes::kDeleteArg1, mgp::Type::Any)}, {}, module, memory);
