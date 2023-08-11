6786f69e27748cff386ac2d1d5d8a04c0188cc22
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-08-11 16:33:25 +0200

    [E-nodes < T465] Implement nodes.link (#293)
    * Implement nodes.link
    
    * Implement nodes.link
    
    * CHanges
    
    * Format and add empty line
    
    * Fix PR change

--- cpp/nodes_module/algorithm/nodes.hpp
+++ cpp/nodes_module/algorithm/nodes.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <string>
+
 
 namespace Nodes {
 
@@ -14,8 +16,16 @@ constexpr const std::string_view kResultRelationshipTypes = "relationship_types"
 constexpr const std::string_view kProcedureDelete = "delete";
 constexpr const std::string_view kDeleteArg1 = "nodes";
 
+/*link constants*/
+constexpr size_t minimumNodeListSize = 2;
+constexpr std::string_view kProcedureLink = "link";
+constexpr std::string_view kArgumentNodesLink = "nodes";
+constexpr std::string_view kArgumentTypeLink = "type";
+
 void RelationshipTypes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 void Delete(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+void Link(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 }  // namespace Nodes
