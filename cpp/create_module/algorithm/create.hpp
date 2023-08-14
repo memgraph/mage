da359286e10bcf886f64082eb42513525cb5068b
imilinovic <ivan.milinovic123@gmail.com>
2023-08-14 08:51:25 +0200

    remove const

--- cpp/create_module/algorithm/create.hpp
+++ cpp/create_module/algorithm/create.hpp
@@ -6,18 +6,18 @@
 namespace Create {
 
 /* remove_rel_properties constants */
-constexpr const std::string_view kProcedureRemoveRelProperties = "remove_rel_properties";
-constexpr const std::string_view kRemoveRelPropertiesArg1 = "relationships";
-constexpr const std::string_view kRemoveRelPropertiesArg2 = "keys";
-constexpr const std::string_view kResultRemoveRelProperties = "relationship";
+constexpr std::string_view kProcedureRemoveRelProperties = "remove_rel_properties";
+constexpr std::string_view kRemoveRelPropertiesArg1 = "relationships";
+constexpr std::string_view kRemoveRelPropertiesArg2 = "keys";
+constexpr std::string_view kResultRemoveRelProperties = "relationship";
 
 /* relationship constants */
-constexpr const std::string_view kProcedureRelationship = "relationship";
-constexpr const std::string_view kRelationshipArg1 = "from";
-constexpr const std::string_view kRelationshipArg2 = "relationshipType";
-constexpr const std::string_view kRelationshipArg3 = "properties";
-constexpr const std::string_view kRelationshipArg4 = "to";
-constexpr const std::string_view kResultRelationship = "relationship";
+constexpr std::string_view kProcedureRelationship = "relationship";
+constexpr std::string_view kRelationshipArg1 = "from";
+constexpr std::string_view kRelationshipArg2 = "relationshipType";
+constexpr std::string_view kRelationshipArg3 = "properties";
+constexpr std::string_view kRelationshipArg4 = "to";
+constexpr std::string_view kResultRelationship = "relationship";
 
 void RemoveRelProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
