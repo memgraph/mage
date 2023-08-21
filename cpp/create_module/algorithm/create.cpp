bec09e56cdfb65fbee1d2276de6c5985831c06e2
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-08-21 08:12:14 +0200

    [E-add-create-functions < E-idora] Add part of create functions (#318)
    Implement create.node, create.set_properties, create.remove_labels and create.set_rel_property procedures

--- cpp/create_module/algorithm/create.cpp
+++ cpp/create_module/algorithm/create.cpp
@@ -1,5 +1,186 @@
 #include "create.hpp"
 
+void Create::SetElementProp(mgp::Relationship &element, const mgp::List &prop_key_list,
+                            const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory) {
+  for (size_t i = 0; i < prop_key_list.Size(); i++) {
+    element.SetProperty(std::string(prop_key_list[i].ValueString()), prop_value_list[i]);
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultRelProp).c_str(), std::move(element));
+  }
+}
+
+void Create::ProcessElement(const mgp::Value &element, const mgp::Graph graph, const mgp::List &prop_key_list,
+                            const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory,
+                            std::unordered_set<mgp::Id> &relIds) {
+  if (!(element.IsRelationship() || element.IsInt())) {
+    throw mgp::ValueException("First argument must be a relationship, id or a list of those.");
+  }
+  if (element.IsRelationship()) {
+    auto rel = element.ValueRelationship();
+    SetElementProp(rel, prop_key_list, prop_value_list, record_factory);
+    return;
+  }
+  relIds.insert(mgp::Id::FromInt(element.ValueInt()));
+}
+
+void Create::SetRelProperty(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto graph = mgp::Graph(memgraph_graph);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    mgp::List prop_key_list = mgp::List();
+    prop_key_list.AppendExtend(arguments[1]);
+    mgp::List prop_value_list = mgp::List();
+    prop_value_list.AppendExtend(arguments[2]);
+
+    std::unordered_set<mgp::Id> relIds;
+
+    if (arguments[0].IsList()) {
+      for (const auto element : arguments[0].ValueList()) {
+        ProcessElement(element, graph, prop_key_list, prop_value_list, record_factory, relIds);
+      }
+    } else {
+      ProcessElement(arguments[0], graph, prop_key_list, prop_value_list, record_factory, relIds);
+    }
+    if (relIds.empty()) {
+      return;
+    }
+    for (auto rel : graph.Relationships()) {
+      if (relIds.contains(rel.Id())) {
+        SetElementProp(rel, prop_key_list, prop_value_list, record_factory);
+      }
+    }
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Create::RemoveElementLabels(mgp::Node &element, const mgp::List &labels,
+                                 const mgp::RecordFactory &record_factory) {
+  for (auto label : labels) {
+    element.RemoveLabel(label.ValueString());
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultRemoveLabels).c_str(), std::move(element));
+  }
+}
+
+void Create::ProcessElement(const mgp::Value &element, const mgp::Graph graph, const mgp::List &labels,
+                            const mgp::RecordFactory &record_factory) {
+  if (!(element.IsNode() || element.IsInt())) {
+    throw mgp::ValueException("First argument must be type node, id or a list of those.");
+  }
+  if (element.IsNode()) {
+    auto node = element.ValueNode();
+    RemoveElementLabels(node, labels, record_factory);
+    return;
+  }
+  auto node = graph.GetNodeById(mgp::Id::FromInt(element.ValueInt()));
+  RemoveElementLabels(node, labels, record_factory);
+}
+
+void Create::RemoveLabels(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto graph = mgp::Graph(memgraph_graph);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    const auto labels = arguments[1].ValueList();
+
+    if (arguments[0].IsList()) {
+      for (const auto element : arguments[0].ValueList()) {
+        ProcessElement(element, graph, labels, record_factory);
+      }
+      return;
+    }
+    ProcessElement(arguments[0], graph, labels, record_factory);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Create::SetElementProp(mgp::Node &element, const mgp::List &prop_key_list, const mgp::List &prop_value_list,
+                            const mgp::RecordFactory &record_factory) {
+  for (size_t i = 0; i < prop_key_list.Size(); i++) {
+    element.SetProperty(std::string(prop_key_list[i].ValueString()), prop_value_list[i]);
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultProperties).c_str(), std::move(element));
+  }
+}
+
+void Create::ProcessElement(const mgp::Value &element, const mgp::Graph graph, const mgp::List &prop_key_list,
+                            const mgp::List &prop_value_list, const mgp::RecordFactory &record_factory) {
+  if (element.IsNode()) {
+    auto node = element.ValueNode();
+    SetElementProp(node, prop_key_list, prop_value_list, record_factory);
+    return;
+  }
+  if (element.IsInt()) {
+    auto node = graph.GetNodeById(mgp::Id::FromInt(element.ValueInt()));
+    SetElementProp(node, prop_key_list, prop_value_list, record_factory);
+    return;
+  }
+  throw mgp::ValueException("First argument must be a node, id or a list of those.");
+}
+
+void Create::SetProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  const auto graph = mgp::Graph(memgraph_graph);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    const auto prop_key_list = arguments[1].ValueList();
+    const auto prop_value_list = arguments[2].ValueList();
+
+    if (prop_key_list.Size() != prop_value_list.Size()) {
+      throw mgp::IndexException("Key and value lists must be the same size.");
+    }
+
+    if (!arguments[0].IsList()) {
+      ProcessElement(arguments[0], graph, prop_key_list, prop_value_list, record_factory);
+      return;
+    }
+    for (const auto element : arguments[0].ValueList()) {
+      ProcessElement(element, graph, prop_key_list, prop_value_list, record_factory);
+    }
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Create::Node(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  auto graph = mgp::Graph(memgraph_graph);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    const auto labels = arguments[0].ValueList();
+    const auto properties = arguments[1].ValueMap();
+
+    mgp::Node node = graph.CreateNode();
+
+    for (const auto label : labels) {
+      node.AddLabel(label.ValueString());
+    }
+
+    for (const auto property : properties) {
+      node.SetProperty((std::string)property.key, property.value);
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultNode).c_str(), std::move(node));
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
 void Create::Nodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
   mgp::memory = memory;
   const auto arguments = mgp::List(args);
@@ -145,4 +326,3 @@ void Create::RemoveProperties(mgp_list *args, mgp_graph *memgraph_graph, mgp_res
     return;
   }
 }
-
--- cpp/create_module/create_module.cpp
+++ cpp/create_module/create_module.cpp
@@ -5,6 +5,28 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
+    
+    AddProcedure(Create::SetRelProperty, Create::kProcedureSetRelProp, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kArgumentsRelationship, mgp::Type::Any),
+                  mgp::Parameter(Create::kArgumentsKey, mgp::Type::String),
+                  mgp::Parameter(Create::kArgumentsValue, mgp::Type::Any)},
+                 {mgp::Return(Create::kReturnRelProp, mgp::Type::Relationship)}, module, memory);
+
+    AddProcedure(Create::RemoveLabels, Create::kProcedureRemoveLabels, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kArgumentsNodes, mgp::Type::Any),
+                  mgp::Parameter(Create::kArgumentsLabels, {mgp::Type::List, mgp::Type::String})},
+                 {mgp::Return(Create::kReturnRemoveLabels, mgp::Type::Node)}, module, memory);
+
+    AddProcedure(Create::SetProperties, Create::kProcedureSetProperties, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kArgumentsNodes, mgp::Type::Any),
+                  mgp::Parameter(Create::kArgumentsKeys, {mgp::Type::List, mgp::Type::String}),
+                  mgp::Parameter(Create::kArgumentsValues, {mgp::Type::List, mgp::Type::Any})},
+                 {mgp::Return(Create::kReturnProperties, mgp::Type::Node)}, module, memory);
+
+    AddProcedure(Create::Node, Create::kProcedureNode, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Create::kArgumentsLabelsList, {mgp::Type::List, mgp::Type::String}),
+                  mgp::Parameter(Create::kArgumentsProperties, mgp::Type::Map)},
+                 {mgp::Return(Create::kReturnNode, mgp::Type::Node)}, module, memory);
 
     AddProcedure(
         Create::Nodes, std::string(Create::kProcedureNodes).c_str(), mgp::ProcedureType::Write,
