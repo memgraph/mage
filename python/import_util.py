30925dd2b391bd9d317a1acb904e9a72aa672d90
idoraban <idora.ban@memgraph.io>
2023-08-23 06:32:26 +0000

    add work in progress

--- python/import_util.py
+++ python/import_util.py
@@ -3,6 +3,7 @@ import json as js
 from mage.export_import_util.parameters import Parameter
 import mgp
 from typing import Union, List, Dict, Any
+import re
 
 
 def convert_from_isoformat(
@@ -33,7 +34,9 @@ def convert_from_isoformat(
         return property
 
 
-def create_vertex(ctx: mgp.ProcCtx, properties: Dict[str, Any], labels: List[str]):
+def create_vertex(
+    ctx: mgp.ProcCtx, properties: Dict[str, Any], labels: List[str]
+):
     vertex = ctx.graph.create_vertex()
     vertex_properties = vertex.properties
 
@@ -82,7 +85,6 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
     vertex_ids = dict()
 
     for object in graph_objects:
-
         if all(
             key in object
             for key in (
@@ -96,17 +98,19 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
             id_value = object[Parameter.ID.value]
         else:
             raise KeyError(
-                "Each graph object needs to have 'type', 'properties' and 'id' keys."
+                "Each graph object needs to have 'type', \
+                 'properties' and 'id' keys."
             )
 
         if type_value == Parameter.NODE.value:
-
             if Parameter.LABELS.value in object:
                 labels_value = object[Parameter.LABELS.value]
             else:
                 raise KeyError("Each node object needs to have 'labels' key.")
 
-            vertex_ids[id_value] = create_vertex(ctx, properties_value, labels_value)
+            vertex_ids[id_value] = create_vertex(
+                ctx, properties_value, labels_value
+            )
 
         elif type_value == Parameter.RELATIONSHIP.value:
             if all(
@@ -122,13 +126,93 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
                 edge_type = object[Parameter.LABEL.value]
             else:
                 raise KeyError(
-                    "Each relationship object needs to have 'start', 'end' and 'label' keys."
+                    "Each relationship object needs to have 'start', \
+                     'end' and 'label' keys."
                 )
 
             create_edge(
-                ctx, properties_value, start_node_id, end_node_id, edge_type, vertex_ids
+                ctx,
+                properties_value,
+                start_node_id,
+                end_node_id,
+                edge_type,
+                vertex_ids,
             )
         else:
-            raise KeyError("The provided file does not match the correct JSON format.")
+            raise KeyError(
+                "The provided file does not match the correct JSON format."
+            )
 
     return mgp.Record()
+
+
+def set_default_config(config):
+    if not config.get("readLabels"):
+        config.update({"readLabels": False})
+    if not config.get("defaultRelationshipType"):
+        config.update({"defaultRelationshipType": "RELATED"})
+    if not config.get("storeNodeIds"):
+        config.update({"storeNodeIds": False})
+    if not config.get("source"):
+        config.update({"source": {}})
+    if not config.get("target"):
+        config.update({"target": {}})
+
+
+def get_attribute(line, attribute_name):
+    match = re.search(fr'{attribute_name}="(\w+)"', line)
+    if match:
+        return match.group(1)
+    return None
+
+
+@mgp.write_proc
+def graphml(
+    ctx: mgp.ProcCtx,
+    path: str = "",
+    config: mgp.Map = {},
+) -> mgp.Record(status=str):
+    """
+    Procedure to export the whole database to a graphML file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the graphML file containing the exported graph database.
+    config : Map
+
+    """
+
+    set_default_config(config)
+
+    try:
+        with open(path, "r") as file:
+            file_content = file.read()
+            file_content = file_content.replace('\t', '')
+            file_content = file_content.replace('><', '>\n<')
+            file_content = file_content.replace('\n<data', '<data')
+            # file_content = file_content.replace('\n</node', '</node')
+            # file_content = file_content.replace('\n</edge', '</edge')
+    except Exception:
+        raise OSError("Could not open/read file.")
+
+    node_keys = dict()
+    rel_keys = dict()
+
+    for line in file_content:
+
+        if (line.startswith("<?xml") or line.startswith("<graph") or line.startswith("<graphml")):
+            continue
+        if (line.startswith("</")):
+            continue
+        if (line.startswith("<key")):
+            if (get_attribute(line, "for") == "node"):
+                node_keys.update({get_attribute(line, "id"): [get_attribute(line, "name"), get_attribute(line, "type"), get_attribute(line, "list")]})
+            if (get_attribute(line, "for") == "relationship"):
+                rel_keys.update({get_attribute(line, "id"): [get_attribute(line, "name"), get_attribute(line, "type"), get_attribute(line, "list")]})
+
+    print(file_content)
+
+    file.close()
+
+    return mgp.Record(status="success")
