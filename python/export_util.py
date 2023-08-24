e9b74337311316a33a742e393b8497a4771ecdcb
idoraban <idora.ban@memgraph.io>
2023-08-24 10:34:26 +0000

    transfer to new branch

--- python/export_util.py
+++ python/export_util.py
@@ -3,6 +3,7 @@ import io
 import json as js
 import mgp
 
+from math import floor
 from dataclasses import dataclass
 from datetime import datetime, date, time, timedelta
 from gqlalchemy import Memgraph
@@ -76,16 +77,82 @@ def convert_to_isoformat(
         return property
 
 
-def get_graph(ctx: mgp.ProcCtx) -> List[Union[Node, Relationship]]:
+def to_duration_iso_format(value: timedelta) -> str:
+    """Converts timedelta to ISO-8601 duration: P<date>T<time>"""
+    date_parts: List[str] = []
+    time_parts: List[str] = []
+
+    if value.days != 0:
+        date_parts.append(f"{abs(value.days)}D")
+
+    if value.seconds != 0 or value.microseconds != 0:
+        abs_seconds = abs(value.seconds)
+        hours = floor(abs_seconds / 3600)
+        minutes = floor((abs_seconds - hours * 3600) / 60)
+        seconds = abs_seconds - hours * 3600 - minutes * 60
+        microseconds = value.microseconds
+
+        if hours > 0:
+            time_parts.append(f"{hours}H")
+        if minutes > 0:
+            time_parts.append(f"{minutes}M")
+        if seconds > 0 or microseconds > 0:
+            microseconds_part = (
+                f".{abs(value.microseconds)}"
+                if value.microseconds != 0
+                else ""
+            )
+            time_parts.append(f"{seconds}{microseconds_part}S")
+
+    date_duration_str = "".join(date_parts)
+    time_duration_str = f'T{"".join(time_parts)}' if time_parts else ""
+
+    return f"P{date_duration_str}{time_duration_str}"
+
+
+def convert_to_isoformat_graphML(
+    property: Union[
+        None,
+        str,
+        bool,
+        int,
+        float,
+        List[Any],
+        Dict[str, Any],
+        timedelta,
+        time,
+        datetime,
+        date,
+    ]
+):
+    if isinstance(property, timedelta):
+        return to_duration_iso_format(property)
+
+    if isinstance(property, (time, date, datetime)):
+        return property.isoformat()
+
+    else:
+        return property
+
+
+def get_graph(
+    ctx: mgp.ProcCtx, graphML: bool
+) -> List[Union[Node, Relationship]]:
     nodes = list()
     relationships = list()
 
     for vertex in ctx.graph.vertices:
         labels = [label.name for label in vertex.labels]
-        properties = {
-            key: convert_to_isoformat(vertex.properties.get(key))
-            for key in vertex.properties.keys()
-        }
+        if graphML:
+            properties = {
+                key: convert_to_isoformat_graphML(vertex.properties.get(key))
+                for key in vertex.properties.keys()
+            }
+        else:
+            properties = {
+                key: convert_to_isoformat(vertex.properties.get(key))
+                for key in vertex.properties.keys()
+            }
 
         nodes.append(Node(vertex.id, labels, properties).get_dict())
 
@@ -119,13 +186,19 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
         Path to the JSON file containing the exported graph database.
     """
 
-    graph = get_graph(ctx)
+    graph = get_graph(ctx, 0)
     try:
         with open(path, "w") as outfile:
-            js.dump(graph, outfile, indent=Parameter.STANDARD_INDENT.value, default=str)
+            js.dump(
+                graph,
+                outfile,
+                indent=Parameter.STANDARD_INDENT.value,
+                default=str,
+            )
     except PermissionError:
         raise PermissionError(
-            "You don't have permissions to write into that file. Make sure to give the necessary permissions to user memgraph."
+            "You don't have permissions to write into that file. Make sure \
+             to give the necessary permissions to user memgraph."
         )
     except Exception:
         raise OSError("Could not open or write to the file.")
@@ -138,7 +211,7 @@ def json_stream(ctx: mgp.ProcCtx) -> mgp.Record(stream=str):
     """
     Procedure to export the whole database to a stream.
     """
-    return mgp.Record(stream=js.dumps(get_graph(ctx)))
+    return mgp.Record(stream=js.dumps(get_graph(ctx, 0)))
 
 
 def save_file(file_path: str, data_list: list):
@@ -153,7 +226,8 @@ def save_file(file_path: str, data_list: list):
             writer.writerows(data_list)
     except PermissionError:
         raise PermissionError(
-            "You don't have permissions to write into that file. Make sure to give the necessary permissions to user memgraph."
+            "You don't have permissions to write into that file. \
+             Make sure to give the necessary permissions to user memgraph."
         )
     except csv.Error as e:
         raise csv.Error(
@@ -203,7 +277,7 @@ def csv_query(
         PermissionError: If you provided file path that you have no permissions to write at.
         csv.Error: If an error occurred while writing into stream or CSV file.
         OSError: If the file can't be opened or written to.
-    """
+    """  # noqa: E501
 
     # file or config have to be provided
     if not file_path and not stream:
@@ -212,7 +286,8 @@ def csv_query(
     # only config provided with stream set to false
     if not file_path and not stream:
         raise Exception(
-            "If you provided only stream value, it has to be set to True to get any results."
+            "If you provided only stream value, \
+             it has to be set to True to get any results."
         )
 
     memgraph = Memgraph()
@@ -221,7 +296,8 @@ def csv_query(
     # if query yields no result
     if not len(results):
         raise Exception(
-            "Your query yields no results. Check if the database is empty or rewrite the provided query."
+            "Your query yields no results. Check if the database \
+             is empty or rewrite the provided query."
         )
 
     result_keys = list(results[0])
@@ -235,3 +311,287 @@ def csv_query(
         data = csv_to_stream(data_list)
 
     return mgp.Record(file_path=file_path, data=data)
+
+
+def write_header(output: io.StringIO):
+    output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    output.write(
+        '<graphml xmlns="http://graphml.graphdrawing.org/xmlns" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://graphml.graphdrawing.org/xmlns http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd">\n'  # noqa: E501
+    )
+
+
+def translate_types(variable: Any):
+    if isinstance(variable, str):
+        return "string"
+    if isinstance(variable, bool):
+        return "boolean"
+    if isinstance(variable, float):
+        return "float"
+    if isinstance(variable, int):
+        return "int"
+    raise Exception(
+        "Property values can only be primitive types or arrays of primitive types."  # noqa: E501
+    )
+
+
+def check_if_elements_same_type(variable: Any):
+    for element in variable:
+        if not isinstance(element, type(variable[0])):
+            raise Exception(
+                "If property value is a list it must consist of same typed elements."  # noqa: E501
+            )
+
+
+def get_type_string(variable: Any) -> Union[str, List[Any]]:
+    if (
+        type(variable).__name__ == "tuple"
+    ):  # TODO I need to diferentiate maps and lists
+        if len(variable) == 0:
+            return ["string", "string"]
+        check_if_elements_same_type(variable)
+        return [
+            "string",
+            translate_types(variable[0]),
+        ]
+    return translate_types(variable)
+
+
+def write_keys(
+    graph: List[Union[Node, Relationship]],
+    output: io.StringIO,
+    config: mgp.Map,
+):
+    node_keys = dict()
+    rel_keys = dict()
+    for element in graph:
+        if element.get("type") == "node":
+            if element.get("labels"):
+                if config.get("format").upper() == "TINKERPOP":
+                    node_keys.update({"labelV": get_type_string("labelV")})
+                else:
+                    node_keys.update(
+                        {"labels": get_type_string("labels")}
+                    )  # what if two nodes have same property name but different value type? or node's property name is "labels"?  # noqa: E501
+
+            for key, value in element.get("properties").items():
+                node_keys.update({key: get_type_string(value)})
+
+        elif element.get("type") == "relationship":
+            if config.get("format").upper() == "TINKERPOP":
+                rel_keys.update({"labelE": get_type_string("labelE")})
+            else:
+                rel_keys.update({"label": get_type_string("label")})
+
+            for key, value in element.get("properties").items():
+                rel_keys.update({key: get_type_string(value)})
+
+    if config.get("format").upper() == "GEPHI":
+        node_keys.update({"TYPE": get_type_string("TYPE")})
+        rel_keys.update({"TYPE": get_type_string("TYPE")})
+
+    for key, value in node_keys.items():
+        output.write(f'<key id="{key}" for="node" attr.name="{key}"')
+        if config.get("useTypes"):
+            if isinstance(value, str):
+                output.write(f' attr.type="{value}"')
+            else:
+                output.write(f' attr.type="{value[0]}" attr.list="{value[1]}"')
+        output.write("/>\n")
+    for key, value in rel_keys.items():
+        output.write(f'<key id="{key}" for="edge" attr.name="{key}"')
+        if config.get("useTypes"):
+            if isinstance(value, str):
+                output.write(f' attr.type="{value}"')
+            else:
+                output.write(f' attr.type="{value[0]}" attr.list="{value[1]}"')
+        output.write("/>\n")
+
+
+def write_graph(output: io.StringIO):
+    output.write('<graph id="G" edgedefault="directed">\n')
+
+
+def get_gephi_label_value(
+    element: Union[Node, Relationship], config: mgp.Map
+) -> str:
+    for caption in config.get("caption"):
+        if caption in element.get("properties").keys():
+            return str(element.get("properties").get(caption))
+
+    if element.get("properties").values():
+        return str(list(element.get("properties").values())[0])
+
+    return str(element.get("id"))
+
+
+def write_labels_as_data(
+    element: Union[Node, Relationship], output: io.StringIO, config: mgp.Map
+):
+    if not element.get("labels"):
+        return
+    if config.get("format").upper() == "TINKERPOP":
+        output.write('<data key="labelV">')
+        for i in range(0, len(element.get("labels"))):
+            if i == 0:
+                output.write(element.get("labels")[i])
+            else:
+                output.write(f':{element.get("labels")[i]}')
+        output.write("</data>")
+    if config.get("format").upper() == "GEPHI":
+        output.write('<data key="TYPE">')
+        for label in element.get("labels"):
+            output.write(f":{label}")
+        output.write("</data>")
+        output.write(
+            f'<data key="label">{get_gephi_label_value(element, config)}</data>'  # noqa: E501
+        )
+    else:
+        output.write('<data key="labels">')
+        for label in element.get("labels"):
+            output.write(f":{label}")
+        output.write("</data>")
+
+
+def get_value_string(value: Any) -> str:
+    if isinstance(value, (set, list, tuple, map)):
+        return js.dumps(value, ensure_ascii=False)
+    return str(value)
+
+
+def write_nodes_and_rels(
+    graph: List[Union[Node, Relationship]],
+    output: io.StringIO,
+    config: mgp.Map,
+):
+    for element in graph:
+        if element.get("type") == "node":
+            output.write(f'<node id="n{str(element.get("id"))}')
+            if (
+                element.get("labels")
+                and config.get("format").upper() != "TINKERPOP"
+            ):
+                output.write('" labels="')
+                for label in element.get("labels"):
+                    output.write(f":{label}")
+            output.write('">')
+
+            write_labels_as_data(element, output, config)
+
+            for key, value in element.get("properties").items():
+                output.write(
+                    f'<data key="{key}">{get_value_string(value)}</data>'
+                )
+            output.write("</node>\n")
+
+        elif element.get("type") == "relationship":
+            output.write(
+                f'<edge id="e{str(element.get("id"))}" source="n{str(element.get("start"))}" target="n{str(element.get("end"))}" label="{element.get("label")}">'  # noqa: E501
+            )
+
+            if config.get("format").upper() == "TINKERPOP":
+                output.write(
+                    f'<data key="labelE">{element.get("label")}</data>'
+                )
+            else:
+                output.write(
+                    f'<data key="label">{element.get("label")}</data>'
+                )
+            if config.get("format").upper() == "GEPHI":
+                output.write(f'<data key="TYPE">{element.get("label")}</data>')
+
+            for key, value in element.get("properties").items():
+                output.write(
+                    f'<data key="{key}">{get_value_string(value)}</data>'
+                )
+            output.write("</edge>\n")
+
+
+def write_footer(output: io.StringIO):
+    output.write("</graph>\n")
+    output.write("</graphml>")
+
+
+def set_default_config(config: mgp.Map):
+    if not config.get("stream"):
+        config.update({"stream": False})
+    if not config.get("format"):
+        config.update({"format": ""})  # should it be Gephi?
+    if not config.get("caption"):
+        config.update({"caption": []})
+    if not config.get("useTypes"):
+        config.update({"useTypes": False})
+    if not config.get("leaveOutLabels"):
+        config.update({"leaveOutLabels": False})
+    if not config.get("leaveOutProperties"):
+        config.update({"leaveOutProperties": False})
+    if (
+        not isinstance(config.get("stream"), bool)
+        or not isinstance(config.get("format"), str)
+        or not isinstance(config.get("caption"), list)
+        or not isinstance(config.get("useTypes"), bool)
+        or not isinstance(config.get("leaveOutLabels"), bool)
+        or not isinstance(config.get("leaveOutProperties"), bool)
+    ):
+        raise TypeError(
+            "Config parameter must be a map with specific \
+             keys and values described in documentation."
+        )
+
+
+@mgp.read_proc
+def graphml(
+    ctx: mgp.ProcCtx,
+    path: str = "",
+    config: mgp.Map = {},
+) -> mgp.Record(status=str):
+    """
+    Procedure to export the whole database to a graphML file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the graphML file containing the exported graph database.
+    config : Map
+
+    """
+
+    graph = get_graph(ctx, 1)
+    set_default_config(config)
+
+    if config.get("leaveOutLabels") or config.get("leaveOutProperties"):
+        for element in graph:
+            if config.get("leaveOutLabels"):
+                element.update({"labels": []})
+            if config.get("leaveOutProperties"):
+                element.update({"properties": {}})
+
+    output = io.StringIO()
+
+    if not path and not config.get("stream"):
+        raise Exception(
+            "Please provide file name or set stream to True in config."
+        )
+
+    write_header(output)
+    write_keys(graph, output, config)
+    write_graph(output)
+    write_nodes_and_rels(graph, output, config)
+    write_footer(output)
+
+    try:
+        if path:
+            with open(path, "w") as outfile:
+                outfile.write(output.getvalue())
+            outfile.close()
+    except PermissionError:
+        raise PermissionError(
+            "You don't have permissions to write into that file. Make sure \
+             to give the necessary permissions to user memgraph."
+        )
+    except Exception:
+        raise OSError("Could not open or write to the file.")
+
+    if config.get("stream"):
+        return mgp.Record(status=output.getvalue())
+
+    return mgp.Record(status="success")
--- python/import_util.py
+++ python/import_util.py
@@ -3,6 +3,9 @@ import json as js
 from mage.export_import_util.parameters import Parameter
 import mgp
 from typing import Union, List, Dict, Any
+import defusedxml.ElementTree as ET
+from export_util import convert_to_isoformat_graphML
+import ast
 
 
 def convert_from_isoformat(
@@ -33,7 +36,9 @@ def convert_from_isoformat(
         return property
 
 
-def create_vertex(ctx: mgp.ProcCtx, properties: Dict[str, Any], labels: List[str]):
+def create_vertex(
+    ctx: mgp.ProcCtx, properties: Dict[str, Any], labels: List[str]
+):
     vertex = ctx.graph.create_vertex()
     vertex_properties = vertex.properties
 
@@ -82,7 +87,6 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
     vertex_ids = dict()
 
     for object in graph_objects:
-
         if all(
             key in object
             for key in (
@@ -96,17 +100,19 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
             id_value = object[Parameter.ID.value]
         else:
             raise KeyError(
-                "Each graph object needs to have 'type', 'properties' and 'id' keys."
+                "Each graph object needs to have 'type', \
+                 'properties' and 'id' keys."
             )
 
         if type_value == Parameter.NODE.value:
-
             if Parameter.LABELS.value in object:
                 labels_value = object[Parameter.LABELS.value]
             else:
                 raise KeyError("Each node object needs to have 'labels' key.")
 
-            vertex_ids[id_value] = create_vertex(ctx, properties_value, labels_value)
+            vertex_ids[id_value] = create_vertex(
+                ctx, properties_value, labels_value
+            )
 
         elif type_value == Parameter.RELATIONSHIP.value:
             if all(
@@ -122,13 +128,243 @@ def json(ctx: mgp.ProcCtx, path: str) -> mgp.Record():
                 edge_type = object[Parameter.LABEL.value]
             else:
                 raise KeyError(
-                    "Each relationship object needs to have 'start', 'end' and 'label' keys."
+                    "Each relationship object needs to have 'start', \
+                     'end' and 'label' keys."
                 )
 
             create_edge(
-                ctx, properties_value, start_node_id, end_node_id, edge_type, vertex_ids
+                ctx,
+                properties_value,
+                start_node_id,
+                end_node_id,
+                edge_type,
+                vertex_ids,
             )
         else:
-            raise KeyError("The provided file does not match the correct JSON format.")
+            raise KeyError(
+                "The provided file does not match the correct JSON format."
+            )
 
     return mgp.Record()
+
+
+def find_node(
+    ctx: mgp.ProcCtx, label: str, prop_key: str, prop_value: str
+) -> Any:  # TODO prop value cast??
+    for vertex in ctx.graph.vertices:
+        if (
+            label in [label.name for label in vertex.labels]
+            and prop_key in vertex.properties.keys()
+            and convert_to_isoformat_graphML(vertex.properties.get(prop_key))
+            == prop_value
+        ):
+            return vertex.id
+    return None
+
+
+def cast_element(
+    text: str, type: str
+) -> Union[List[Any], str, int, bool, float]:
+    if text is None:
+        return ""
+    if type == "string":
+        return str(text)
+    if type == "int":
+        return int(text)
+    if type == "boolean":
+        return bool(text)
+    if type == "float":
+        return float(text)
+    if type is None:
+        return text
+
+
+def cast(
+    text: str, type: str, is_list: str
+) -> Union[List[Any], str, int, bool, float]:
+    if is_list is not None:
+        casted_list = list()
+        for element in ast.literal_eval(text):
+            casted_list.append(cast_element(element, is_list))
+        return casted_list
+    return cast_element(text, type)
+
+
+def set_default_keys(key_dict: Dict[str, Any], properties: Dict[str, Any]):
+    for key, value in key_dict.items():
+        if value[3] is not None:
+            properties.update({value[0]: cast(value[3], value[1], value[2])})
+
+
+def set_default_config(config: mgp.Map):
+    if not config.get("readLabels"):
+        config.update({"readLabels": False})
+    if not config.get("defaultRelationshipType"):
+        config.update({"defaultRelationshipType": "RELATED"})
+    if not config.get("storeNodeIds"):
+        config.update({"storeNodeIds": False})
+    if not config.get("source"):
+        config.update({"source": {}})
+    if not config.get("target"):
+        config.update({"target": {}})
+    if (
+        not isinstance(config.get("readLabels"), bool)
+        or not isinstance(config.get("defaultRelationshipType"), str)
+        or not isinstance(config.get("storeNodeIds"), bool)
+        or not isinstance(config.get("source"), dict)
+        or not isinstance(config.get("target"), dict)
+        or (
+            config.get("source") and "label" not in config.get("source").keys()
+        )
+        or (
+            config.get("target") and "label" not in config.get("target").keys()
+        )
+    ):
+        raise TypeError(
+            "Config parameter must be a map with specific \
+             keys and values described in documentation."
+        )
+
+
+@mgp.write_proc
+def graphml(
+    ctx: mgp.ProcCtx,
+    path: str = "",
+    config: mgp.Map = {},
+) -> mgp.Record(status=str):
+    """
+    Procedure to export the whole database to a graphML file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the graphML file containing the exported graph database.
+    config : Map
+
+    """
+
+    set_default_config(config)
+
+    try:
+        tree = ET.parse(path)
+    except Exception:
+        raise OSError("Could not open/read file.")
+
+    root = tree.getroot()
+    graphml_ns = root.tag.split("}")[0].strip("{")
+    namespace = {"graphml": graphml_ns}
+
+    node_keys = dict()
+    rel_keys = dict()
+
+    for key in root.findall(".//graphml:key", namespace):
+        # key value legend: value[0] = attr.name
+        #                   value[1] = attr.type
+        #                   value[2] = attr.list
+        #                   value[3] = default
+        # -> should I make a class out of it?
+        value = [key.attrib["attr.name"]]
+        if "attr.type" in key.attrib.keys():
+            value.append(key.attrib["attr.type"])
+        else:
+            value.append(None)
+        if "attr.list" in key.attrib.keys():
+            value.append(key.attrib["attr.list"])
+        else:
+            value.append(None)
+        child = key.findall(".//default")
+        if child:
+            value.append(child[0].text)
+        else:
+            value.append(None)
+        if key.attrib["for"].lower() == "node":
+            node_keys.update({key.attrib["id"]: value})
+        elif key.attrib["for"].lower() == "edge":
+            rel_keys.update({key.attrib["id"]: value})
+
+    real_ids = dict()
+
+    for node in root.findall(".//graphml:node", namespace):
+        labels = []
+        properties = dict()
+        if config.get("readLabels"):
+            labels = node.attrib["labels"].split(":")
+            labels.pop(0)
+        if config.get("storeNodeIds"):
+            properties.update({"id": node.attrib["id"]})
+
+        set_default_keys(node_keys, properties)
+
+        for data in node.findall("graphml:data", namespace):
+            key = node_keys.get(data.attrib["key"])
+            if key is None:
+                key = [data.attrib["key"], "string", None, None]
+            if config.get("readLabels") and data.attrib["key"] == "labels":
+                new_labels = node.attrib["labels"].split(":")
+                new_labels.pop(0)
+                labels = labels + new_labels
+            else:
+                properties.update({key[0]: cast(data.text, key[1], key[2])})
+
+        real_ids.update(
+            {node.attrib["id"]: create_vertex(ctx, properties, labels)}
+        )
+
+    for rel in root.findall(".//graphml:edge", namespace):
+        if "label" in rel.attrib.keys():
+            rel_type = rel.attrib["label"]
+        else:
+            rel_type = config.get("defaultRelationshipType")
+
+        properties = dict()
+        set_default_keys(rel_keys, properties)
+
+        for data in rel.findall("graphml:data", namespace):
+            key = rel_keys.get(data.attrib["key"])
+            if key is None:
+                key = [data.attrib["key"], "string", None, None]
+            if not data.attrib["key"] == "label":  # Tinkerpop???
+                properties.update({key[0]: cast(data.text, key[1], key[2])})
+
+        if rel.attrib["source"] not in real_ids.keys():
+            if not config.get("source"):
+                # without source/target config, we look for the internal id
+                real_ids.update({rel.attrib["source"]: rel.attrib["source"]})
+            else:
+                source_config = config.get("source")
+                if "id" not in source_config.keys():
+                    source_config.update({"id": "id"})
+                node_id = find_node(
+                    ctx,
+                    source_config["label"],
+                    source_config["id"],
+                    rel.attrib["source"],
+                )
+                real_ids.update({rel.attrib["source"]: node_id})
+
+        if rel.attrib["target"] not in real_ids.keys():
+            if not config.get("target"):
+                # without source/target config, we look for the internal id
+                real_ids.update({rel.attrib["target"]: rel.attrib["target"]})
+            else:
+                target_config = config.get("target")
+                if "id" not in target_config.keys():
+                    target_config.update({"id": "id"})
+                node_id = find_node(
+                    ctx,
+                    target_config["label"],
+                    target_config["id"],
+                    rel.attrib["target"],
+                )
+                real_ids.update({rel.attrib["target"]: node_id})
+
+        create_edge(
+            ctx,
+            properties,
+            rel.attrib["source"],
+            rel.attrib["target"],
+            rel_type,
+            real_ids,
+        )
+
+    return mgp.Record(status="success")
