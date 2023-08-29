b72c3e2abaa1df3bb23fd7c66de4dda1458a2aa2
idoraban <idora.ban@memgraph.io>
2023-08-29 12:59:19 +0000

    implement refactor.categorize

--- cpp/refactor_module/refactor_module.cpp
+++ cpp/refactor_module/refactor_module.cpp
@@ -6,6 +6,18 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    AddProcedure(Refactor::Categorize, Refactor::kProcedureCategorize, mgp::ProcedureType::Write,
+                 {
+                     mgp::Parameter(Refactor::kArgumentsCatSourceKey, mgp::Type::String),
+                     mgp::Parameter(Refactor::kArgumentsCatRelType, mgp::Type::String),
+                     mgp::Parameter(Refactor::kArgumentsCatRelOutgoing, mgp::Type::Bool),
+                     mgp::Parameter(Refactor::kArgumentsCatLabelName, mgp::Type::String),
+                     mgp::Parameter(Refactor::kArgumentsCatPropKey, mgp::Type::String),
+                     mgp::Parameter(Refactor::kArgumentsCopyPropKeys, {mgp::Type::List, mgp::Type::String},
+                                    mgp::Value(mgp::List{})),
+                 },
+                 {mgp::Return(Refactor::kReturnCategorize, mgp::Type::String)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
