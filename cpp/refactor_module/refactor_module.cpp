e8d03d025c7df1223aca874daeef649f3f5a12ff
idoraban <idora.ban@memgraph.io>
2023-08-29 06:39:25 +0000

    implement refactor module

--- cpp/refactor_module/refactor_module.cpp
+++ cpp/refactor_module/refactor_module.cpp
@@ -6,6 +6,15 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::memory = memory;
 
+    AddProcedure(Refactor::CloneNodes, Refactor::kProcedureCloneNodes, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Refactor::kArgumentsNodesToClone, {mgp::Type::List, mgp::Type::Node}),
+                  mgp::Parameter(Refactor::kArgumentsCloneRels, mgp::Type::Bool, false),
+                  mgp::Parameter(Refactor::kArgumentsSkipPropClone, {mgp::Type::List, mgp::Type::String},
+                                 mgp::Value(mgp::List{}))},
+                 {mgp::Return(Refactor::kReturnClonedNodeId, mgp::Type::Int),
+                  mgp::Return(Refactor::kReturnNewNode, mgp::Type::Node)},
+                 module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
