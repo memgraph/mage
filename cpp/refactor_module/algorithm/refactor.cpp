20976dd6f2928325b32fdb02388a38daf2e65eab
Matija <pintamato@gmail.com>
2023-09-04 11:49:03 +0200

    IMplement collapseNode

--- cpp/refactor_module/algorithm/refactor.cpp
+++ cpp/refactor_module/algorithm/refactor.cpp
@@ -1,6 +1,11 @@
 #include "refactor.hpp"
 
+void TransferProperties(const mgp::Node &node, mgp::Relationship &rel){
+  for(auto &[key, value]: node.Properties()){
+    rel.SetProperty(key, value);
+  }
 
+}
 
 
 void Refactor::CollapseNode(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
@@ -11,24 +16,25 @@ void Refactor::CollapseNode(mgp_list *args, mgp_graph *memgraph_graph, mgp_resul
     mgp::Graph graph = mgp::Graph(memgraph_graph);
     const mgp::Node node = arguments[0].ValueNode();
     
-    auto in_rel = node.InRelationships();
-
-    /*
-    auto inDegree = std::distance(node.InRelationships().begin(), node.InRelationships().end());
-    auto outDegree = std::distance(node.OutRelationships().begin(), node.OutRelationships().end());
-
-    if(inDegree != 1 && outDegree != 1){
+    if(node.InDegree() != 1 && node.OutDegree() != 1){
         throw mgp::ValueException("Out and in degree of the nodes both must be 1!");
     } 
 
+    
+    const mgp::Node from_node = (*node.InRelationships().begin()).From();
+    const mgp::Node to_node = (*node.OutRelationships().begin()).To();
 
-    //discuss if exception throw if from_node == node == to_node
+    
+    if(from_node == node && to_node == node){
+      throw mgp::ValueException("Nodes with self relationships are non collapsible!");
+    }
+    
 
-    mgp::Node from_node = (*node.InRelationships().begin()).From();
-    mgp::Node to_node = (*node.OutRelationships().begin()).To();
     mgp::Relationship new_rel = graph.CreateRelationship(from_node, to_node, "aa");
+    TransferProperties(node, new_rel);
     graph.DetachDeleteNode(node);
-    */
+ 
+
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
     return;
