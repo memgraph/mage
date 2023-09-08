128c0dda1475e92c4863b26068f18f4380fd1586
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-09-08 15:55:59 +0200

    Implement refactor clone_nodes (#330)

--- cpp/refactor_module/algorithm/refactor.cpp
+++ cpp/refactor_module/algorithm/refactor.cpp
@@ -1,5 +1,6 @@
 #include "refactor.hpp"
 
+#include <mg_utils.hpp>
 #include <unordered_set>
 
 mgp::Node getCategoryNode(mgp::Graph &graph, std::unordered_set<mgp::Node> &created_nodes,
@@ -59,6 +60,54 @@ void Refactor::Categorize(mgp_list *args, mgp_graph *memgraph_graph, mgp_result 
 
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kResultCategorize).c_str(), "success");
+  } catch (const std::exception &e) {
+    mgp::result_set_error_msg(result, e.what());
+    return;
+  }
+}
+
+void Refactor::InsertCloneNodesRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const int cycle_id,
+                                      const int node_id) {
+  auto *record = mgp::result_new_record(result);
+
+  mg_utility::InsertIntValueResult(record, std::string(kResultClonedNodeId).c_str(), cycle_id, memory);
+  mg_utility::InsertNodeValueResult(graph, record, std::string(kResultNewNode).c_str(), node_id, memory);
+}
+
+void Refactor::CloneNodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::memory = memory;
+  const auto arguments = mgp::List(args);
+  auto graph = mgp::Graph(memgraph_graph);
+  try {
+    const auto nodes = arguments[0].ValueList();
+    const auto clone_rels = arguments[1].ValueBool();
+    const auto skip_props = arguments[2].ValueList();
+    std::unordered_set<mgp::Value> skip_props_searchable{skip_props.begin(), skip_props.end()};
+
+    for (auto node : nodes) {
+      mgp::Node old_node = node.ValueNode();
+      mgp::Node new_node = graph.CreateNode();
+
+      for (auto label : old_node.Labels()) {
+        new_node.AddLabel(label);
+      }
+
+      for (auto prop : old_node.Properties()) {
+        if (skip_props.Empty() || !skip_props_searchable.contains(mgp::Value(prop.first))) {
+          new_node.SetProperty(prop.first, prop.second);
+        }
+      }
+
+      if (clone_rels) {
+        for (auto rel : old_node.InRelationships()) {
+          graph.CreateRelationship(rel.From(), new_node, rel.Type());
+        }
+        for (auto rel : old_node.OutRelationships()) {
+          graph.CreateRelationship(new_node, rel.To(), rel.Type());
+        }
+      }
+      InsertCloneNodesRecord(memgraph_graph, result, memory, old_node.Id().AsInt(), new_node.Id().AsInt());
+    }
   } catch (const std::exception &e) {
     mgp::result_set_error_msg(result, e.what());
     return;
--- cpp/refactor_module/algorithm/refactor.hpp
+++ cpp/refactor_module/algorithm/refactor.hpp
@@ -18,4 +18,19 @@ constexpr const std::string_view kArgumentsCopyPropKeys = "copy_props_list";
 constexpr const std::string_view kResultCategorize = "status";
 
 void Categorize(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+/* clone_nodes constants */
+
+constexpr const std::string_view kProcedureCloneNodes = "clone_nodes";
+constexpr const std::string_view kReturnClonedNodeId = "cloned_node_id";
+constexpr const std::string_view kReturnNewNode = "new_node";
+constexpr const std::string_view kArgumentsNodesToClone = "nodes";
+constexpr const std::string_view kArgumentsCloneRels = "clone_rels";
+constexpr const std::string_view kArgumentsSkipPropClone = "skip_props";
+constexpr const std::string_view kResultClonedNodeId = "cloned_node_id";
+constexpr const std::string_view kResultNewNode = "new_node";
+
+void InsertCloneNodesRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const int cycle_id,
+                            const int node_id);
+void CloneNodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Refactor
