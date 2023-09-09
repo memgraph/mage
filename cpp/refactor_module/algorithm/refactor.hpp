22da9e8c4c561111f5d03c646f8be2269438ed08
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-09-09 13:47:44 +0200

    [E-idora < T368, T369] Implement refactor.clone_subgraph and subgraph_from_path (#335)
    Implement refactor.clone_subgraph and refactor.clone_subgraph_from_path

--- cpp/refactor_module/algorithm/refactor.hpp
+++ cpp/refactor_module/algorithm/refactor.hpp
@@ -17,8 +17,6 @@ constexpr const std::string_view kArgumentsCopyPropKeys = "copy_props_list";
 
 constexpr const std::string_view kResultCategorize = "status";
 
-void Categorize(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-
 /* clone_nodes constants */
 
 constexpr const std::string_view kProcedureCloneNodes = "clone_nodes";
@@ -30,7 +28,26 @@ constexpr const std::string_view kArgumentsSkipPropClone = "skip_props";
 constexpr const std::string_view kResultClonedNodeId = "cloned_node_id";
 constexpr const std::string_view kResultNewNode = "new_node";
 
+/* clone_subgraph_from_paths constants */
+
+constexpr const std::string_view kProcedureCSFP = "clone_subgraph_from_paths";
+constexpr const std::string_view kArgumentsPath = "paths";
+constexpr const std::string_view kArgumentsConfigMap = "config";
+
+/* clone_subgraph constants */
+
+constexpr const std::string_view kProcedureCloneSubgraph = "clone_subgraph";
+constexpr const std::string_view kArgumentsNodes = "nodes";
+constexpr const std::string_view kArgumentsRels = "rels";
+
+void Categorize(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 void InsertCloneNodesRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const int cycle_id,
                             const int node_id);
 void CloneNodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void CloneNodesAndRels(mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory,
+                       const std::vector<mgp::Node> &nodes, const std::vector<mgp::Relationship> &rels,
+                       const mgp::Map &config_map);
+void CloneSubgraphFromPaths(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void CloneSubgraph(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
 }  // namespace Refactor
