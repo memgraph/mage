9fc7984c8ea5be063c29b73fa1cf406f52c5e78b
ind1xa <95344788+ind1xa@users.noreply.github.com>
2023-09-10 07:20:27 +0200

    [E-refactor < E-idora] Add part of refactor procedures (#350)
    Implement refactor.categorize, refactor.clone_nodes, refactor.clone_subgraph and refactor.clone_subgraph_from_path

--- cpp/refactor_module/algorithm/refactor.hpp
+++ cpp/refactor_module/algorithm/refactor.hpp
@@ -3,6 +3,42 @@
 #include <mgp.hpp>
 
 namespace Refactor {
+/* categorize constants */
+
+constexpr const std::string_view kProcedureCategorize = "categorize";
+constexpr const std::string_view kReturnCategorize = "status";
+
+constexpr const std::string_view kArgumentsCatSourceKey = "original_prop_key";
+constexpr const std::string_view kArgumentsCatRelType = "rel_type";
+constexpr const std::string_view kArgumentsCatRelOutgoing = "is_outgoing";
+constexpr const std::string_view kArgumentsCatLabelName = "new_label";
+constexpr const std::string_view kArgumentsCatPropKey = "new_prop_name_key";
+constexpr const std::string_view kArgumentsCopyPropKeys = "copy_props_list";
+
+constexpr const std::string_view kResultCategorize = "status";
+
+/* clone_nodes constants */
+
+constexpr const std::string_view kProcedureCloneNodes = "clone_nodes";
+constexpr const std::string_view kReturnClonedNodeId = "cloned_node_id";
+constexpr const std::string_view kReturnNewNode = "new_node";
+constexpr const std::string_view kArgumentsNodesToClone = "nodes";
+constexpr const std::string_view kArgumentsCloneRels = "clone_rels";
+constexpr const std::string_view kArgumentsSkipPropClone = "skip_props";
+constexpr const std::string_view kResultClonedNodeId = "cloned_node_id";
+constexpr const std::string_view kResultNewNode = "new_node";
+
+/* clone_subgraph_from_paths constants */
+
+constexpr const std::string_view kProcedureCSFP = "clone_subgraph_from_paths";
+constexpr const std::string_view kArgumentsPath = "paths";
+constexpr const std::string_view kArgumentsConfigMap = "config";
+
+/* clone_subgraph constants */
+
+constexpr const std::string_view kProcedureCloneSubgraph = "clone_subgraph";
+constexpr const std::string_view kArgumentsNodes = "nodes";
+constexpr const std::string_view kArgumentsRels = "rels";
 
 /* rename_label constants */
 constexpr std::string_view kProcedureRenameLabel = "rename_label";
@@ -17,6 +53,17 @@ constexpr std::string_view kRenameNodePropertyArg1 = "old_property";
 constexpr std::string_view kRenameNodePropertyArg2 = "new_property";
 constexpr std::string_view kRenameNodePropertyArg3 = "nodes";
 constexpr std::string_view kRenameNodePropertyResult = "nodes_changed";
+
+void Categorize(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+void InsertCloneNodesRecord(mgp_graph *graph, mgp_result *result, mgp_memory *memory, const int cycle_id,
+                            const int node_id);
+void CloneNodes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void CloneNodesAndRels(mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory,
+                       const std::vector<mgp::Node> &nodes, const std::vector<mgp::Relationship> &rels,
+                       const mgp::Map &config_map);
+void CloneSubgraphFromPaths(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void CloneSubgraph(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 void RenameLabel(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
