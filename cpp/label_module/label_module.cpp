7150b1010a94b5942f4400bfecab6621ea004143
imilinovic <ivan.milinovic123@gmail.com>
2023-09-15 13:22:20 +0200

    renaming and changing to functions

--- cpp/label_module/label_module.cpp
+++ cpp/label_module/label_module.cpp
@@ -5,10 +5,10 @@
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
   try {
     mgp::memory = memory;
-    AddProcedure(Label::Exists, Label::kProcedureExists, mgp::ProcedureType::Read,
-                 {mgp::Parameter(Label::kArgumentsNode, mgp::Type::Any),
-                  mgp::Parameter(Label::kArgumentsLabel, mgp::Type::String)},
-                 {mgp::Return(Label::kReturnExists, mgp::Type::Bool)}, module, memory);
+    mgp::AddFunction(Label::Exists, Label::kFunctionExists,
+                     {mgp::Parameter(Label::kArgumentsNode, mgp::Type::Any),
+                      mgp::Parameter(Label::kArgumentsLabel, mgp::Type::String)},
+                     module, memory);
 
   } catch (const std::exception &e) {
     return 1;
--- cpp/refactor_module/refactor_module.cpp
+++ cpp/refactor_module/refactor_module.cpp
@@ -1,4 +1,5 @@
 #include <mgp.hpp>
+#include <string>
 
 #include "algorithm/refactor.hpp"
 
@@ -32,16 +33,18 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
                   mgp::Parameter(Refactor::kArgumentsCloneRels, mgp::Type::Bool, false),
                   mgp::Parameter(Refactor::kArgumentsSkipPropClone, {mgp::Type::List, mgp::Type::String},
                                  mgp::Value(mgp::List{}))},
-                 {mgp::Return(Refactor::kReturnClonedNodeId, mgp::Type::Int),
-                  mgp::Return(Refactor::kReturnNewNode, mgp::Type::Node)},
+                 {mgp::Return(Refactor::kResultClonedNodeId, mgp::Type::Int),
+                  mgp::Return(Refactor::kResultNewNode, mgp::Type::Node),
+                  mgp::Return(Refactor::kResultCloneNodeError, mgp::Type::String)},
                  module, memory);
 
     AddProcedure(
         Refactor::CloneSubgraphFromPaths, Refactor::kProcedureCSFP, mgp::ProcedureType::Write,
         {mgp::Parameter(Refactor::kArgumentsPath, {mgp::Type::List, mgp::Type::Path}),
          mgp::Parameter(Refactor::kArgumentsConfigMap, {mgp::Type::Map, mgp::Type::Any}, mgp::Value(mgp::Map{}))},
-        {mgp::Return(Refactor::kReturnClonedNodeId, mgp::Type::Int),
-         mgp::Return(Refactor::kReturnNewNode, mgp::Type::Node)},
+        {mgp::Return(Refactor::kResultClonedNodeId, mgp::Type::Int),
+         mgp::Return(Refactor::kResultNewNode, mgp::Type::Node),
+         mgp::Return(Refactor::kResultCloneNodeError, mgp::Type::String)},
         module, memory);
 
     AddProcedure(
@@ -49,8 +52,9 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
         {mgp::Parameter(Refactor::kArgumentsNodes, {mgp::Type::List, mgp::Type::Node}),
          mgp::Parameter(Refactor::kArgumentsRels, {mgp::Type::List, mgp::Type::Relationship}, mgp::Value(mgp::List())),
          mgp::Parameter(Refactor::kArgumentsConfigMap, {mgp::Type::Map, mgp::Type::Any}, mgp::Value(mgp::Map{}))},
-        {mgp::Return(Refactor::kReturnClonedNodeId, mgp::Type::Int),
-         mgp::Return(Refactor::kReturnNewNode, mgp::Type::Node)},
+        {mgp::Return(Refactor::kResultClonedNodeId, mgp::Type::Int),
+         mgp::Return(Refactor::kResultNewNode, mgp::Type::Node),
+         mgp::Return(Refactor::kResultCloneNodeError, mgp::Type::String)},
         module, memory);
 
     AddProcedure(Refactor::RenameLabel, Refactor::kProcedureRenameLabel, mgp::ProcedureType::Write,
@@ -66,8 +70,9 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
                  {mgp::Return(Refactor::kRenameNodePropertyResult, mgp::Type::Int)}, module, memory);
     AddProcedure(Refactor::Invert, std::string(Refactor::kProcedureInvert).c_str(), mgp::ProcedureType::Write,
                  {mgp::Parameter(std::string(Refactor::kArgumentRelationship).c_str(), mgp::Type::Any)},
-                 {mgp::Return(std::string(Refactor::kReturnIdInvert).c_str(), mgp::Type::Int),
-                  mgp::Return(std::string(Refactor::kReturnRelationshipInvert).c_str(), mgp::Type::Relationship)},
+                 {mgp::Return(std::string(Refactor::kResultIdInvert).c_str(), mgp::Type::Int),
+                  mgp::Return(std::string(Refactor::kResultRelationshipInvert).c_str(), mgp::Type::Relationship),
+                  mgp::Return(std::string(Refactor::kResultErrorInvert).c_str(), mgp::Type::String)},
                  module, memory);
     AddProcedure(Refactor::CollapseNode, std::string(Refactor::kProcedureCollapseNode).c_str(),
                  mgp::ProcedureType::Write,
