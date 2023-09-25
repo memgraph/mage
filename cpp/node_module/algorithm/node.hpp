e4031829496817e7ed2038163e2c57e1e31ebfd5
Matija <pintamato@gmail.com>
2023-09-25 17:30:13 +0200

    Revert "Implement node in out degree"
    This reverts commit 4420226f938ec18c9a575cb74dda859e32077ec8.

--- cpp/node_module/algorithm/node.hpp
+++ cpp/node_module/algorithm/node.hpp
@@ -25,16 +25,6 @@ constexpr std::string_view kRelationshipTypesArg1 = "node";
 constexpr std::string_view kRelationshipTypesArg2 = "types";
 constexpr std::string_view kResultRelationshipTypes = "relationship_types";
 
-/* degree_in constants */
-constexpr std::string_view kFunctionDegreeIn = "degree_in";
-constexpr std::string_view kDegreeInArg1 = "node";
-constexpr std::string_view kDegreeInArg2 = "types";
-
-/* degree_in constants */
-constexpr std::string_view kFunctionDegreeOut = "degree_out";
-constexpr std::string_view kDegreeOutArg1 = "node";
-constexpr std::string_view kDegreeOutArg2 = "types";
-
 void RelationshipsExist(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 bool RelationshipExist(const mgp::Node &node, std::string &rel_type);
@@ -44,9 +34,5 @@ bool FindRelationship(std::unordered_set<std::string_view> types, mgp::Relations
 void RelationshipExists(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 void RelationshipTypes(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-
-void DegreeIn(mgp_list *args, mgp_func_context *ctx, mgp_func_result *res, mgp_memory *memory);
-
-void DegreeOut(mgp_list *args, mgp_func_context *ctx, mgp_func_result *res, mgp_memory *memory);
 
 }  // namespace Node
