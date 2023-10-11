79f49db6abb83b678b14406d9349f545266880a3
Matija <pintamato@gmail.com>
2023-10-11 18:19:28 +0200

    Implement algo.cover

--- cpp/algo_module/algorithm/algo.hpp
+++ cpp/algo_module/algorithm/algo.hpp
@@ -1,5 +1,14 @@
 #pragma once
 
 #include <mgp.hpp>
+#include <unordered_set>
 
-namespace Algo {}  // namespace Algo
+namespace Algo {
+
+/* cover constants */
+constexpr std::string_view kProcedureCover = "cover";
+constexpr std::string_view kCoverArg1 = "nodes";
+constexpr std::string_view kCoverRet1 = "rel";
+
+void Cover(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+}  // namespace Algo
