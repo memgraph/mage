e82f65f1be85a9d3263066d8fa188405224c718a
imilinovic <ivan.milinovic123@gmail.com>
2023-10-14 16:59:17 +0200

    merge.relationship

--- cpp/merge_module/algorithm/merge.hpp
+++ cpp/merge_module/algorithm/merge.hpp
@@ -2,4 +2,18 @@
 
 #include <mgp.hpp>
 
-namespace Merge {}  // namespace Merge
+namespace Merge {
+
+/* relationship constants */
+constexpr const std::string_view kProcedureRelationship = "relationship";
+constexpr const std::string_view kRelationshipArg1 = "startNode";
+constexpr const std::string_view kRelationshipArg2 = "relationshipType";
+constexpr const std::string_view kRelationshipArg3 = "identProps";
+constexpr const std::string_view kRelationshipArg4 = "props";
+constexpr const std::string_view kRelationshipArg5 = "endNode";
+constexpr const std::string_view kRelationshipArg6 = "onMatchProps";
+constexpr const std::string_view kRelationshipResult = "rel";
+
+void Relationship(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+
+}  // namespace Merge
--- cpp/merge_module/merge_module.cpp
+++ cpp/merge_module/merge_module.cpp
@@ -6,6 +6,15 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
   try {
     mgp::MemoryDispatcherGuard guard{memory};
 
+    AddProcedure(Merge::Relationship, Merge::kProcedureRelationship, mgp::ProcedureType::Write,
+                 {mgp::Parameter(Merge::kRelationshipArg1, mgp::Type::Node),
+                  mgp::Parameter(Merge::kRelationshipArg2, mgp::Type::String),
+                  mgp::Parameter(Merge::kRelationshipArg3, mgp::Type::Map),
+                  mgp::Parameter(Merge::kRelationshipArg4, mgp::Type::Map),
+                  mgp::Parameter(Merge::kRelationshipArg5, mgp::Type::Node),
+                  mgp::Parameter(Merge::kRelationshipArg6, mgp::Type::Map)},
+                 {mgp::Return(Merge::kRelationshipResult, mgp::Type::Relationship)}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
