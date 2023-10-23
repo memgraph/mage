8f73d16c54eef7f8fb55585a21c4716177141991
Matija Pintarić <99442742+mpintaric55334@users.noreply.github.com>
2023-10-23 12:48:57 +0200

    Implement algo cover (#389)

--- cpp/algo_module/algorithm/algo.cpp
+++ cpp/algo_module/algorithm/algo.cpp
@@ -1,1 +1,28 @@
 #include "algo.hpp"
+
+void Algo::Cover(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
+  mgp::MemoryDispatcherGuard guard{memory};
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+  try {
+    auto list_nodes = arguments[0].ValueList();
+    std::unordered_set<mgp::Node> nodes;
+    for (const auto &elem : list_nodes) {
+      auto node = elem.ValueNode();
+      nodes.insert(node);
+    }
+
+    for (const auto &node : nodes) {
+      for (const auto rel : node.OutRelationships()) {
+        if (nodes.find(rel.To()) != nodes.end()) {
+          auto record = record_factory.NewRecord();
+          record.Insert(std::string(kCoverRet1).c_str(), rel);
+        }
+      }
+    }
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
