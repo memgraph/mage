4076074b85f34557e0f9c78163e0e96a2003fa3e
Matija <pintamato@gmail.com>
2023-10-25 18:00:27 +0200

    Delete QM

--- e2e/schema_test/test_node_type_properties2/input.cyp
+++
@@ -1,1 +0,0 @@
-CREATE (n:NonMandatory)-[r:RELATIONSHIP]->(m:Mandatory {property: ["2",3 ,4]});
--- python/convert.py
+++
@@ -1,9 +0,0 @@
-import mgp
-from json import loads
-
-
-@mgp.function
-def str2object(string: str) -> mgp.Any:
-    if string:
-        return loads(string)
-    return None
