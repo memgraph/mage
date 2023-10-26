fefc58b78cc23f55f3fa36a74a213a3573bf40e3
imilinovic <ivan.milinovic123@gmail.com>
2023-10-26 15:10:42 +0200

    merge with parent

--- cpp/algo_module/algorithm/algo.hpp
+++ cpp/algo_module/algorithm/algo.hpp
@@ -40,6 +40,8 @@ constexpr const std::string_view kAllSimplePathsArg4 = "max_length";
 constexpr const std::string_view kResultAllSimplePaths = "path";
 
 /* cover constants */
+constexpr std::string_view kProcedureCover = "cover";
+constexpr std::string_view kCoverArg1 = "nodes";
 constexpr std::string_view kCoverRet1 = "rel";
 
 void AllSimplePaths(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
