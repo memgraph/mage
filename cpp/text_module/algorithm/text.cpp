047904b92d035a4ba60d4e46a4eda5961b998d0f
Andi Skrgat <andi8647@gmail.com>
2023-11-24 10:28:45 +0100

    Add impl of regex search

--- cpp/text_module/algorithm/text.cpp
+++ cpp/text_module/algorithm/text.cpp
@@ -1,7 +1,10 @@
 #include "text.hpp"
 
-void Text::Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
-  mgp::MemoryDispatcherGuard guard{memory};;
+#include <regex>
+#include <vector>
+
+void Text::Join(mgp_list *args, mgp_graph * /*memgraph_graph*/, mgp_result *result, mgp_memory *memory) {
+  mgp::MemoryDispatcherGuard guard{memory};
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
 
@@ -26,6 +29,48 @@ void Text::Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, m
 
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kResultJoin).c_str(), result);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Text::RegexGroups(mgp_list *args, mgp_graph * /*memgraph_graph*/, mgp_result *result, mgp_memory *memory) {
+  mgp::MemoryDispatcherGuard guard{memory};
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    const std::string input{arguments[0].ValueString()};
+    const std::string regex_str{arguments[1].ValueString()};
+
+    std::regex regex{regex_str};
+
+    size_t mark_count = regex.mark_count();
+    std::vector<int> submatches;
+    for (size_t i = 0; i <= mark_count; ++i) {
+      submatches.emplace_back(i);
+    }
+
+    mgp::List all_results;
+    mgp::List local_results;
+    uint32_t cnt = 1;
+    auto words_begin = std::sregex_token_iterator(input.begin(), input.end(), regex, submatches);
+    auto words_end = std::sregex_token_iterator();
+
+    for (auto it = words_begin; it != words_end; it++, cnt++) {
+      if (auto match = it->str(); !match.empty()) {
+        local_results.AppendExtend(mgp::Value(match));
+      }
+      if (cnt % (mark_count + 1) == 0) {
+        all_results.AppendExtend(mgp::Value(local_results));
+        local_results = mgp::List();
+      }
+    }
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultRegexGroups).c_str(), all_results);
 
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
