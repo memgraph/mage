844555b55cae7b13698e1a01babf7dc01240aef4
Andi Skrgat <andi8647@gmail.com>
2023-11-27 12:08:17 +0100

    Add impl of format

--- cpp/text_module/algorithm/text.cpp
+++ cpp/text_module/algorithm/text.cpp
@@ -1,7 +1,11 @@
 #include "text.hpp"
 
-void Text::Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory) {
-  mgp::MemoryDispatcherGuard guard{memory};;
+#include <fmt/args.h>
+#include <fmt/format.h>
+
+void Text::Join(mgp_list *args, mgp_graph * /*memgraph_graph*/, mgp_result *result, mgp_memory *memory) {
+  mgp::MemoryDispatcherGuard guard{memory};
+  ;
   const auto arguments = mgp::List(args);
   const auto record_factory = mgp::RecordFactory(result);
 
@@ -26,6 +30,31 @@ void Text::Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, m
 
     auto record = record_factory.NewRecord();
     record.Insert(std::string(kResultJoin).c_str(), result);
+
+  } catch (const std::exception &e) {
+    record_factory.SetErrorMessage(e.what());
+    return;
+  }
+}
+
+void Text::Format(mgp_list *args, mgp_graph * /*memgraph_graph*/, mgp_result *result, mgp_memory *memory) {
+  mgp::MemoryDispatcherGuard guard{memory};
+  ;
+  const auto arguments = mgp::List(args);
+  const auto record_factory = mgp::RecordFactory(result);
+
+  try {
+    std::string text{arguments[0].ValueString()};
+    const auto params{arguments[1].ValueList()};
+
+    fmt::dynamic_format_arg_store<fmt::format_context> storage;
+    std::for_each(params.begin(), params.end(),
+                  [&storage](const mgp::Value &value) { storage.push_back(value.ToString()); });
+
+    std::string result = fmt::vformat(text, storage);
+
+    auto record = record_factory.NewRecord();
+    record.Insert(std::string(kResultFormat).c_str(), result);
 
   } catch (const std::exception &e) {
     record_factory.SetErrorMessage(e.what());
--- cpp/text_module/algorithm/text.hpp
+++ cpp/text_module/algorithm/text.hpp
@@ -5,11 +5,16 @@
 namespace Text {
 
 /* join constants */
-constexpr const std::string_view kProcedureJoin = "join";
-constexpr const std::string_view kJoinArg1 = "strings";
-constexpr const std::string_view kJoinArg2 = "delimiter";
-constexpr const std::string_view kResultJoin = "string";
+constexpr std::string_view kProcedureJoin = "join";
+constexpr std::string_view kJoinArg1 = "strings";
+constexpr std::string_view kJoinArg2 = "delimiter";
+constexpr std::string_view kResultJoin = "string";
+/* format constants */
+constexpr std::string_view kProcedureFormat = "format";
+constexpr std::string_view kStringToFormat = "text";
+constexpr std::string_view kParameters = "params";
+constexpr std::string_view kResultFormat = "result";
 
 void Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
-
+void Format(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 }  // namespace Text
