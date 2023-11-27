9d4f9b90f2eb3bb901dc4f9e73b8d83aa77052d3
Andi <andi8647@gmail.com>
2023-11-27 12:44:08 +0100

    Add implementation of regex search (#420)

--- cpp/text_module/algorithm/text.hpp
+++ cpp/text_module/algorithm/text.hpp
@@ -9,7 +9,13 @@ constexpr const std::string_view kProcedureJoin = "join";
 constexpr const std::string_view kJoinArg1 = "strings";
 constexpr const std::string_view kJoinArg2 = "delimiter";
 constexpr const std::string_view kResultJoin = "string";
+/* regex_groups constants */
+constexpr const std::string_view kProcedureRegexGroups = "regexGroups";
+constexpr const std::string_view kInput = "input";
+constexpr const std::string_view kRegex = "regex";
+constexpr const std::string_view kResultRegexGroups = "results";
 
 void Join(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
+void RegexGroups(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result, mgp_memory *memory);
 
 }  // namespace Text
--- cpp/text_module/text_module.cpp
+++ cpp/text_module/text_module.cpp
@@ -11,6 +11,11 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
                   mgp::Parameter(Text::kJoinArg2, mgp::Type::String)},
                  {mgp::Return(Text::kResultJoin, mgp::Type::String)}, module, memory);
 
+    AddProcedure(Text::RegexGroups, Text::kProcedureRegexGroups, mgp::ProcedureType::Read,
+                 {mgp::Parameter(Text::kInput, mgp::Type::String),
+                  mgp::Parameter(Text::kRegex, mgp::Type::String)},
+                 {mgp::Return(Text::kResultRegexGroups, {mgp::Type::List, mgp::Type::Any})}, module, memory);
+
   } catch (const std::exception &e) {
     return 1;
   }
