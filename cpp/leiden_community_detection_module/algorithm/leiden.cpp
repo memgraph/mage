3ef8d3662df2f8d113eddbfe4d6f00d7bcdb997c
DavIvek <david.ivekovic@memgraph.io>
2024-09-03 14:04:50 +0200

    fix naming

--- cpp/leiden_community_detection_module/algorithm/leiden.cpp
+++ cpp/leiden_community_detection_module/algorithm/leiden.cpp
@@ -3,10 +3,6 @@
 namespace leiden_alg {
 
 
-constexpr int kReplaceMap = 0;
-constexpr int kThreadsOpt = 1;
-constexpr int kNumColors = 16;
-
 std::vector<std::int64_t> GetCommunities(const mg_graph::GraphView<> &graph) {
     auto number_of_nodes = graph.Nodes().size();
 
