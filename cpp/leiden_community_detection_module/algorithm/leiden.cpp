58c0a645c23e3fff3a2116670e9c7365b13a6842
DavIvek <david.ivekovic@memgraph.io>
2024-09-05 14:07:13 +0200

    wip

--- cpp/leiden_community_detection_module/algorithm/leiden.cpp
+++ cpp/leiden_community_detection_module/algorithm/leiden.cpp
@@ -1,15 +1,45 @@
 #include "leiden.hpp"
+#include <cstddef>
 #include <queue>
-#include <random>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 #include <cstdint>
-#include <type_traits>
 #include "data_structures/graph_view.hpp"
 
 namespace leiden_alg {
 
+struct Graph {
+  std::size_t num_nodes;
+  std::unordered_map<std::int64_t, std::unordered_set<std::int64_t>> adjacency_list;
+
+  // Add an edge to the graph
+  void addEdge(std::int64_t u, std::int64_t v) {
+      adjacency_list[u].insert(v);
+      adjacency_list[v].insert(u);
+  }
+
+  std::size_t size() const {
+      return num_nodes;
+  }
+
+  const std::unordered_set<std::int64_t> &neighbors(std::int64_t u) const {
+      return adjacency_list.at(u);
+  }
+};
+
 const double gamma = 1.0;
+
+// Function to count edges between a node and a set of nodes in a community
+int countEdgesBetween(const Graph &G, int node, const std::unordered_set<int> &community) {
+    int count = 0;
+    for (int neighbor : G.neighbors(node)) {
+        if (community.count(neighbor)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 // TODO: Optimize this function
 std::int64_t BinomialCoefficient(const int n, const int k) {
@@ -50,28 +80,32 @@ double computeCPM (std::unordered_map<std::int64_t, std::int64_t> &partitions, c
     return h;
 }
 
-std::unordered_map<std::int64_t, std::int64_t> moveNodesFast(std::unordered_map<std::int64_t, std::int64_t> &partitions, const mg_graph::GraphView<> &graph) {
+std::unordered_map<std::int64_t, std::int64_t> moveNodesFast(std::unordered_map<std::int64_t, std::int64_t> &partitions, const mg_graph::GraphView<> &memgraph_graph, const Graph &graph) {
     std::queue<std::int64_t> nodes;
-    for (std::int64_t i = 0; i < graph.Nodes().size(); i++) {
+    std::unordered_set<std::int64_t> nodes_set;
+    for (std::int64_t i = 0; i < graph.size(); i++) {
         nodes.push(i);
+        nodes_set.insert(i);
     }
 
     while(!nodes.empty()) {
       auto node_id = nodes.front();
       nodes.pop();
+      nodes_set.erase(node_id);
       auto best_community = partitions[node_id];
-      auto best_delta = computeCPM(partitions, graph);
-      for (const auto &neighbour : graph.Neighbours(node_id)) {
+      auto best_delta = computeCPM(partitions, memgraph_graph); // fix later
+      for (const auto &neighbour : graph.neighbors(node_id)) {
           auto &current_community = partitions[node_id];
-          current_community = neighbour.node_id;
-          auto delta = computeCPM(partitions, graph);
+          current_community = neighbour;
+          auto delta = computeCPM(partitions, memgraph_graph); // fix later
           if (delta > best_delta) {
               best_delta = delta;
-              best_community = neighbour.node_id;
-              // find neighbours that are not in this community and add them to the queue
-              for (const auto &neighbour : graph.Neighbours(node_id)) {
-                  if (partitions[neighbour.node_id] == current_community) {
-                      nodes.push(neighbour.node_id);
+              best_community = neighbour;
+              // find neighbours that are not in this community and in the queue and add them
+              for (const auto &neighbour2 : graph.neighbors(node_id)) {
+                  if (partitions[neighbour2] != best_community && nodes_set.find(neighbour2) == nodes_set.end()) {
+                      nodes.push(neighbour2);
+                      nodes_set.insert(neighbour2);
                   }
               }
           }
@@ -81,5 +115,45 @@ std::unordered_map<std::int64_t, std::int64_t> moveNodesFast(std::unordered_map<
       }
     }
 }
+
+std::unordered_map<std::int64_t, std::int64_t> singletonPartition(const Graph &graph) {
+    std::unordered_map<std::int64_t, std::int64_t> partitions;
+    for (std::int64_t i = 0; i < graph.size(); i++) {
+        partitions[i] = i;
+    }
+    return partitions;
+}
+
+std::unordered_map<std::int64_t, std::int64_t> mergeNodesSubset(std::unordered_map<std::int64_t, std::int64_t> &partitions, const Graph &graph, std::int64_t subset) {
+    // TODO: implement this function
+}
+
+std::unordered_map<std::int64_t, std::int64_t> refinePartition(std::unordered_map<std::int64_t, std::int64_t> &partitions, Graph &graph) {
+  auto refined_partitions = singletonPartition(graph);
+  for (const auto &[node_id, community_id] : partitions) {
+      // TODO: merge nodes subset function
+  }
+  return refined_partitions;
+}
+
+
+void aggregateGraph (std::unordered_map<std::int64_t, std::int64_t> &partitions, Graph &graph) {
+  // communities becomes the new nodes
+  std::unordered_map<std::int64_t, std::unordered_set<std::int64_t>> new_adjacency_list;
+  for (const auto &[node_id, community_id] : partitions) {
+      new_adjacency_list[community_id] = std::unordered_set<std::int64_t>();
+  }
+
+  for (const auto &[node_id, community_id] : partitions) {
+      for (const auto &neighbour : graph.neighbors(node_id)) {
+          if (partitions[neighbour] != community_id) {
+              new_adjacency_list[community_id].insert(partitions[neighbour]);
+          }
+      }
+  }
+
+  graph.adjacency_list = std::move(new_adjacency_list);
+}
+
 
 }  // namespace leiden_alg
