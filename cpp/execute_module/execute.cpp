7f743e512fd84878e0065a9e2b13b8362c1bcbbc
Josip Mrden <josip.mrden@memgraph.io>
2024-12-20 11:27:32 +0100

    Add retry mechanism

--- cpp/execute_module/execute.cpp
+++ cpp/execute_module/execute.cpp
@@ -1,12 +1,23 @@
 #include <fmt/core.h>
+#include <chrono>
+#include <mg_exceptions.hpp>
 #include <mgp.hpp>
 #include <string>
 #include <string_view>
+#include <thread>
 
 constexpr char *kProcedureExecuteQuery = "query";
 constexpr char *kArgumentInputQuery = "input_query";
 constexpr char *kArgumentParameters = "parameters";
+constexpr char *kArgumentConfig = "config";
 constexpr char *kReturnSuccess = "success";
+
+constexpr char *kConfigKeyMaxRetries = "max_retries";
+constexpr char *kConfigKeyRetryType = "retry_type";
+constexpr char *kCOnfigKeyInitialBackoff = "initial_backoff";
+
+constexpr char *kConfigKeyExponentialBackoff = "EXPONENTIAL";
+constexpr char *kCOnfigKeyLinearBackoff = "LINEAR";
 
 void replaceString(std::string &subject, const std::string &search, const std::string &replace) {
   size_t pos = 0;
@@ -25,6 +36,43 @@ void ExecuteQuery(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result,
 
   auto input_query = std::string(arguments[0].ValueString());
   const auto parameters = arguments[1].ValueMap();
+  const auto config = arguments[2].ValueMap();
+
+  auto max_retries = 0;
+  auto backoff = "EXPONENTIAL";
+  auto initial_backoff = 10;
+  if (config.KeyExists(kConfigKeyMaxRetries)) {
+    if (!config.At(kConfigKeyMaxRetries).IsInt()) {
+      record_factory.SetErrorMessage("max_retries parameter needs to be an integer!");
+      record.Insert(kReturnSuccess, false);
+      return;
+    }
+    max_retries = config.At(kConfigKeyMaxRetries).ValueInt();
+  }
+
+  if (max_retries != 0) {
+    if (config.KeyExists(kConfigKeyRetryType)) {
+      if (!config.At(kConfigKeyRetryType).IsString()) {
+        record_factory.SetErrorMessage("retry_type parameter needs to be an string!");
+        record.Insert(kReturnSuccess, false);
+        return;
+      }
+      auto retry_type = std::string(config.At(kConfigKeyRetryType).ValueString());
+      if (retry_type != kConfigKeyExponentialBackoff && retry_type != kCOnfigKeyLinearBackoff) {
+        record_factory.SetErrorMessage("retry_type parameter needs to either EXPONENTIAL or LINEAR!");
+        record.Insert(kReturnSuccess, false);
+        return;
+      }
+    }
+    if (config.KeyExists(kCOnfigKeyInitialBackoff)) {
+      if (!config.At(kCOnfigKeyInitialBackoff).IsInt()) {
+        record_factory.SetErrorMessage("initial_backoff parameter needs to be an integer!");
+        record.Insert(kReturnSuccess, false);
+        return;
+      }
+      initial_backoff = config.At(kCOnfigKeyInitialBackoff).ValueInt();
+    }
+  }
 
   for (const auto &[key, value] : parameters) {
     if (value.IsString()) {
@@ -40,18 +88,34 @@ void ExecuteQuery(mgp_list *args, mgp_graph *memgraph_graph, mgp_result *result,
     }
   }
 
-  try {
-    auto input_query_execution = mgp::QueryExecution(memgraph_graph);
-    auto execution_result = input_query_execution.ExecuteQuery(input_query);
+  do {
+    try {
+      auto input_query_execution = mgp::QueryExecution(memgraph_graph);
+      auto execution_result = input_query_execution.ExecuteQuery(input_query);
 
-    while (const auto maybe_result = execution_result.PullOne()) {
+      while (execution_result.PullOne()) {
+      }
+
+      record.Insert(kReturnSuccess, true);
+    } catch (const mg_exception::SerializationException &e) {
+      record_factory.SetErrorMessage(e.what());
+      record.Insert(kReturnSuccess, false);
+      max_retries--;
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(initial_backoff));
+      if (backoff == kConfigKeyExponentialBackoff) {
+        initial_backoff *= 2;
+      }
+    } catch (const std::exception &e) {
+      record_factory.SetErrorMessage(e.what());
+      record.Insert(kReturnSuccess, false);
+      return;
     }
+  } while (max_retries > 0);
 
-    record.Insert(kReturnSuccess, true);
-  } catch (const std::exception &e) {
-    record_factory.SetErrorMessage(e.what());
-    record.Insert(kReturnSuccess, false);
-  }
+  record_factory.SetErrorMessage("Did not successfully execute query!");
+  record.Insert(kReturnSuccess, false);
+  return;
 }
 
 extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *memory) {
@@ -59,7 +123,8 @@ extern "C" int mgp_init_module(struct mgp_module *module, struct mgp_memory *mem
     mgp::MemoryDispatcherGuard guard{memory};
     mgp::AddProcedure(
         ExecuteQuery, kProcedureExecuteQuery, mgp::ProcedureType::Read,
-        {mgp::Parameter(kArgumentInputQuery, mgp::Type::String), mgp::Parameter(kArgumentParameters, mgp::Type::Map)},
+        {mgp::Parameter(kArgumentInputQuery, mgp::Type::String), mgp::Parameter(kArgumentParameters, mgp::Type::Map),
+         mgp::Parameter(kArgumentConfig, mgp::Type::Map)},
         {mgp::Return(kReturnSuccess, mgp::Type::Bool)}, module, memory);
   } catch (const std::exception &e) {
     return 1;
