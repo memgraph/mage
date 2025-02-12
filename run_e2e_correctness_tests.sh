5fc7b1d658196e3ccfbe47d82b4189347a9b05eb
DavIvek <david.ivekovic@memgraph.io>
2025-02-12 17:08:34 +0100

    run corretness tests in docker

--- run_e2e_correctness_tests.sh
+++ run_e2e_correctness_tests.sh
@@ -3,33 +3,26 @@
 MEMGRAPH_PORT=$1
 NEO4J_PORT=$2
 NEO4J_CONTAINER=$3
+MAGE_CONTAINER=$4
+MEMGRAPH_NETWORK=$5
 
-timeout=20
-counter=0
-
+echo "Start Neo4j..."
 docker run --rm \
-          --name "$NEO4J_CONTAINER"  \
-          -p 7474:7474 -p "$NEO4J_PORT":7687 \
-          -d \
-          -v "$HOME/neo4j/plugins:/plugins" \
-          --env NEO4J_AUTH=none  \
-          -e NEO4J_apoc_export_file_enabled=true \
-          -e NEO4J_apoc_import_file_enabled=true \
-          -e NEO4J_apoc_import_file_use__neo4j__config=true  \
-          -e NEO4J_PLUGINS='["apoc"]' neo4j:5.10.0
+    --name "$NEO4J_CONTAINER"  \
+    --network "$MEMGRAPH_NETWORK" \
+    -d \
+    -v "$HOME/neo4j/plugins:/plugins" \
+    --env NEO4J_AUTH=none  \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true  \
+    -e NEO4J_PLUGINS='["apoc"]' neo4j:5.10.0
 
-echo "Waiting for Neo4j to start..."
-while ! curl --silent --fail http://localhost:7474; do
-  sleep 1
-  counter=$((counter+1))
-  if [ $counter -gt $timeout ]; then
-    echo "Neo4j failed to start in $timeout seconds"
-    exit 1
-  fi
-done
+echo "Installing python3 dependencies..."
+docker exec -i -u root "$MAGE_CONTAINER" bash -c "pip install -r /mage/python/tests/requirements.txt --break-system-packages"
 
-echo "Neo4j started. Running tests..."
-python3 test_e2e_correctness.py --memgraph-port "$MEMGRAPH_PORT" --neo4j-port "$NEO4J_PORT"
+echo "Running e2e correctness tests..."
+docker exec -i -u root "$MAGE_CONTAINER" bash -c "cd /mage/e2e_correctness && python3 test_e2e_correctness.py --memgraph-port $MEMGRAPH_PORT --neo4j-port $NEO4J_PORT --neo4j-container $NEO4J_CONTAINER"
 
 echo "Stopping Neo4j..."
 docker stop "$NEO4J_CONTAINER"
