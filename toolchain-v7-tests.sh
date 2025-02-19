41d3c4e92bb9b82aca6db5ac26d7bd121fa8cef7
matt <mattkjames7@gmail.com>
2025-02-19 15:24:24 +0000

    updated toolchain test script

--- toolchain-v7-tests.sh
+++ toolchain-v7-tests.sh
@@ -3,7 +3,7 @@
 # This test requires the toolchain archive to be in the directory above mage,
 # it also needs to have a plugins directory with the appropriate plugins for 
 # neo4j inside it
-# run `./toolchain-v7-tests.sh > ../tests.log 2>&` to save the output to a log
+# run `./toolchain-v7-tests.sh > ../tests.log 2>&1` to save the output to a log
 
 
 # Get the directory containing this script
@@ -48,19 +48,19 @@ else
 fi
 
 # create python env
-if [ ! -d "env" ]; then
-  python3 -m venv env
-fi
-source env/bin/activate
+# if [ ! -d "env" ]; then
+#   python3 -m venv env
+# fi
+# source env/bin/activate
 
 # prepare to build mage
 if [ ! -d "mage/dist" ]; then
   echo "Preparing to build mage"
   curl https://sh.rustup.rs -sSf | sh -s -- -y
   export PATH="${HOME}/.cargo/bin:${PATH}"
-  pip install -r mage/python/requirements.txt 
-  pip install -r mage/python/tests/requirements.txt 
-  pip install  dgl -f https://data.dgl.ai/wheels/torch-2.3/cu121/repo.html --no-cache
+  python3 -m pip install -r mage/python/requirements.txt --break-system-packages
+  python3 -m pip install -r mage/python/tests/requirements.txt  --break-system-packages
+  python3 -m pip install  dgl -f https://data.dgl.ai/wheels/torch-2.3/cu121/repo.html --no-cache --break-system-packages
   cd mage
   python3 setup build
 else
@@ -73,14 +73,15 @@ ctest --output-on-failure
 cd $BASE_DIR
 
 # Run Python tests
-cd mage/pythonp
+cd mage/python
 python3 -m pytest .
 cd $BASE_DIR
 
 # Run E2E tests
 ./memgraph/build/memgraph \
   --query-modules-directory $BASE_DIR/mage/dist \
-  --storage-properties-on-edges true &
+  --storage-properties-on-edges true \
+  --log-level=TRACE &
 cd mage
 ./test_e2e
 cd $BASE_DIR
@@ -89,22 +90,47 @@ cd $BASE_DIR
 export PLUGINS_DIR="$BASE_DIR/plugins"
 export LOGS_DIR="$BASE_DIR/logs"
 mkdir -p $LOGS_DIR
-docker run \
-    --name testneo4j \
-    -p 7474:7474 -p 7688:7687 \
+# docker run \
+#     --name testneo4j \
+#     -p 7474:7474 -p 7688:7687 \
+#     -d \
+#     --env NEO4J_AUTH=none \
+#     -v $PLUGINS_DIR:/plugins  \
+#     -v $LOG_DIR:/logs \
+#     neo4j:4.4.35
+docker run --rm \
+    --name testneo4j  \
+    -p 7474:7473 \
+    -p 7688:7687 \
     -d \
-    --env NEO4J_AUTH=none \
-    -v $PLUGINS_DIR:/plugins  \
-    -v $LOG_DIR:/logs \
-    neo4j:5.26-ubi9
+    -v "$HOME/neo4j/plugins:/plugins" \
+    --env NEO4J_AUTH=none  \
+    -e NEO4J_apoc_export_file_enabled=true \
+    -e NEO4J_apoc_import_file_enabled=true \
+    -e NEO4J_apoc_import_file_use__neo4j__config=true  \
+    -e NEO4J_PLUGINS='["apoc"]' neo4j:5.10.0
+  #  -e NEO4J_dbms_connector_bolt_advertised__address=localhost:7688 \
+    
+
+sleep 20
+# echo "Waiting for Neo4j to start..."
+# while ! curl --silent --fail http://localhost:7474; do
+#   sleep 1
+#   counter=$((counter+1))
+#   if [ $counter -gt $timeout ]; then
+#     echo "Neo4j failed to start in $timeout seconds"
+#     exit 1
+#   fi
+# done
+echo "Neo4j is up and running."
+
 cd mage
-python test_e2e_correctness.py \
+python3 test_e2e_correctness.py \
   --memgraph-port 7687 \
   --neo4j-port 7688 \
-  --neo4j-container 127.0.0.1
+  --neo4j-container "127.0.0.1"
 
 
 # clean up
-pkill -f "$BASE_DIR/memgraph/build/memgraph
+pkill -f "./memgraph/build/memgraph --query-modules-directory /mnt/data-disk-d/mattkjames7/mage/dist --storage-properties-on-edges true --log-level=TRACE"
 docker stop testneo4j
-docker remove testneo4j
