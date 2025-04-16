6bf1b098cdb3c5e79e895607e619d61e434b2618
Marko Budiselic <marko.budiselic@memgraph.com>
2025-04-16 11:03:26 +0200

    Add v3.2 smoke test updates

---
+++ smoke-release-testing/mgconsole/expressions.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$SCRIPT_DIR/../utils.bash"
+
+# TODO(gitbuda): test the OR expressions https://github.com/memgraph/memgraph/pull/2783
+test_template() {
+  __host="$1"
+  __port="$2"
+  echo "FEATURE: ABC"
+
+  echo "RETURN 1;" | $MEMGRAPH_CONSOLE_BINARY --host $__host --port $__port
+}
+
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
+  # NOTE: Take a look at session_trace.bash for the v1 implementation of binary-docker picker.
+  trap cleanup_memgraph_binary_processes EXIT # To make sure cleanup is done.
+  set -e # To make sure the script will return non-0 in case of a failure.
+  run_memgraph_binary_and_test "--log-level=TRACE --log-file=mg_test_template.logs" test_template
+fi
