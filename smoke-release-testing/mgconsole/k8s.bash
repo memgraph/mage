b881293e76b1e091995428a3bf3fcefbf8bd446e
Marko Budiselic <marko.budiselic@memgraph.com>
2025-04-27 17:19:27 +0200

    Remove TODOs

--- smoke-release-testing/mgconsole/k8s.bash
+++ smoke-release-testing/mgconsole/k8s.bash
@@ -2,11 +2,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/../utils.bash"
 
-# TODO(gitbuda): SAVE https://kubernetes.io/docs/reference/kubectl/quick-reference/
-# TODO(gitbuda): https://mcvidanagama.medium.com/set-up-a-multi-node-kubernetes-cluster-locally-using-kind-eafd46dd63e5
-
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
-  # NOTE: In the custom values file affinity and telemetry were disabled.
+  # NOTE: In the custom values file telemetry was disabled and NodePort was set
+  # as the serviceType. The values file was copied from
+  # https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
   helm install myhadb memgraph/memgraph-high-availability \
     --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
     -f $SCRIPT_DIR/config/k8s-ha-values.yaml
