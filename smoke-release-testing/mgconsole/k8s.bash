704b3c2ed25fad10ff1a3127263eb91343ce14a3
Marko Budiselic <marko.budiselic@memgraph.com>
2025-04-28 21:00:12 +0200

    Add k8s to the smoke testing

--- smoke-release-testing/mgconsole/k8s.bash
+++ smoke-release-testing/mgconsole/k8s.bash
@@ -3,19 +3,26 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/../utils.bash"
 
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
-  # NOTE: In the custom values file affinity and telemetry were disabled.
+  # NOTE: In the custom values file telemetry was disabled and NodePort was set
+  # as the serviceType. The values file was copied from
+  # https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
+  # NOTE: It's critical to run `helm repo update` because otherwise you'll
+  # inject latest template that might not be compatible.
   helm install myhadb memgraph/memgraph-high-availability \
-    --set memgraph.env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,memgraph.env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
+    --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
     -f $SCRIPT_DIR/config/k8s-ha-values.yaml
-  helm list
-  kubectl get pods
   sleep 1000
+  # helm list
+  # kubectl get pods
   # kubectl get pod $pod_name -o yaml
   # kubectl logs $pod_name
   # kubectl describe pods $pod_name
-  # TODO(gitbuda): https://mcvidanagama.medium.com/set-up-a-multi-node-kubernetes-cluster-locally-using-kind-eafd46dd63e5
-
+  # kubectl cluster-info dump
   ## https://komodor.com/learn/kubectl-port-forwarding-how-it-works-use-cases-examples
   # kubectl port-forward $pod_name 17687:7687
   helm uninstall myhadb
+  # NOTE: helm uninstall is not deleting PVCs
+  # kubectl delete pvc --all
+  # NOTE: it take some time to delete all PVs, check with
+  # kubectl get pv
 fi
