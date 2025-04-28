a667e96e4a159b5d5b5c201f3aaf1aa28dc9b327
Marko Budiselic <marko.budiselic@memgraph.com>
2025-04-28 15:24:52 +0200

    Fix k8s setup

--- smoke-release-testing/mgconsole/k8s.bash
+++ smoke-release-testing/mgconsole/k8s.bash
@@ -6,6 +6,8 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
   # NOTE: In the custom values file telemetry was disabled and NodePort was set
   # as the serviceType. The values file was copied from
   # https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
+  # NOTE: It's critical to run `helm repo update` because otherwise you'll
+  # inject latest template that might not be compatible.
   helm install myhadb memgraph/memgraph-high-availability \
     --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
     -f $SCRIPT_DIR/config/k8s-ha-values.yaml
@@ -19,4 +21,8 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
   ## https://komodor.com/learn/kubectl-port-forwarding-how-it-works-use-cases-examples
   # kubectl port-forward $pod_name 17687:7687
   helm uninstall myhadb
+  # NOTE: helm uninstall is not deleting PVCs
+  # kubectl delete pvc --all
+  # NOTE: it take some time to delete all PVs, check with
+  # kubectl get pv
 fi
