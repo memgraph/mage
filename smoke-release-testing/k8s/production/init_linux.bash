7caa32602f79297e93f795a3209eda6b1574fdfc
Marko Budiselic <mbudiselicbuda@gmail.com>
2025-05-01 11:25:24 +0000

    Add real production setup script

---
+++ smoke-release-testing/k8s/production/init_linux.bash
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+# Sources:
+#   * https://medium.com/@subhampradhan966/kubeadm-setup-for-ubuntu-24-04-lts-f6a5fc67f0df
+#   * https://chatgpt.com/share/6812882c-f450-800a-b13c-04eefccc0880
+
+sudo swapoff -a
+# sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl
+
+sudo apt install -y containerd
+sudo mkdir -p /etc/containerd
+containerd config default | sudo tee /etc/containerd/config.toml > /dev/null
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
+sudo apt install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+exit 0
+
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
