dbb13932afed9b8b6eef91bba05bf0a874a47eb5
Marko Budiselic <marko.budiselic@memgraph.com>
2025-05-04 20:33:24 +0200

    Add stanalone memgraph helm run

--- smoke-release-testing/k8s/run-cluster.bash
+++ smoke-release-testing/k8s/run-cluster.bash
@@ -9,14 +9,18 @@ if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
   # https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
   # NOTE: It's critical to run `helm repo update` because otherwise you'll
   # inject latest template that might not be compatible.
+
   helm install myhadb memgraph/memgraph-high-availability \
     --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
     -f $SCRIPT_DIR/ha-values.yaml
   sleep 1000
+
   # helm list
-  helm uninstall myhadb
+  # helm uninstall myhadb
   # NOTE: helm uninstall is not deleting PVCs
   # kubectl delete pvc --all
   # NOTE: it take some time to delete all PVs, check with
   # kubectl get pv
+
+  # helm install mydb memgraph/memgraph
 fi
