48bd17624e09723aabe7ddb9e2433d5bf7137163
Marko Budiselic <marko.budiselic@memgraph.com>
2025-05-10 22:34:45 +0200

    Add RETURN 1; against k8s single instance memgraph

--- smoke-release-testing/k8s/run.bash
+++ smoke-release-testing/k8s/run.bash
@@ -3,30 +3,42 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/../utils.bash"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# NOTES:
+#   * In the custom values file telemetry was disabled and NodePort was set
+#   as the serviceType. The values file was copied from
+#   https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
+#   * It's critical to run `helm repo update` because otherwise you'll inject
+#   latest template that might not be compatible.
+#   * `helm uninstall` has to be used otherwise on `kubectl delete pod
+#   <pod-name>` the pod gets restarted.
+#   * `helm uninstall` is not deleting PVCs, there is the `kubectl delete pvc
+#   --all`.
+#   * It takes some time to delete all PVs, check with `kubectl get pv`.
+#   * If you want more details or helm dry run just append `--debug` of
+#   `--dry-run`.
+
+test_k8s_single() {
+  kind load docker-image $MEMGRAPH_NEXT_DOCKERHUB_IMAGE -n experiment
+  MEMGRAPH_NEXT_DOCKERHUB_TAG="${MEMGRAPH_NEXT_DOCKERHUB_IMAGE##*:}"
+  helm install memgraph-single-smoke memgraph/memgraph \
+    -f $SCRIPT_DIR/values-single.yaml --set image.tag=$MEMGRAPH_NEXT_DOCKERHUB_TAG
+  kubectl wait --for=condition=Ready pod/memgraph-single-smoke-0 --timeout=60s
+  kubectl port-forward memgraph-single-smoke-0 17687:7687 &
+  PF_PID=$!
+  sleep 2
+  echo "RETURN 1;" | $MEMGRAPH_CONSOLE_BINARY --port 17687
+  kill $PF_PID
+  wait $PF_PID 2>/dev/null
+  helm uninstall memgraph-single-smoke
+}
+
+test_k8s_ha() {
+  helm install myhadb memgraph/memgraph-high-availability \
+    --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
+    -f $SCRIPT_DIR/values-ha.yaml
+  # TODO(gitbuda): Setup cluster commands + routing + test query.
+}
+
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
-  # NOTE: In the custom values file telemetry was disabled and NodePort was set
-  # as the serviceType. The values file was copied from
-  # https://github.com/memgraph/helm-charts/blob/main/charts/memgraph-high-availability/values.yaml.
-  # NOTE: It's critical to run `helm repo update` because otherwise you'll
-  # inject latest template that might not be compatible.
-
-  # helm install myhadb memgraph/memgraph-high-availability \
-  #   --set env.MEMGRAPH_ENTERPRISE_LICENSE=$MEMGRAPH_ENTERPRISE_LICENSE,env.MEMGRAPH_ORGANIZATION_NAME=$MEMGRAPH_ORGANIZATION_NAME \
-  #   -f $SCRIPT_DIR/values-ha.yaml
-  # sleep 1000
-
-  # helm install mydb memgraph/memgraph
-
-  helm install mydb-core memgraph/memgraph \
-    -f $SCRIPT_DIR/values-core.yaml --debug # --dry-run
-  sleep 1000
-  # NOTE: helm uninstall has to be used otherwise on `kubectl delete pod <pod-name>` the pod gets restarted.
-
-  # helm list
-  # helm uninstall myhadb
-  # NOTE: helm uninstall is not deleting PVCs
-  # kubectl delete pvc --all
-  # NOTE: it take some time to delete all PVs, check with
-  # kubectl get pv
-
+  test_k8s_single
 fi
