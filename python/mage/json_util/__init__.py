d0e2e81fddf8cf3dcbd260a7e8ae97d84e42e4ba
Memgraph <josip.mrden@memgraph.io>
2025-08-08 11:56:08 +0200

    Put module in top folder

--- python/mage/json_util/__init__.py
+++
@@ -1,3 +0,0 @@
-"""json_util package initialization."""
-# These functions are used by external callers and tests
-from .core import to_json, from_json_list, load_from_path, load_from_url  # noqa: F401
