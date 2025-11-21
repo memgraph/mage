b128ff52e1e0a5a8a945a55108f118fb6f37d738
matt <mattkjames7@gmail.com>
2025-11-21 14:44:17 +0000

    sbom output dir

--- scripts/sbom/mgbuild-container-sbom.sh
+++ scripts/sbom/mgbuild-container-sbom.sh
@@ -11,4 +11,4 @@ docker exec -i -u root $CONTAINER_NAME bash -c "chmod -R a+rwX /home/mg/.conan2 
 docker exec -i -u mg $CONTAINER_NAME bash -c "cd /home/mg/mage && ./scripts/sbom/memgraph-sbom.sh"
 
 mkdir -p sbom
-docker cp $CONTAINER_NAME:/home/mg/mage/cpp/memgraph/memgraph-build-sbom.json sbom/
+docker cp $CONTAINER_NAME:/home/mg/mage/cpp/memgraph/sbom/memgraph-build-sbom.json sbom/
