8052b1d29a7064764bd9ecb360cd14499f9500a9
matt <mattkjames7@gmail.com>
2025-12-01 14:57:04 +0000

    Update scripts and workflows to use conan remote in CI

--- scripts/sbom/mgbuild-container-sbom.sh
+++ scripts/sbom/mgbuild-container-sbom.sh
@@ -5,10 +5,11 @@
 set -euo pipefail
 
 CONTAINER_NAME=mgbuild
+CONAN_REMOTE=${CONAN_REMOTE:-""}
 
 docker exec -i -u root $CONTAINER_NAME bash -c "chmod -R a+rwX /home/mg/.conan2 2>/dev/null || true"
 
-docker exec -i -u mg $CONTAINER_NAME bash -c "cd /home/mg/mage && ./scripts/sbom/memgraph-sbom.sh"
+docker exec -i -u mg $CONTAINER_NAME bash -c "cd /home/mg/mage && export CONAN_REMOTE=$CONAN_REMOTE && ./scripts/sbom/memgraph-sbom.sh"
 
 mkdir -p sbom
 docker cp $CONTAINER_NAME:/home/mg/mage/cpp/memgraph/sbom/memgraph-build-sbom.json sbom/
