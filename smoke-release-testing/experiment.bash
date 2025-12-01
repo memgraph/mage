7c2c2c523030d3ed983e279d1b7a568ca856f67f
Marko Budiselic <marko.budiselic@memgraph.com>
2025-11-30 16:26:00 -0800

    Finish the LOAD PARQUET test

--- smoke-release-testing/experiment.bash
+++ smoke-release-testing/experiment.bash
@@ -2,8 +2,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/utils.bash"
 
-# TODO(gitbuda): Add v3.7 tests.
-
 # NOTE: Use the below line if you just want to spin up the containers and leave them running.
 # run_memgraph_docker_containers RC RC
 # NOTE: Use the below line if you want to cleanup the containers after run of this script.
@@ -12,11 +10,8 @@ echo "Waiting for memgraph to initialize..."
 wait_for_memgraph $MEMGRAPH_DEFAULT_HOST $MEMGRAPH_NEXT_DATA_BOLT_PORT
 echo "Memgraph is up and running!"
 
-# source ./mgconsole/user_profiles.bash
-# test_user_profiles $MEMGRAPH_DEFAULT_HOST $MEMGRAPH_NEXT_DATA_BOLT_PORT
-
 source ./mgconsole/load_xyz.bash
-test_load_xyz $MEMGRAPH_DEFAULT_HOST $MEMGRAPH_NEXT_DATA_BOLT_PORT
+test_load_parquet $MEMGRAPH_DEFAULT_HOST $MEMGRAPH_NEXT_DATA_BOLT_PORT
 
 # NOTE: Test what's the exit status of the script by using `echo $?`:
 #   * if it's == 0 -> all good
--- smoke-release-testing/mgconsole/load_xyz.bash
+++ smoke-release-testing/mgconsole/load_xyz.bash
@@ -2,10 +2,10 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/../utils.bash"
 
-test_load_xyz() {
-  echo "FEATURE: LOAD CSV/JSONL/PARQUET"
+test_load_parquet() {
   # Added in v3.7.
-  # TODO(gitbuda): Create test files and mount them when starting containers.
+  echo "FEATURE: LOAD PARQUET"
+  run_next "MATCH (n) DETACH DELETE n;"
   run_next "LOAD PARQUET FROM '/data/nodes.parquet' AS row CREATE (n:Node {id: row.id});"
-  run_next "MATCH (n) RETURN n;"
+  run_next_csv "MATCH (n) RETURN n;" | python3 $SCRIPT_DIR/validator.py validate_number_of_results -e 1
 }
