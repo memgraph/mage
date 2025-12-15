7d07c5987b3c597738b19a77b4ae8fb3503e42bd
Dr Matt James <mattkjames7@gmail.com>
2025-12-15 17:02:42 +0000

    Fix MAGE build (#707)
    Removes shadowed variable, modified setup script to output build error.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,5 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 export PATH="$HOME/.cargo/bin:${PATH}"
 rustup toolchain install 1.85
 rustup default 1.85
-mkdir $HOME/query_modules
+mkdir -p $HOME/query_modules
 python3 setup build -p $HOME/query_modules/ --cpp-build-flags CMAKE_BUILD_TYPE=${BUILD_TYPE}
