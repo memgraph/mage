e8b8655c5f8e5b845e4a51b7901c3869d5d8ee51
matt <mattkjames7@gmail.com>
2026-01-22 17:15:03 +0000

    tidy up licenses

--- cpp/cugraph_module/algorithms/graph_generator.cu
+++ cpp/cugraph_module/algorithms/graph_generator.cu
@@ -1,5 +1,4 @@
 // Copyright (c) 2016-2022 Memgraph Ltd. [https://memgraph.com]
-// Modified for cuGraph 25.x API compatibility
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- cpp/cugraph_module/mg_cugraph_utility.hpp
+++ cpp/cugraph_module/mg_cugraph_utility.hpp
@@ -1,5 +1,4 @@
 // Copyright 2022 Memgraph Ltd.
-// Modified for cuGraph 25.x API compatibility
 //
 // Licensed under the Apache License, Version 2.0
 
